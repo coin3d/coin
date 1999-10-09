bae264d2d8c1d55042265abef3e4ae208b5b664c
Morten Eriksen <mortene@sim.no>
1999-10-09 19:56:49 +0000

    Added first example to distribution: a clone of OIV's ``ivcat'' tool.

---
+++ examples/ivcat/ivcat.cpp
@@ -0,0 +1,157 @@
+/**************************************************************************\
+ * 
+ *  Copyright (C) 1998-1999 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+\**************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <Inventor/SoDB.h>
+#include <Inventor/SoInput.h>
+#include <Inventor/SoOutput.h>
+#include <Inventor/actions/SoWriteAction.h>
+#include <Inventor/nodes/SoSeparator.h>
+
+/* These two externs are for interfacing against getopt(). */
+extern int optind;
+extern char * optarg;
+
+
+void
+usage(const char * argv_0)
+{
+  fprintf(stderr, "\nUsage: %s [-bfth] [-o filename] file ...\n", argv_0);
+
+  fprintf(stderr, "\t-b:\twrite in binary format\n");
+  fprintf(stderr, "\t-f:\tflatten SoFile nodes\n");
+  fprintf(stderr, "\t-t:\tinline all textures\n");
+  fprintf(stderr, "\t-h:\tshow usage\n");
+  fprintf(stderr, "\t-o:\twrite to ``filename'' instead of stdout\n");
+
+  fprintf(stderr,
+	  "\n  ``%s'' will read all files given on the command line,\n",
+	  argv_0);
+  fprintf(stderr,
+	  "  compile them into a single scene and write them back out\n");
+  fprintf(stderr,
+	  "  again on stdout (unless the ``-o'' option is used).\n\n");
+  fprintf(stderr,
+	  "  Use ``-'' or no input files to read from stdin.\n\n");
+}
+
+int
+main(int argc, char * argv[])
+{
+  int writeasbinary = 0;
+  int flattenfiles = 0;
+  int flattentextures = 0;
+  const char * outname = NULL;
+
+  /* Parse command line. */
+
+  int getoptchar;
+  while ((getoptchar = getopt(argc, argv, "bftho:")) != EOF) {
+    char c = (char)getoptchar;
+    switch (c) {
+    case 'b':
+      writeasbinary = 1;
+      break;
+    case 'f':
+      flattenfiles = 1;
+      break;
+    case 't':
+      flattentextures = 1;
+      break;
+    case 'h':
+      usage(argv[0]);
+      exit(0);
+      break;
+    case 'o':
+      outname = optarg;
+      if (!outname) {
+	usage(argv[0]);
+	exit(1);
+      }
+      break;
+    case '?':
+      usage(argv[0]);
+      exit(1);
+      break;
+    }
+  }
+
+  SoDB::init();
+
+  int i = optind;
+  SoInput stdinp;
+  SoSeparator * root = NULL;
+
+  if (i >= argc) {
+    /* No files specified, read from stdin. */
+    root = SoDB::readAll(&stdinp);
+  }
+  else {
+    SoInput fileinp;
+    root = new SoSeparator;
+
+    for (; i < argc; i++) {
+      if (strcmp("-", argv[i]) == 0) {
+	SoSeparator * tmproot = SoDB::readAll(&stdinp);
+	if (tmproot)
+	  root->addChild(tmproot->getNumChildren() == 1 ?
+			 tmproot->getChild(0) : tmproot);
+      }
+      else {
+	if (fileinp.openFile(argv[i])) {
+	  SoSeparator * tmproot = SoDB::readAll(&fileinp);
+	  if (tmproot)
+	    root->addChild(tmproot->getNumChildren() == 1 ?
+			   tmproot->getChild(0) : tmproot);
+	}
+	else {
+	  fprintf(stderr, "Couldn't open file '%s' -- skipping.\n", argv[i]);
+	}
+      }
+    }
+  }
+
+  if (flattenfiles) {
+    // FIXME: flatten SoFile nodes. 19991009 mortene.
+    fprintf(stderr, "Warning: SoFile flattening not supported yet!\n");
+  }
+
+  if (flattentextures) {
+    // FIXME: inline SoTexture2 nodes. 19991009 mortene.
+    fprintf(stderr, "Warning: texture inlining not supported yet!\n");
+  }
+
+  if (root) {
+    SoOutput out;
+    if (outname) {
+      if (!out.openFile(outname)) {
+	fprintf(stderr, "Couldn't write output to '%s'.\n", outname);
+	exit(1);
+      }
+    }
+    out.setBinary(writeasbinary);
+
+    SoNode * topnode = root->getNumChildren() == 1 ? root->getChild(0) : root;
+    SoWriteAction wa(&out);
+    wa.apply(topnode);
+  }
+
+  return 0;
+}
