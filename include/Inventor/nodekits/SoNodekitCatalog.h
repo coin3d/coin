268f20cb531d6ede7e705ff6c6146eeac5061af8
Morten Eriksen <mortene@sim.no>
1999-11-11 22:14:07 +0000

    Skeleton for nodekit catalog class.

--- include/Inventor/nodekits/SoNodekitCatalog.h
+++ include/Inventor/nodekits/SoNodekitCatalog.h
@@ -20,8 +20,68 @@
 #ifndef __SONODEKITCATALOG_H__
 #define __SONODEKITCATALOG_H__
 
+#include <Inventor/SoType.h>
+
+class SbName;
+
+
 class SoNodekitCatalog
 {
+public:
+  static void initClass(void);
+
+  SoNodekitCatalog(void);
+  ~SoNodekitCatalog();
+
+  int getNumEntries(void) const;
+  int getPartNumber(const SbName & name) const;
+  const SbName & getName(int part) const;
+
+  SoType getType(int part) const;
+  SoType getType(const SbName & name) const;
+  SoType getDefaultType(int part) const;
+  SoType getDefaultType(const SbName & name) const;
+  SbBool isNullByDefault(int part) const;
+  SbBool isNullByDefault(const SbName & name) const;
+
+  SbBool isLeaf(int part) const;
+  SbBool isLeaf(const SbName & name) const;
+  const SbName & getParentName(int part) const;
+  const SbName & getParentName(const SbName & name) const;
+  int getParentPartNumber(int part) const;
+  int getParentPartNumber(const SbName & name) const;
+  const SbName & getRightSiblingName(int part) const;
+  const SbName & getRightSiblingName(const SbName & name) const;
+  int getRightSiblingPartNumber(int part) const;
+  int getRightSiblingPartNumber(const SbName & name) const;
+
+  SbBool isList(int part) const;
+  SbBool isList(const SbName & name) const;
+  SoType getListContainerType(int part) const;
+  SoType getListContainerType(const SbName & name) const;
+  const SoTypeList & getListItemTypes(int part) const;
+  const SoTypeList & getListItemTypes(const SbName & name) const;
+
+  SbBool isPublic(int part) const;
+  SbBool isPublic(const SbName & name) const;
+
+  SoNodekitCatalog * clone(SoType type) const;
+
+  SbBool addEntry(const SbName & name, SoType type, SoType defaulttype,
+		  SbBool isdefaultnull, const SbName & parent,
+		  const SbName & rightsibling, SbBool islist,
+		  SoType listcontainertype, SoType listitemtype,
+		  SbBool ispublic);
+  void addListItemType(int part, SoType type);
+  void addListItemType(const SbName & name, SoType type);
+
+  void narrowTypes(const SbName & name, SoType newtype, SoType newdefaulttype);
+  void setNullByDefault(const SbName & name, SbBool nullbydefault);
+
+  SbBool recursiveSearch(int part, const SbName & name,
+			 SoTypeList * checked) const;
+
+  void printCheck(void) const;
 };
 
 #endif // !__SONODEKITCATALOG_H__
--- src/nodekits/SoNodekitCatalog.cpp
+++ src/nodekits/SoNodekitCatalog.cpp
@@ -26,3 +26,389 @@
 */
 
 #include <Inventor/nodekits/SoNodekitCatalog.h>
+#include <Inventor/lists/SoTypeList.h>
+#include <Inventor/SbName.h>
+#include <assert.h>
+
+/*!
+  FIXME: write function documentation
+*/
+void
+SoNodekitCatalog::initClass(void)
+{
+  assert(0 && "FIXME: not implemented yet");
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SoNodekitCatalog::SoNodekitCatalog(void)
+{
+  assert(0 && "FIXME: not implemented yet");
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SoNodekitCatalog::~SoNodekitCatalog()
+{
+  assert(0 && "FIXME: not implemented yet");
+}
+
+/*!
+  FIXME: write function documentation
+*/
+int
+SoNodekitCatalog::getNumEntries(void) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return 0;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+int
+SoNodekitCatalog::getPartNumber(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return 0;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+const SbName &
+SoNodekitCatalog::getName(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  static SbName name;
+  return name;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SoType
+SoNodekitCatalog::getType(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return SoType::badType();
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SoType
+SoNodekitCatalog::getType(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return SoType::badType();
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SoType
+SoNodekitCatalog::getDefaultType(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return SoType::badType();
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SoType
+SoNodekitCatalog::getDefaultType(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return SoType::badType();
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::isNullByDefault(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::isNullByDefault(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::isLeaf(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::isLeaf(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+const SbName &
+SoNodekitCatalog::getParentName(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  static SbName name;
+  return name;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+const SbName &
+SoNodekitCatalog::getParentName(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  static SbName name;
+  return name;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+int
+SoNodekitCatalog::getParentPartNumber(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return 0;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+int
+SoNodekitCatalog::getParentPartNumber(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return 0;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+const SbName &
+SoNodekitCatalog::getRightSiblingName(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  static SbName name;
+  return name;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+const SbName &
+SoNodekitCatalog::getRightSiblingName(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  static SbName name;
+  return name;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+int
+SoNodekitCatalog::getRightSiblingPartNumber(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return 0;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+int
+SoNodekitCatalog::getRightSiblingPartNumber(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return 0;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::isList(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::isList(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SoType
+SoNodekitCatalog::getListContainerType(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return SoType::badType();
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SoType
+SoNodekitCatalog::getListContainerType(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return SoType::badType();
+}
+
+/*!
+  FIXME: write function documentation
+*/
+const SoTypeList &
+SoNodekitCatalog::getListItemTypes(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  static SoTypeList l;
+  return l;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+const SoTypeList &
+SoNodekitCatalog::getListItemTypes(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  static SoTypeList l;
+  return l;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::isPublic(int /*part*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::isPublic(const SbName & /*name*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SoNodekitCatalog *
+SoNodekitCatalog::clone(SoType /*type*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return NULL;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::addEntry(const SbName & /*name*/, SoType /*type*/,
+			   SoType /*defaulttype*/, SbBool /*isdefaultnull*/,
+			   const SbName & /*parent*/, const SbName & /*rightsibling*/,
+			   SbBool /*islist*/, SoType /*listcontainertype*/,
+			   SoType /*listitemtype*/, SbBool /*ispublic*/)
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+void
+SoNodekitCatalog::addListItemType(int /*part*/, SoType /*type*/)
+{
+  assert(0 && "FIXME: not implemented yet");
+}
+
+/*!
+  FIXME: write function documentation
+*/
+void
+SoNodekitCatalog::addListItemType(const SbName & /*name*/, SoType /*type*/)
+{
+  assert(0 && "FIXME: not implemented yet");
+}
+
+/*!
+  FIXME: write function documentation
+*/
+void
+SoNodekitCatalog::narrowTypes(const SbName & /*name*/,
+			      SoType /*newtype*/, SoType /*newdefaulttype*/)
+{
+  assert(0 && "FIXME: not implemented yet");
+}
+
+/*!
+  FIXME: write function documentation
+*/
+void
+SoNodekitCatalog::setNullByDefault(const SbName & /*name*/, SbBool /*nullbydefault*/)
+{
+  assert(0 && "FIXME: not implemented yet");
+}
+
+/*!
+  FIXME: write function documentation
+*/
+SbBool
+SoNodekitCatalog::recursiveSearch(int /*part*/, const SbName & /*name*/,
+				  SoTypeList * /*checked*/) const
+{
+  assert(0 && "FIXME: not implemented yet");
+  return FALSE;
+}
+
+/*!
+  FIXME: write function documentation
+*/
+void
+SoNodekitCatalog::printCheck(void) const
+{
+  assert(0 && "FIXME: not implemented yet");
+}
