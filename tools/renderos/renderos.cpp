6357550be8d482b1e6ada79ea44eed4e74596f2b
Morten Eriksen <mortene@sim.no>
2000-01-17 21:59:55 +0000

    Little utility program for rendering a scene to file via the SoOffscreenRenderer interface.

---
+++ tools/renderos/renderos.cpp
@@ -0,0 +1,157 @@
+/**************************************************************************\
+ *
+ *  Copyright (C) 1998-2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+\**************************************************************************/
+
+#include <Inventor/SoDB.h>
+#include <Inventor/SoInput.h>
+#include <Inventor/SoInteraction.h>
+#include <Inventor/SoOffscreenRenderer.h>
+#include <Inventor/nodekits/SoNodeKit.h>
+#include <Inventor/nodes/SoDirectionalLight.h>
+#include <Inventor/nodes/SoPerspectiveCamera.h>
+#include <Inventor/nodes/SoSeparator.h>
+#include <stdio.h>
+
+// FIXME: do a proper check for this through configure.
+// 19991206 mortene.
+#ifndef _WIN32
+#define HAVE_GETOPT 1
+#endif // !_WIN32
+
+#if HAVE_GETOPT
+#include <unistd.h>
+/* These two externs are for interfacing against getopt(). */
+extern int optind;
+extern char * optarg;
+#endif // HAVE_GETOPT
+
+
+void
+usage(const char * invname)
+{
+#if HAVE_GETOPT
+  fprintf(stderr, "\nUsage: %s [-h] [-x width] [-y height] <modelfile.iv> <snapshot.rgb>\n\n", invname);
+  fprintf(stderr, "\t-h:\tshow usage\n");
+  fprintf(stderr, "\t-x:\tset width in pixels (default 640)\n");
+  fprintf(stderr, "\t-y:\tset height in pixels (default 480)\n");
+  fprintf(stderr, "\n");
+#else // !HAVE_GETOPT
+  fprintf(stderr, "\nUsage: %s <modelfile.iv> <snapshot.rgb>\n\n", invname);
+#endif // !HAVE_GETOPT
+}
+
+int
+main(int argc, char ** argv)
+{
+  int width = 640;
+  int height = 480;
+
+#if HAVE_GETOPT
+  /* Parse command line. */
+  int getoptchar;
+  while ((getoptchar = getopt(argc, argv, "?hx:y:")) != EOF) {
+    char c = (char)getoptchar;
+    switch (c) {
+    case '?':
+    case 'h':
+      usage(argv[0]);
+      exit(0);
+      break;
+    case 'x':
+      width = atoi(optarg);
+      break;
+    case 'y':
+      height = atoi(optarg);
+      break;
+    default:
+      assert(FALSE);
+      break;
+    }
+  }
+
+  int i = optind;
+#else // !HAVE_GETOPT
+  int i = 1;
+#endif // !HAVE_GETOPT
+
+  if ((argc - i) != 2) {
+    usage(argv[0]);
+    exit(1);
+  }
+
+  const char * modelfile = argv[i++];
+  const char * snapshotdump = argv[i++];
+
+  SoDB::init();
+  SoNodeKit::init();
+  SoInteraction::init();
+
+  SoInput in;
+  if (!in.openFile(modelfile)) {
+    (void)fprintf(stderr, "Couldn't open file '%s'.\n", modelfile);
+    exit(1);
+  }
+
+  SoSeparator * fileroot = SoDB::readAll(&in);
+  if (!fileroot) {
+    (void)fprintf(stderr, "Couldn't read file '%s'.\n", modelfile);
+    exit(1);
+  }
+
+  SoSeparator * root = new SoSeparator;
+  root->ref();
+
+  SoPerspectiveCamera * camera = new SoPerspectiveCamera;
+  root->addChild(camera);
+
+  SoDirectionalLight * light = new SoDirectionalLight;
+  light->direction.setValue(0.5, 0.5, -0.8);
+  root->addChild(light);
+
+  root->addChild(fileroot);
+
+  SbViewportRegion vp(width, height);
+  camera->viewAll(root, vp);
+
+  SoOffscreenRenderer osr(vp);
+//    osr.setComponents(SoOffscreenRenderer::RGB);
+  SbBool wasrendered = osr.render(root);
+  root->unref();
+
+  if (!wasrendered) {
+    (void)fprintf(stderr, "Couldn't render buffer (functionality not available?).\n");
+    exit(1);
+  }
+
+  FILE * snapfp = fopen(snapshotdump, "w");
+  if (!snapfp) {
+    (void)fprintf(stderr, "Couldn't open file '%s'.\n", snapshotdump);
+    exit(1);
+  }
+
+  if (!osr.writeToRGB(snapfp)) {
+    (void)fprintf(stderr, "Couldn't write file '%s'.\n", snapshotdump);
+  }
+  else {
+    (void)fprintf(stdout, "Bitmap file in SGI RGB format written to '%s'.\n",
+                  snapshotdump);
+  }
+
+  (void)fclose(snapfp);
+  return 0;
+}
