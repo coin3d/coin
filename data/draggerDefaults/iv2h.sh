f10b9ffb8dc1e376001a023e78f7b3c8b488eb7f
Morten Eriksen <mortene@sim.no>
2000-02-02 11:58:54 +0000

    HPUX aCC wants to have backslashes at the end of multi-line strings.

--- data/draggerDefaults/iv2h.sh
+++ data/draggerDefaults/iv2h.sh
@@ -6,9 +6,13 @@ echo "#ifndef _SO${UPCASEBASE}_IV_H_"
 echo "#define _SO${UPCASEBASE}_IV_H_"
 echo
 
-echo "static const char draggergeometry[] = \"#Inventor V2.1 ascii"
+# The backslashes at the end of each line should not really be
+# necessary (I think -- haven't checked the spec yet), but HPUX aCC
+# needs them. mortene.
 
-cat $1 | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | egrep -v '^[ \t]*#' | sed 's/#.*//g'
+echo "static const char draggergeometry[] = \"#Inventor V2.1 ascii\\"
+
+cat $1 | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | egrep -v '^[ \t]*#' | sed 's/#.*//g' | sed 's/$/\\/'
 
 echo "\";"
 
