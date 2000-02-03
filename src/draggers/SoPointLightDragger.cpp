d06705cac572dc3fde44063820d2c9e0d43f5fbf
Morten Eriksen <mortene@sim.no>
2000-02-03 15:34:54 +0000

    SoPointLightDragger implementation, by pederb.

--- src/draggers/SoPointLightDragger.cpp
+++ src/draggers/SoPointLightDragger.cpp
@@ -20,7 +20,15 @@
 #include <Inventor/draggers/SoPointLightDragger.h>
 #include <Inventor/draggers/SoDragPointDragger.h>
 #include <Inventor/nodes/SoMaterial.h>
+#include <Inventor/sensors/SoFieldSensor.h>
+#include <Inventor/sensors/SoFieldSensor.h>
+#include <Inventor/SbRotation.h>
+#include <Inventor/SbVec3f.h>
+#include <Inventor/SbMatrix.h>
 
+#if COIN_DEBUG
+#include <Inventor/errors/SoDebugError.h>
+#endif // COIN_DEBUG
 
 SO_KIT_SOURCE(SoPointLightDragger);
 
@@ -38,38 +46,112 @@ SoPointLightDragger::SoPointLightDragger(void)
   SO_KIT_ADD_CATALOG_ENTRY(material, SoMaterial, TRUE, topSeparator, translator, TRUE);
   SO_KIT_ADD_CATALOG_ENTRY(translator, SoDragPointDragger, TRUE, topSeparator, geomSeparator, TRUE);
 
-  SO_NODE_ADD_FIELD(translation, (0.0f, 0.0f, 0.0f));
+  if (SO_KIT_IS_FIRST_INSTANCE()) {
+    SoInteractionKit::readDefaultParts("pointLightDragger.iv", NULL, 0);
+  }
 
+  SO_NODE_ADD_FIELD(translation, (0.0f, 0.0f, 0.0f));
   SO_KIT_INIT_INSTANCE();
+
+  SoDragger *pdragger = SO_GET_ANY_PART(this, "translator", SoDragPointDragger);
+  assert(pdragger);
+
+  this->setPartAsDefault("material", "pointLightOverallMaterial");
+  
+  this->addValueChangedCallback(SoPointLightDragger::valueChangedCB);
+  this->fieldSensor = new SoFieldSensor(SoPointLightDragger::fieldSensorCB, this);
+  this->fieldSensor->setPriority(0);
+  this->setUpConnections(TRUE, TRUE);
 }
 
 
 SoPointLightDragger::~SoPointLightDragger()
 {
-  COIN_STUB();
+  delete this->fieldSensor;
 }
 
 SbBool
 SoPointLightDragger::setUpConnections(SbBool onoff, SbBool doitalways)
 {
-  COIN_STUB();
-  return FALSE;
+  if (!doitalways && this->connectionsSetUp == onoff) return onoff;
+  
+  if (onoff) {
+    inherited::setUpConnections(onoff, doitalways);
+    SoDragger *child = (SoDragger*) this->getAnyPart("translator", FALSE);
+    child->setPartAsDefault("yzTranslator.translator", 
+                            "pointLightTranslatorPlaneTranslator");
+    child->setPartAsDefault("xzTranslator.translator", 
+                            "pointLightTranslatorPlaneTranslator");
+    child->setPartAsDefault("xyTranslator.translator", 
+                            "pointLightTranslatorPlaneTranslator");
+
+    child->setPartAsDefault("yzTranslator.translatorActive",
+                            "pointLightTranslatorPlaneTranslatorActive");
+    child->setPartAsDefault("xzTranslator.translatorActive",
+                            "pointLightTranslatorPlaneTranslatorActive");
+    child->setPartAsDefault("xyTranslator.translatorActive",
+                            "pointLightTranslatorPlaneTranslatorActive");
+
+    child->setPartAsDefault("xTranslator.translator", 
+                            "pointLightTranslatorLineTranslator");
+    child->setPartAsDefault("yTranslator.translator", 
+                            "pointLightTranslatorLineTranslator");
+    child->setPartAsDefault("zTranslator.translator", 
+                            "pointLightTranslatorLineTranslator");
+
+    child->setPartAsDefault("xTranslator.translatorActive", 
+                            "pointLightTranslatorLineTranslatorActive");
+    child->setPartAsDefault("yTranslator.translatorActive", 
+                            "pointLightTranslatorLineTranslatorActive");
+    child->setPartAsDefault("zTranslator.translatorActive", 
+                            "pointLightTranslatorLineTranslatorActive");
+    this->registerChildDragger(child);
+    if (this->fieldSensor->getAttachedField() != &this->translation) {
+      this->fieldSensor->attach(&this->translation);
+    }
+  }
+  else {
+    SoDragger *child = (SoDragger*) this->getAnyPart("translator", FALSE);
+    this->unregisterChildDragger(child);
+    if (this->fieldSensor->getAttachedField() != NULL) {
+      this->fieldSensor->detach();
+    }
+    inherited::setUpConnections(onoff, doitalways);
+  }
+  return !(this->connectionsSetUp = onoff);
 }
 
 void
 SoPointLightDragger::setDefaultOnNonWritingFields(void)
 {
+  // FIXME: not sure if I should call setDefault() for any fields here...
+  // pederb, 20000203
   COIN_STUB();
 }
 
 void
-SoPointLightDragger::fieldSensorCB(void * f, SoSensor * s)
+SoPointLightDragger::fieldSensorCB(void *d, SoSensor *)
 {
-  COIN_STUB();
+  SoPointLightDragger *thisp = (SoPointLightDragger*)d;
+  SbMatrix matrix = thisp->getMotionMatrix();
+  thisp->workFieldsIntoTransform(matrix);
+  thisp->setMotionMatrix(matrix);
 }
 
 void
-SoPointLightDragger::valueChangedCB(void * f, SoDragger * d)
+SoPointLightDragger::valueChangedCB(void *, SoDragger * d)
 {
-  COIN_STUB();
+  SoPointLightDragger *thisp = (SoPointLightDragger*)d;
+
+  SbMatrix matrix = thisp->getMotionMatrix();
+  SbVec3f t;
+  t[0] = matrix[3][0];
+  t[1] = matrix[3][1];
+  t[2] = matrix[3][2];
+  
+  thisp->fieldSensor->detach();
+  if (thisp->translation.getValue() != t) {
+    thisp->translation = t;
+  }
+  thisp->fieldSensor->attach(&thisp->translation);
 }
