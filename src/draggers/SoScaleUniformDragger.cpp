af10bfa5497b9fce41c1803ca0e10bc9074ea9af
Morten Eriksen <mortene@sim.no>
2000-02-07 07:55:20 +0000

    Implementation of SoScaleUniformDragger, by pederb.

--- src/draggers/SoScaleUniformDragger.cpp
+++ src/draggers/SoScaleUniformDragger.cpp
@@ -21,6 +21,13 @@
 #include <Inventor/nodes/SoSeparator.h>
 #include <Inventor/nodes/SoSwitch.h>
 
+#include <Inventor/SbMatrix.h>
+#include <Inventor/SbVec2f.h>
+#include <Inventor/SbRotation.h>
+#include <Inventor/SbVec3f.h>
+#include <Inventor/projectors/SbLineProjector.h>
+#include <Inventor/sensors/SoFieldSensor.h>
+#include <math.h>
 
 SO_KIT_SOURCE(SoScaleUniformDragger);
 
@@ -42,68 +49,167 @@ SoScaleUniformDragger::SoScaleUniformDragger(void)
   SO_KIT_ADD_CATALOG_ENTRY(feedback, SoSeparator, TRUE, feedbackSwitch, feedbackActive, TRUE);
   SO_KIT_ADD_CATALOG_ENTRY(feedbackActive, SoSeparator, TRUE, feedbackSwitch, "", TRUE);
 
+  if (SO_KIT_IS_FIRST_INSTANCE()) {
+    SoInteractionKit::readDefaultParts("scaleUniformDragger.iv", NULL, 0);
+  }
   SO_NODE_ADD_FIELD(scaleFactor, (1.0f, 1.0f, 1.0f));
 
   SO_KIT_INIT_INSTANCE();
+
+  // initialize default parts
+  this->setPartAsDefault("scaler", "scaleUniformScaler");
+  this->setPartAsDefault("scalerActive", "scaleUniformScalerActive");
+  this->setPartAsDefault("feedback", "scaleUniformFeedback");
+  this->setPartAsDefault("feedbackActive", "scaleUniformFeedbackActive");
+
+  // initialize swich values
+  SoSwitch *sw;
+  sw = SO_GET_ANY_PART(this, "scalerSwitch", SoSwitch);
+  SoInteractionKit::setSwitchValue(sw, 0);
+  sw = SO_GET_ANY_PART(this, "feedbackSwitch", SoSwitch);
+  SoInteractionKit::setSwitchValue(sw, 0);
+
+  // setup projector
+  this->lineProj = new SbLineProjector();
+
+  this->addStartCallback(SoScaleUniformDragger::startCB);
+  this->addMotionCallback(SoScaleUniformDragger::motionCB);
+  this->addFinishCallback(SoScaleUniformDragger::finishCB);
+
+  this->addValueChangedCallback(SoScaleUniformDragger::valueChangedCB);
+
+  this->fieldSensor = new SoFieldSensor(SoScaleUniformDragger::fieldSensorCB, this);
+  this->fieldSensor->setPriority(0);
+
+  this->setUpConnections(TRUE, TRUE);
+
 }
 
 
 SoScaleUniformDragger::~SoScaleUniformDragger()
 {
-  COIN_STUB();
+  delete this->lineProj;
+  delete this->fieldSensor;
 }
 
 SbBool
 SoScaleUniformDragger::setUpConnections(SbBool onoff, SbBool doitalways)
 {
-  COIN_STUB();
-  return FALSE;
+  if (!doitalways && this->connectionsSetUp == onoff) return onoff;
+
+  SbBool oldval = this->connectionsSetUp;
+
+  if (onoff) {
+    inherited::setUpConnections(onoff, doitalways);
+
+    SoScaleUniformDragger::fieldSensorCB(this, NULL);
+
+    if (this->fieldSensor->getAttachedField() != &this->scaleFactor) {
+      this->fieldSensor->attach(&this->scaleFactor);
+    }
+  }
+  else {
+    if (this->fieldSensor->getAttachedField() != NULL) {
+      this->fieldSensor->detach();
+    }
+    inherited::setUpConnections(onoff, doitalways);
+  }
+  this->connectionsSetUp = onoff;
+  return oldval;
 }
 
 void
-SoScaleUniformDragger::fieldSensorCB(void * f, SoSensor * s)
+SoScaleUniformDragger::fieldSensorCB(void * d, SoSensor *)
 {
-  COIN_STUB();
+  assert(d);
+  SoScaleUniformDragger *thisp = (SoScaleUniformDragger*)d;
+  SbMatrix matrix = thisp->getMotionMatrix();
+
+  SbVec3f t, s;
+  SbRotation r, so;
+
+  matrix.getTransform(t, r, s, so);
+  s = thisp->scaleFactor.getValue();
+  matrix.setTransform(t, r, s, so);
+  thisp->setMotionMatrix(matrix);
 }
 
 void
-SoScaleUniformDragger::valueChangedCB(void * f, SoDragger * d)
+SoScaleUniformDragger::valueChangedCB(void *, SoDragger * d)
 {
-  COIN_STUB();
+  SoScaleUniformDragger *thisp = (SoScaleUniformDragger*)d;
+  SbMatrix matrix = thisp->getMotionMatrix();
+
+  SbVec3f trans, scale;
+  SbRotation rot, scaleOrient;
+  matrix.getTransform(trans, rot, scale, scaleOrient);
+  thisp->fieldSensor->detach();
+  if (thisp->scaleFactor.getValue() != scale)
+    thisp->scaleFactor = scale;
+  thisp->fieldSensor->attach(&thisp->scaleFactor);
 }
 
 void
-SoScaleUniformDragger::startCB(void * f, SoDragger * d)
+SoScaleUniformDragger::startCB(void *, SoDragger * d)
 {
-  COIN_STUB();
+  SoScaleUniformDragger *thisp = (SoScaleUniformDragger*)d;
+  thisp->dragStart();
 }
 
 void
-SoScaleUniformDragger::motionCB(void * f, SoDragger * d)
+SoScaleUniformDragger::motionCB(void *, SoDragger * d)
 {
-  COIN_STUB();
+  SoScaleUniformDragger *thisp = (SoScaleUniformDragger*)d;
+  thisp->drag();
 }
 
 void
-SoScaleUniformDragger::finishCB(void * f, SoDragger * d)
+SoScaleUniformDragger::finishCB(void *, SoDragger * d)
 {
-  COIN_STUB();
+  SoScaleUniformDragger *thisp = (SoScaleUniformDragger*)d;
+  thisp->dragFinish();
 }
 
 void
 SoScaleUniformDragger::dragStart(void)
 {
-  COIN_STUB();
+  SoSwitch *sw;
+  sw = SO_GET_ANY_PART(this, "scalerSwitch", SoSwitch);
+  SoInteractionKit::setSwitchValue(sw, 1);
+  sw = SO_GET_ANY_PART(this, "feedbackSwitch", SoSwitch);
+  SoInteractionKit::setSwitchValue(sw, 1);
+
+  SbVec3f startPt = this->getLocalStartingPoint();
+  this->lineProj->setLine(SbLine(SbVec3f(0.0f, 0.0f, 0.0f), startPt));
 }
 
 void
 SoScaleUniformDragger::drag(void)
 {
-  COIN_STUB();
+  this->lineProj->setViewVolume(this->getViewVolume());
+  this->lineProj->setWorkingSpace(this->getLocalToWorldMatrix());
+  SbVec3f startPt = this->getLocalStartingPoint();
+  SbVec3f projPt = lineProj->project(this->getNormalizedLocaterPosition());
+
+  float orglen = startPt.length();
+  float currlen = projPt.length();
+  
+  float scale = 0.0f;
+  if (orglen > 0.0f) scale = currlen / orglen;
+  
+  if (scale > 0.0f && startPt.dot(projPt) < 0.0f) scale = 0.0f;
+
+  this->setMotionMatrix(this->appendScale(this->getStartMotionMatrix(),
+                                          SbVec3f(scale, scale, scale),
+                                          SbVec3f(0.0f, 0.0f, 0.0f)));
 }
 
 void
 SoScaleUniformDragger::dragFinish(void)
 {
-  COIN_STUB();
+  SoSwitch *sw;
+  sw = SO_GET_ANY_PART(this, "scalerSwitch", SoSwitch);
+  SoInteractionKit::setSwitchValue(sw, 0);
+  sw = SO_GET_ANY_PART(this, "feedbackSwitch", SoSwitch);
+  SoInteractionKit::setSwitchValue(sw, 0);
 }
