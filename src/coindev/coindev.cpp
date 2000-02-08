4f01b16940d662529fc1d00b73d2ffb3bafe59b9
Morten Eriksen <mortene@sim.no>
2000-02-08 21:04:28 +0000

    Kill the "coindev" trick for overloading methods in other shared libraries -- as this can be done just as easy from the (test)app.

--- src/coindev/coindev.cpp
+++
@@ -1,3 +0,0 @@
-// This file is supposed to be used as a testbed for new code during
-// Coin development. Read the file "HACKING" at the root of the Coin
-// package for further information on how to use this mechanism.
