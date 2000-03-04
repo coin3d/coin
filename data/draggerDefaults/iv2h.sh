94e52b1bf3beae8823ac21c0792ba60d78475e44
Morten Eriksen <mortene@sim.no>
2000-03-04 23:17:37 +0000

    Misc small fixes, including a compilation fix for the SUNWspro 4.2 compiler by Loring Holden.

--- data/draggerDefaults/iv2h.sh
+++ data/draggerDefaults/iv2h.sh
@@ -2,19 +2,15 @@
 
 UPCASEBASE=`basename $1 | sed 's/\.iv$//' | tr [a-z] [A-Z]`
 
-echo "#ifndef _SO${UPCASEBASE}_IV_H_"
-echo "#define _SO${UPCASEBASE}_IV_H_"
+echo "#ifndef SO${UPCASEBASE}_IV_H"
+echo "#define SO${UPCASEBASE}_IV_H"
 echo
-
-# The backslashes at the end of each line should not really be
-# necessary (I think -- haven't checked the spec yet), but HPUX aCC
-# needs them. mortene.
-
-echo "static const char draggergeometry[] = \"#Inventor V2.1 ascii\\n\""
+echo "static const char draggergeometry[] = \"#Inventor V2.1 ascii\""
+echo "\"\\n\""
 
 cat $1 | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | egrep -v '^[ \t]*#' | sed 's/#.*//g' | sed 's/$/\\n"/' | sed 's/^/"/'
 
 echo "\"\\n\";"
 
 echo
-echo "#endif /* !_SO${UPCASEBASE}_IV_H_*/"
+echo "#endif /* !SO${UPCASEBASE}_IV_H */"
