16aa2a3720921e20b4243b69d56bf1156b4ec518
Morten Eriksen <mortene@sim.no>
2000-06-02 10:14:41 +0000

    "--clean" removes Makefile.in files too. "$DIE" is either "false" or "true" (more useful values for scripting). Fixed by larsa.

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@
 
 # Author: Morten Eriksen, <mortene@sim.no>.
 
-DIE=0
+DIE=false
 
 PROJECT=Coin
 MACRODIR=conf-macros
@@ -25,6 +25,7 @@ if test "$1" = "--clean"; then
         missing \
         mkinstalldirs \
         stamp-h*
+  find . -name Makefile.in -print | xargs rm
   exit
 elif test "$1" = "--add"; then
   AUTOMAKE_ADD="--add-missing --gnu --copy"
@@ -44,7 +45,7 @@ if test -z "`autoconf --version | grep \" $AUTOCONF_VER\" 2> /dev/null`"; then
     echo ""
     echo "   ftp://ftp.sim.no/pub/coin/autoconf-2.14.1-coin.tar.gz"
     echo ""
-    DIE=1
+    DIE=true
 fi
 
 AUTOMAKE_VER=1.4a-SIM-20000531  # Automake from CVS
@@ -58,7 +59,7 @@ if test -z "`automake --version | grep \" $AUTOMAKE_VER\" 2> /dev/null`"; then
     echo ""
     echo "   ftp://ftp.sim.no/pub/coin/automake-1.4a-coin.tar.gz"
     echo ""
-    DIE=1
+    DIE=true
 fi
 
 LIBTOOL_VER=1.3.5
@@ -69,7 +70,7 @@ if test -z "`libtool --version | egrep \"$LIBTOOL_VER\" 2> /dev/null`"; then
     echo ""
     echo "Get ftp://ftp.gnu.org/pub/libtool/libtool-1.3.5.tar.gz"
     echo ""
-    DIE=1
+    DIE=true
 fi
 
 # The separate $MACRODIR module was added late in the project, and
@@ -86,15 +87,12 @@ then
         echo "You probably checked out $PROJECT before ``$MACRODIR'' was added."
         echo "Run 'cvs -d :pserver:cvs@cvs.sim.no:/export/cvsroot co $MACRODIR'"
         echo "to try again."
-	DIE=1
+	DIE=true
 	exit 1
     fi
 fi
 
-if test "$DIE" -eq 1; then
-        exit 1
-fi
-
+$DIE && exit 1
 
 echo "Running aclocal (generating aclocal.m4)..."
 aclocal -I $MACRODIR
