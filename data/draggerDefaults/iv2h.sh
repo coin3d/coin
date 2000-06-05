381b33bf55695dcfbc003117978a460a610138cd
Morten Eriksen <mortene@sim.no>
2000-06-05 17:01:37 +0000

    Minor fix to work better with Linux "echo" command. (This might break with "echo" on other platforms -- but hopefully not. The code now looks more correct.  :^/)

--- data/draggerDefaults/iv2h.sh
+++ data/draggerDefaults/iv2h.sh
@@ -6,11 +6,15 @@ echo "#ifndef SO${UPCASEBASE}_IV_H"
 echo "#define SO${UPCASEBASE}_IV_H"
 echo
 echo "static const char ${UPCASEBASE}_draggergeometry[] = \"#Inventor V2.1 ascii\""
-echo '"\\n"'
+
+# Make sure there's at least two linefeeds after the header string,
+# to be compliant with the fileformat spec.
+echo \"\\n\"
+echo \"\\n\"
 
 cat $1 | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | egrep -v '^[ \t]*#' | sed 's/#.*//g' | sed 's/$/\\n"/' | sed 's/^/"/'
 
-echo '"\\n";'
+echo ';'
 
 echo
 echo "#endif /* !SO${UPCASEBASE}_IV_H */"
