8cbfddc9d4392f8222e7114922c4f6166b41e96e
Morten Eriksen <mortene@sim.no>
2000-06-06 11:35:42 +0000

    Misc improvements to the script.

--- data/draggerDefaults/iv2h.sh
+++ data/draggerDefaults/iv2h.sh
@@ -1,21 +1,54 @@
 #!/bin/sh
+############################################################################
+# iv2h.sh
+#
+# This script generates a C header file from an Inventor file.  The header
+# just contains the inventor file as a static char buffer.
+#
+# Authors:
+#   Morten Eriksen <mortene@sim.no>
+#   Lars J. Aas <larsa@sim.no>
+#
 
-UPCASEBASE=`basename $1 | sed 's/\.iv$//' | tr [a-z] [A-Z]`
+UPCASEBASE=`basename $1 .iv | tr a-z A-Z`
 
-echo "#ifndef SO${UPCASEBASE}_IV_H"
-echo "#define SO${UPCASEBASE}_IV_H"
-echo
-echo "static const char ${UPCASEBASE}_draggergeometry[] = \"#Inventor V2.1 ascii\""
+cat <<HERE_DOC_END
+/**************************************************************************
+ *
+ *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This file is part of the Coin library.
+ *
+ *  This file may be distributed under the terms of the Q Public License
+ *  as defined by Troll Tech AS of Norway and appearing in the file
+ *  LICENSE.QPL included in the packaging of this file.
+ *
+ *  If you want to use Coin in applications not covered by licenses
+ *  compatible with the QPL, you can contact SIM to aquire a
+ *  Professional Edition license for Coin.
+ *
+ *  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
+ *
+ **************************************************************************/
 
-# Make sure there's at least two linefeeds after the header string,
-# to be compliant with the fileformat spec. (The strange syntax is a
-# workaround for ``echo'' not behaving as expected on SGI IRIX with
-# ``echo \"\\n\"'').
-echo | sed 's/$/"\\n\\n"/'
+#ifndef SO_${UPCASEBASE}_IV_H
+#define SO_${UPCASEBASE}_IV_H
 
-cat $1 | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | egrep -v '^[ \t]*#' | sed 's/#.*//g' | sed 's/$/\\n"/' | sed 's/^/"/'
+static const char ${UPCASEBASE}_draggergeometry[] =
+HERE_DOC_END
 
-echo ';'
+cat $1 | sed -e \
+'s/\\/\\\\/g
+s/"/\\"/g
+3,$ s/^[ \t]*#.*//
+s/^/  "/
+s/$/\\n"/
+$ s/$/;/'
 
-echo
-echo "#endif /* !SO${UPCASEBASE}_IV_H */"
+# ATTN: the file did not just get corrupted ;-)
+
+cat <<HERE_DOC_END
+
+#endif /* ! SO_${UPCASEBASE}_IV_H */
+HERE_DOC_END
