f6415e76a97909611ccaeb7f9aa1acd838a8fac9
Morten Eriksen <mortene@sim.no>
2000-06-06 07:27:24 +0000

    Used a sed-workaround for the problem of ``echo'' not behaving as expected on SGI IRIX. Problem reported by Felix Ritter.

--- data/draggerDefaults/iv2h.sh
+++ data/draggerDefaults/iv2h.sh
@@ -8,9 +8,10 @@ echo
 echo "static const char ${UPCASEBASE}_draggergeometry[] = \"#Inventor V2.1 ascii\""
 
 # Make sure there's at least two linefeeds after the header string,
-# to be compliant with the fileformat spec.
-echo \"\\n\"
-echo \"\\n\"
+# to be compliant with the fileformat spec. (The strange syntax is a
+# workaround for ``echo'' not behaving as expected on SGI IRIX with
+# ``echo \"\\n\"'').
+echo | sed 's/$/"\\n\\n"/'
 
 cat $1 | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | egrep -v '^[ \t]*#' | sed 's/#.*//g' | sed 's/$/\\n"/' | sed 's/^/"/'
 
