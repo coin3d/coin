04629fbb668c7fb34995e4a1245ce370404257ae
Morten Eriksen <mortene@sim.no>
2000-06-12 14:55:40 +0000

    Implementation of missing methods in SoTransform, done by pederb.

--- src/nodes/SoTransform.cpp
+++ src/nodes/SoTransform.cpp
@@ -34,13 +34,10 @@
 
 #include <Inventor/nodes/SoTransform.h>
 #include <Inventor/nodes/SoSubNodeP.h>
-
 #include <Inventor/actions/SoGLRenderAction.h>
 #include <Inventor/actions/SoGetMatrixAction.h>
 #include <Inventor/elements/SoModelMatrixElement.h>
 #include <Inventor/elements/SoGLNormalizeElement.h>
-
-#include <coindefs.h> // COIN_STUB()
 
 
 /*!
@@ -94,136 +91,189 @@ void
 SoTransform::initClass(void)
 {
   SO_NODE_INTERNAL_INIT_CLASS(SoTransform);
-}
+} 
 
 /*!
-  FIXME: write function documentation
+  Sets the transformation to translate to \a frompoint, with a rotation
+  so that the (0,0,-1) vector is rotated into the vector from \a frompoint
+  to \a topoint.
 */
 void
 SoTransform::pointAt(const SbVec3f & frompoint, const SbVec3f & topoint)
 {
-  COIN_STUB();
+  this->scaleFactor = SbVec3f(1.0f, 1.0f, 1.0f);
+  this->center = SbVec3f(0.0f, 0.0f, 0.0f);
+  this->scaleOrientation = SbRotation(SbVec3f(0.0f, 0.0f, 1.0f), 0.0f);
+  
+  this->translation = frompoint;  
+  SbVec3f dir = topoint - frompoint;
+  dir.normalize();
+  
+  SbRotation rot(SbVec3f(0.0f, 0.0f, -1.0f), dir);
+  this->rotation = rot;
 }
 
 /*!
-  FIXME: write function documentation
+  Calculates the matrices to/from scale space.
 */
 void
 SoTransform::getScaleSpaceMatrix(SbMatrix & mat, SbMatrix & inv) const
-{
-  COIN_STUB();
+{  
+  SbMatrix tmp;
+  mat.setTranslate(-center.getValue());
+  tmp.setRotate(scaleOrientation.getValue().inverse());
+  mat.multRight(tmp);
+  tmp.setScale(scaleFactor.getValue());
+  mat.multRight(tmp);
+  inv = mat.inverse();
 }
 
 /*!
-  FIXME: write function documentation
+  Calculates the matrices to/from rotation space.
 */
 void
 SoTransform::getRotationSpaceMatrix(SbMatrix & mat, SbMatrix & inv) const
 {
-  COIN_STUB();
+  SbMatrix tmp;
+  mat.setTranslate(-this->center.getValue());
+  tmp.setRotate(this->scaleOrientation.getValue().inverse());
+  mat.multRight(tmp);
+  tmp.setScale(this->scaleFactor.getValue());
+  mat.multRight(tmp);
+  tmp.setRotate(this->scaleOrientation.getValue());
+  mat.multRight(tmp);
+  tmp.setRotate(this->rotation.getValue());
+  mat.multRight(tmp);
+  inv = mat.inverse();
 }
 
 /*!
-  FIXME: write function documentation
+  Calculates the matrices to/from translation space.
 */
 void
 SoTransform::getTranslationSpaceMatrix(SbMatrix & mat, SbMatrix & inv) const
 {
-  COIN_STUB();
+  SbMatrix tmp;
+  mat.setTranslate(-this->center.getValue());
+  tmp.setRotate(this->scaleOrientation.getValue().inverse());
+  mat.multRight(tmp);
+  tmp.setScale(this->scaleFactor.getValue());
+  mat.multRight(tmp);
+  tmp.setRotate(this->scaleOrientation.getValue());
+  mat.multRight(tmp);
+  tmp.setRotate(this->rotation.getValue());
+  mat.multRight(tmp);
+  tmp.setTranslate(this->translation.getValue());
+  mat.multRight(tmp);
+  inv = mat.inverse();
 }
 
 /*!
-  FIXME: write function documentation
+  Premultiplies this transformation by \a mat.
 */
 void
 SoTransform::multLeft(const SbMatrix & mat)
 {
-  COIN_STUB();
+  SbMatrix matrix;
+  matrix.setTransform(this->translation.getValue(),
+                      this->rotation.getValue(),
+                      this->scaleFactor.getValue(),
+                      this->scaleOrientation.getValue(),
+                      this->center.getValue());
+
+  matrix.multLeft(mat);
+  this->setMatrix(matrix);
 }
 
 /*!
-  FIXME: write function documentation
+  Postmultiplies this transformation by \a mat.
 */
 void
 SoTransform::multRight(const SbMatrix & mat)
 {
-  COIN_STUB();
+  SbMatrix matrix;
+  matrix.setTransform(this->translation.getValue(),
+                      this->rotation.getValue(),
+                      this->scaleFactor.getValue(),
+                      this->scaleOrientation.getValue(),
+                      this->center.getValue());
+  matrix.multRight(mat);
+  this->setMatrix(matrix);
 }
 
 /*!
-  FIXME: write function documentation
+  Premultiplies this transformation by the transformation in \a nodeonright.
 */
 void
 SoTransform::combineLeft(SoTransformation * nodeonright)
 {
-  COIN_STUB();
+  SoGetMatrixAction ma(SbViewportRegion(100,100));
+  ma.apply(nodeonright);
+  this->multLeft(ma.getMatrix());
 }
 
 /*!
-  FIXME: write function documentation
+  Postmultiplies this transformation by the transformation in \a nodeonleft.
 */
 void
 SoTransform::combineRight(SoTransformation * nodeonleft)
 {
-  COIN_STUB();
+  SoGetMatrixAction ma(SbViewportRegion(100,100));
+  ma.apply(nodeonleft);
+  this->multRight(ma.getMatrix());
 }
 
 /*!
-  FIXME: write function documentation
+  Sets the fields to create a transformation equal to \a mat.
 */
 void
 SoTransform::setMatrix(const SbMatrix & mat)
-{
-  COIN_STUB();
+{  
+  SbVec3f t,s,c = this->center.getValue();
+  SbRotation r, so;
+  mat.getTransform(t,r,s,so,c);
+  
+  this->translation = t;
+  this->rotation = r;
+  this->scaleFactor = s;
+  this->scaleOrientation = so;
 }
 
-/*!
-  FIXME: write function documentation
+/*!  
+  Sets the \e center field to \a newcenter. This might affect one
+  or more of the other fields.  
 */
 void
 SoTransform::recenter(const SbVec3f & newcenter)
 {
-  COIN_STUB();
+  SbMatrix matrix;
+  matrix.setTransform(this->translation.getValue(),
+                      this->rotation.getValue(),
+                      this->scaleFactor.getValue(),
+                      this->scaleOrientation.getValue(),
+                      this->center.getValue());
+  SbVec3f t,s;
+  SbRotation r, so;
+  matrix.getTransform(t, r, s, so, newcenter);
+  this->translation = t;
+  this->rotation = r;
+  this->scaleFactor = s;
+  this->scaleOrientation = so;
+  this->center = newcenter;
 }
+
 
 // Doc from superclass.
 void
 SoTransform::doAction(SoAction * action)
 {
-#if COIN_DEBUG && 0 // debug
-  SbVec3f angle;
-  float rot;
-  rotation.getValue().getValue(angle, rot);
-  SbVec3f scaleangle;
-  float scalerotation;
-  scaleOrientation.getValue().getValue(scaleangle, scalerotation);
-  SoDebugError::post("SoTransform::doAction",
-                     "\n\ttranslation: <%f, %f, %f>"
-                     "\n\trotation: <%f, %f, %f> %f"
-                     "\n\tscaleFactor: <%f, %f, %f>"
-                     "\n\tscaleOrientation: <%f, %f, %f> %f"
-                     "\n\tcenter: <%f, %f, %f>",
-                     translation.getValue()[0],
-                     translation.getValue()[1],
-                     translation.getValue()[2],
-                     angle[0], angle[1], angle[2], rot,
-                     scaleFactor.getValue()[0],
-                     scaleFactor.getValue()[1],
-                     scaleFactor.getValue()[2],
-                     scaleangle[0], scaleangle[1], scaleangle[2],
-                     scalerotation,
-                     center.getValue()[0],
-                     center.getValue()[1],
-                     center.getValue()[2]);
-#endif // debug
-
   SbMatrix matrix;
-  matrix.setTransform(translation.getValue(),
-                      rotation.getValue(),
-                      scaleFactor.getValue(),
-                      scaleOrientation.getValue(),
-                      center.getValue());
-
+  matrix.setTransform(this->translation.getValue(),
+                      this->rotation.getValue(),
+                      this->scaleFactor.getValue(),
+                      this->scaleOrientation.getValue(),
+                      this->center.getValue());
+  
   SoModelMatrixElement::mult(action->getState(), this, matrix);
 }
 
@@ -231,7 +281,7 @@ SoTransform::doAction(SoAction * action)
 void
 SoTransform::GLRender(SoGLRenderAction * action)
 {
-  if (scaleFactor.getValue() != SbVec3f(1.0f, 1.0f, 1.0f)) {
+  if (this->scaleFactor.getValue() != SbVec3f(1.0f, 1.0f, 1.0f)) {
     SoGLNormalizeElement::setMatrixState(action->getState(), FALSE);
   }
   SoTransform::doAction((SoAction *)action);
@@ -249,13 +299,13 @@ void
 SoTransform::getMatrix(SoGetMatrixAction * action)
 {
   SbMatrix m;
-  m.setTransform(translation.getValue(),
-                 rotation.getValue(),
-                 scaleFactor.getValue(),
-                 scaleOrientation.getValue(),
-                 center.getValue());
+  m.setTransform(this->translation.getValue(),
+                 this->rotation.getValue(),
+                 this->scaleFactor.getValue(),
+                 this->scaleOrientation.getValue(),
+                 this->center.getValue());
   action->getMatrix().multLeft(m);
-
+  
   SbMatrix mi = m.inverse();
   action->getInverse().multRight(mi);
 }
