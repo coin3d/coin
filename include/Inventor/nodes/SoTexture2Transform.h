72817d759e7efdf8fd1a09681e311153487afc7c
Morten Eriksen <mortene@sim.no>
2000-07-01 08:28:03 +0000

    Fixed a bug, implemented some missing methods, some doc. Done by pederb.

--- include/Inventor/nodes/SoTexture2Transform.h
+++ include/Inventor/nodes/SoTexture2Transform.h
@@ -49,6 +49,9 @@ public:
 
 protected:
   virtual ~SoTexture2Transform();
+
+private:
+  void makeMatrix(SbMatrix & mat);
 };
 
 
--- src/nodes/SoTexture2Transform.cpp
+++ src/nodes/SoTexture2Transform.cpp
@@ -19,7 +19,7 @@
 
 /*!
   \class SoTexture2Transform SoTexture2Transform.h Inventor/nodes/SoTexture2Transform.h
-  \brief The SoTexture2Transform class ...
+  \brief The SoTexture2Transform class is used to define 2D texture transformations.
   \ingroup nodes
 
   FIXME: write class doc
@@ -27,30 +27,29 @@
 
 #include <Inventor/nodes/SoTexture2Transform.h>
 #include <Inventor/nodes/SoSubNodeP.h>
-#include <coindefs.h> // COIN_STUB()
 #include <Inventor/actions/SoGLRenderAction.h>
 #include <Inventor/actions/SoPickAction.h>
+#include <Inventor/actions/SoGetMatrixAction.h>
 #include <Inventor/elements/SoGLTextureMatrixElement.h>
 #include <Inventor/actions/SoCallbackAction.h>
 
 
 /*!
   \var SoSFVec2f SoTexture2Transform::translation
-  FIXME: write documentation for field
+  Texture coordinate translation.
 */
 /*!
   \var SoSFFloat SoTexture2Transform::rotation
-  FIXME: write documentation for field
+  Texture coordinate rotation (around z-axis, s is x-axis and t is y-axis).
 */
 /*!
   \var SoSFVec2f SoTexture2Transform::scaleFactor
-  FIXME: write documentation for field
+  Texture coordinate scale factors.
 */
 /*!
   \var SoSFVec2f SoTexture2Transform::center
-  FIXME: write documentation for field
+  Center for scale and rotation.
 */
-
 
 // *************************************************************************
 
@@ -76,11 +75,7 @@ SoTexture2Transform::~SoTexture2Transform()
 {
 }
 
-/*!
-  Does initialization common for all objects of the
-  SoTexture2Transform class. This includes setting up the
-  type system, among other things.
-*/
+// doc from parent
 void
 SoTexture2Transform::initClass(void)
 {
@@ -92,23 +87,55 @@ SoTexture2Transform::initClass(void)
 }
 
 
-/*!
-  FIXME: write function documentation
-*/
+// doc from parent
 void
 SoTexture2Transform::GLRender(SoGLRenderAction * action)
 {
   SoTexture2Transform::doAction(action);
 }
 
-/*!
-  FIXME: write doc
- */
+// doc from parent
 void
 SoTexture2Transform::doAction(SoAction *action)
 {
-  SbMatrix mat, tmp;
-  SbVec2f c = center.isIgnored() ?
+  SbMatrix mat;
+  this->makeMatrix(mat);
+  SoTextureMatrixElement::mult(action->getState(), this,
+                               mat);
+}
+
+// doc from parent
+void
+SoTexture2Transform::callback(SoCallbackAction *action)
+{
+  SoTexture2Transform::doAction(action);
+}
+
+// doc from parent
+void
+SoTexture2Transform::getMatrix(SoGetMatrixAction * action)
+{
+  SbMatrix mat;
+  this->makeMatrix(mat);
+  action->getTextureMatrix().multLeft(mat);
+  action->getTextureInverse().multRight(mat.inverse());
+}
+
+// doc from parent
+void
+SoTexture2Transform::pick(SoPickAction * action)
+{
+  SoTexture2Transform::doAction(action);
+}
+
+//
+// generate a matrix based on the fields
+//
+void
+SoTexture2Transform::makeMatrix(SbMatrix & mat)
+{
+  SbMatrix tmp;
+  SbVec2f c = this->center.isIgnored() ?
     SbVec2f(0.0f, 0.0f) :
     center.getValue();
 
@@ -116,60 +143,29 @@ SoTexture2Transform::doAction(SoAction *action)
   mat[3][0] = -c[0];
   mat[3][1] = -c[1];
 
-  SbVec2f scale = scaleFactor.getValue();
-  if (!scaleFactor.isIgnored() &&
+  SbVec2f scale = this->scaleFactor.getValue();
+  if (!this->scaleFactor.isIgnored() &&
       scale != SbVec2f(1.0f, 1.0f)) {
     tmp.makeIdentity();
     tmp[0][0] = scale[0];
     tmp[1][1] = scale[1];
-    mat.multLeft(tmp);
+    mat.multRight(tmp);
   }
-  if (!rotation.isIgnored() && rotation.getValue() != 0.0f) {
-    float cosa = (float)cos(rotation.getValue());
-    float sina = (float)sin(rotation.getValue());
+  if (!this->rotation.isIgnored() && (this->rotation.getValue() != 0.0f)) {
+    float cosa = (float)cos(this->rotation.getValue());
+    float sina = (float)sin(this->rotation.getValue());
     tmp.makeIdentity();
     tmp[0][0] = cosa;
     tmp[1][0] = -sina;
     tmp[0][1] = sina;
     tmp[1][1] = cosa;
-    mat.multLeft(tmp);
+    mat.multRight(tmp);
   }
-  c += translation.getValue();
-  if (!translation.isIgnored() &&
-      c != SbVec2f(0.0f, 0.0f)) {
+  if (!translation.isIgnored()) c+= this->translation.getValue();
+  if (c != SbVec2f(0.0f, 0.0f)) {
     tmp.makeIdentity();
     tmp[3][0] = c[0];
     tmp[3][1] = c[1];
-    mat.multLeft(tmp);
+    mat.multRight(tmp);
   }
-
-  SoTextureMatrixElement::mult(action->getState(), this,
-                               mat);
-}
-
-/*!
-  FIXME: write doc
- */
-void
-SoTexture2Transform::callback(SoCallbackAction *action)
-{
-  SoTexture2Transform::doAction(action);
-}
-
-/*!
-  FIXME: write doc
- */
-void
-SoTexture2Transform::getMatrix(SoGetMatrixAction * /* action */)
-{
-  COIN_STUB();
-}
-
-/*!
-  FIXME: write doc
- */
-void
-SoTexture2Transform::pick(SoPickAction * /* action */)
-{
-  COIN_STUB();
 }
