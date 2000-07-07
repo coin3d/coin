d59807a359257f9696cd56e99991741d2c49ea0c
Morten Eriksen <mortene@sim.no>
2000-07-07 08:07:43 +0000

    Corrected typo error in class documentation.

--- src/misc/SoOffscreenRenderer.cpp
+++ src/misc/SoOffscreenRenderer.cpp
@@ -18,8 +18,8 @@
 \**************************************************************************/
 
 /*!
-  \class SoBase SoOffscreenRenderer.h Inventor/SoOffscreenRenderer.h
-  \brief The SoOffscreenRenderer class is used for off screen rendering.
+  \class SoSoOffscreenRenderer SoOffscreenRenderer.h Inventor/SoOffscreenRenderer.h
+  \brief The SoOffscreenRenderer class is used for rendering scenes in offscreen buffers.
   \ingroup general
 
   If you want to render to a memory buffer instead of an on-screen
