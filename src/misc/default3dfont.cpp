c0f648695c14f73fe674b890d2da0e44869771bd
Morten Eriksen <mortene@sim.no>
2000-08-29 14:17:02 +0000

    Changed Free Software license from QPL to LGPL.

--- src/misc/default3dfont.cpp
+++ src/misc/default3dfont.cpp
@@ -1,20 +1,20 @@
 /**************************************************************************\
-*
-*  Copyright (C) 1998-2000 by Systems in Motion.  All rights reserved.
-*
-*  This file is part of the Coin library.
-*
-*  This file may be distributed under the terms of the Q Public License
-*  as defined by Troll Tech AS of Norway and appearing in the file
-*  LICENSE.QPL included in the packaging of this file.
-*
-*  If you want to use Coin in applications not covered by licenses
-*  compatible with the QPL, you can contact SIM to aquire a
-*  Professional Edition license for Coin.
-*
-*  Systems in Motion AS, Prof. Brochs gate 6, N-7030 Trondheim, NORWAY
-*  http://www.sim.no/ sales@sim.no Voice: +47 22114160 Fax: +47 67172912
-*
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2000 by Systems in Motion. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  version 2.1 as published by the Free Software Foundation. See the
+ *  file LICENSE.LGPL at the root directory of the distribution for
+ *  more details.
+ *
+ *  If you want to use Coin for applications not compatible with the
+ *  LGPL, please contact SIM to aquire a Professional Edition license.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
+ *  http://www.sim.no support@sim.no Voice: +47 22114160 Fax: +47 22207097
+ *
 \**************************************************************************/
 static float defaultfont_coords_33[] = {
 0.00111111f,0.631111f,
