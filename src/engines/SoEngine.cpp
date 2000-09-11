c57f1c2c3770b57311e68e43dcb264e1d6acd3e4
Morten Eriksen <mortene@sim.no>
2000-09-11 12:42:33 +0000

    Fixed a bug in SoEngine::notify() where SoEngine::inputChanged() wasn't called if SoEngine::isNotifyEnabled()==FALSE when the engine is not dirty.
    (SoEngine::inputChanged() should always be called to recalculate
    internal variables etc -- no matter the status of the notifyenabled
    flag.)

--- src/engines/SoEngine.cpp
+++ src/engines/SoEngine.cpp
@@ -288,65 +288,63 @@ SoEngine::notify(SoNotList * nl)
   this->stateflags.dirty = 0;
 #endif // debug
 
-  // FIXME: we'll need to investigate whether this optimization is valid
-  // and if it is worth it.       pederb, 20000828
-  if (this->stateflags.dirty) {
-    if (!this->isNotifying()) {
-      // "notify" engine about the changed field
-      this->stateflags.isnotifying = 1;
-      this->inputChanged(nl->getLastField());
-      this->stateflags.isnotifying = 0;
-    }
-    // nothing more to do, output connections have been notified earlier
-  }
-  else {
-    if (!this->isNotifying() && this->isNotifyEnabled()) {
-      this->stateflags.isnotifying = 1;
-      this->inputChanged(nl->getLastField());
+  // Avoid recursive notification calls.
+  if (this->isNotifying()) return;
+  this->stateflags.isnotifying = TRUE;
 
-      SoEngineOutput * output;
-      SoField * field;
-      const SoEngineOutputData * outputs = this->getOutputData();
-      int numoutputs = outputs->getNumOutputs();
-      int numconnections;
+  // Let engine know that a field changed, so we can recalculate
+  // internal variables, if necessary.
+  this->inputChanged(nl->getLastField());
+
+  // Nothing more to do, output connections have been notified
+  // earlier.
+  //
+  // FIXME: we'll need to investigate whether this optimization is
+  // valid and if it is worth it.  pederb, 20000828
+  if (this->stateflags.dirty) return;
+
+  if (this->isNotifyEnabled()) {
+    SoEngineOutput * output;
+    SoField * field;
+    const SoEngineOutputData * outputs = this->getOutputData();
+    int numoutputs = outputs->getNumOutputs();
+    int numconnections;
 
 #if COIN_DEBUG && 0 // debug
-      if (this->getTypeId().isDerivedFrom(SoFieldConverter::getClassTypeId()))
-        SoDebugError::postInfo("SoEngine::notify", "numoutputs: %d", numoutputs);
+    if (this->getTypeId().isDerivedFrom(SoFieldConverter::getClassTypeId()))
+      SoDebugError::postInfo("SoEngine::notify", "numoutputs: %d", numoutputs);
 #endif // debug
 
-      for (int i = 0; i < numoutputs; i++) {
-        output = outputs->getOutput(this, i);
-        if (output->isEnabled()) {
-          numconnections = output->getNumConnections();
+    for (int i = 0; i < numoutputs; i++) {
+      output = outputs->getOutput(this, i);
+      if (output->isEnabled()) {
+        numconnections = output->getNumConnections();
 #if COIN_DEBUG && 0 // debug
-          if (this->getTypeId().isDerivedFrom(SoFieldConverter::getClassTypeId()))
-            SoDebugError::postInfo("SoEngine::notify",
-                                   "numconnections: %d",
-                                   numconnections);
+        if (this->getTypeId().isDerivedFrom(SoFieldConverter::getClassTypeId()))
+          SoDebugError::postInfo("SoEngine::notify",
+                                 "numconnections: %d",
+                                 numconnections);
 #endif // debug
-          for (int j = 0; j < numconnections; j++) {
-            this->stateflags.dirty = 1;
-            field = (*output)[j];
+        for (int j = 0; j < numconnections; j++) {
+          this->stateflags.dirty = 1;
+          field = (*output)[j];
 #if COIN_DEBUG && 0 // debug
-            SoDebugError::postInfo("SoEngine::notify",
-                                   "field %p (%s, '%s'), dirty: %d",
-                                   field,
-                                   field->getContainer() ? field->getContainer()->getTypeId().getName().getString() : "*none*",
-                                   field->getContainer() ? field->getContainer()->getName().getString() : "*none*",
-                                   field->getDirty());
-            // Make sure notification happens _also_ when field is
-            // dirty (only valid within this debug code block).
-            if (field->getDirty()) field->notify(nl);
+          SoDebugError::postInfo("SoEngine::notify",
+                                 "field %p (%s, '%s'), dirty: %d",
+                                 field,
+                                 field->getContainer() ? field->getContainer()->getTypeId().getName().getString() : "*none*",
+                                 field->getContainer() ? field->getContainer()->getName().getString() : "*none*",
+                                 field->getDirty());
+          // Make sure notification happens _also_ when field is
+          // dirty (only valid within this debug code block).
+          if (field->getDirty()) field->notify(nl);
 #endif // debug
-            // FIXME: we'll need to investigate whether this
-            // optimization is valid and if it is worth it.  pederb,
-            // 20000828
-            if (!field->getDirty()) field->notify(nl);
-          }
+          // FIXME: we'll need to investigate whether this
+          // optimization is valid and if it is worth it.  pederb,
+          // 20000828
+          if (!field->getDirty()) field->notify(nl);
         }
       }
-      this->stateflags.isnotifying = 0;
     }
   }
 
@@ -357,6 +355,8 @@ SoEngine::notify(SoNotList * nl)
                          this->getTypeId().getName().getString(),
                          this->getName().getString());
 #endif // debug
+
+  this->stateflags.isnotifying = FALSE;
 }
 
 /*!
