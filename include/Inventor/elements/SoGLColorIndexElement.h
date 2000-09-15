8fa8c43969d07b32fade8d4c128518356caabcbc
Morten Eriksen <mortene@sim.no>
2000-09-15 15:00:35 +0000

    Color index element implementation, by pederb (forgot to add these when the rest of the implementation was committed).

---
+++ include/Inventor/elements/SoGLColorIndexElement.h
@@ -0,0 +1,54 @@
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2000 by Systems in Motion. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  version 2.1 as published by the Free Software Foundation. See the
+ *  file LICENSE.LGPL at the root directory of the distribution for
+ *  more details.
+ *
+ *  If you want to use Coin for applications not compatible with the
+ *  LGPL, please contact SIM to acquire a Professional Edition license.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
+ *  http://www.sim.no support@sim.no Voice: +47 22114160 Fax: +47 22207097
+ *
+\**************************************************************************/
+
+#ifndef COIN_SOGLCOLORINDEXELEMENT_H
+#define COIN_SOGLCOLORINDEXELEMENT_H
+
+#include <Inventor/elements/SoReplacedElement.h>
+
+class COIN_DLL_EXPORT SoGLColorIndexElement : public SoReplacedElement {
+  typedef SoReplacedElement inherited;
+
+  SO_ELEMENT_HEADER(SoGLColorIndexElement);
+public:
+  static void initClass(void);
+protected:
+  virtual ~SoGLColorIndexElement();
+
+public:
+  static SbBool isColorIndexMode(SoState * state);
+
+  virtual void init(SoState * state);
+  virtual void push(SoState * state);
+
+  static void set(SoState * const state, SoNode * const node,
+                  const int32_t numindices, const int32_t * const indices);
+
+  static const SoGLColorIndexElement * getInstance(SoState *state);
+
+  int32_t getNum(void) const;
+  void send(const int index);
+
+private:
+  int numindices;
+  const int32_t * indices;
+  SbBool colorindexmode;
+};
+
+#endif // !COIN_SOGLCOLORINDEXELEMENT_H
---
+++ src/elements/SoGLColorIndexElement.cpp
@@ -0,0 +1,129 @@
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2000 by Systems in Motion. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  version 2.1 as published by the Free Software Foundation. See the
+ *  file LICENSE.LGPL at the root directory of the distribution for
+ *  more details.
+ *
+ *  If you want to use Coin for applications not compatible with the
+ *  LGPL, please contact SIM to acquire a Professional Edition license.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
+ *  http://www.sim.no support@sim.no Voice: +47 22114160 Fax: +47 22207097
+ *
+\**************************************************************************/
+
+/*!
+  \class SoGLColorIndexElement Inventor/elements/SoGLColorIndexElement.h
+  \brief The SoGLColorIndexElement class is used when rendering in color index mode.
+
+*/
+
+#include <Inventor/elements/SoGLColorIndexElement.h>
+#include <Inventor/misc/SoState.h>
+#include <assert.h>
+#ifdef _WIN32
+#include <windows.h> /* needed for gl.h */
+#endif // _WIN32
+#include <GL/gl.h>
+
+SO_ELEMENT_SOURCE(SoGLColorIndexElement);
+
+// doc in parent
+void
+SoGLColorIndexElement::initClass(void)
+{
+  SO_ELEMENT_INIT_CLASS(SoGLColorIndexElement, inherited);
+}
+
+
+// doc in parent
+void
+SoGLColorIndexElement::init(SoState * /* state */)
+{
+  this->indices = NULL;
+  this->numindices = 0;
+
+  GLboolean rgba;
+  glGetBooleanv(GL_RGBA_MODE, &rgba);
+  this->colorindexmode = ! rgba;
+}
+
+// doc in parent
+void
+SoGLColorIndexElement::push(SoState * state)
+{
+  inherited::push(state);
+  // need to copy the colorindexmode member
+  SoGLColorIndexElement * elem =
+    (SoGLColorIndexElement *)this->next;
+  elem->colorindexmode = this->colorindexmode;
+}
+
+/*!
+  The destructor.
+*/
+SoGLColorIndexElement::~SoGLColorIndexElement()
+{
+}
+
+/*!
+  Returns TRUE if the current GL context is in color index mode.
+*/
+SbBool
+SoGLColorIndexElement::isColorIndexMode(SoState * state)
+{
+  // use state->getConstElement() instead of SoElement::getConstElement()
+  // to avoid cache dependencies.
+  const SoGLColorIndexElement * element = (SoGLColorIndexElement*)
+    state->getConstElement(classStackIndex);
+  return element->colorindexmode;
+}
+
+/*!
+  Sets current color indices.
+*/
+void
+SoGLColorIndexElement::set(SoState * const state, SoNode * const node,
+                           const int32_t numindices,
+                           const int32_t * const indices)
+{
+  SoGLColorIndexElement * elem = (SoGLColorIndexElement*)
+    SoReplacedElement::getElement(state, classStackIndex, node);
+  elem->indices = indices;
+  elem->numindices = numindices;
+}
+
+
+/*!
+  Returns number of color indices in element.
+*/
+int32_t
+SoGLColorIndexElement::getNum(void) const
+{
+  return this->numindices;
+}
+
+/*!
+  Sends color index \a index to OpenGL.
+*/
+void
+SoGLColorIndexElement::send(const int index)
+{
+  assert(index < this->numindices);
+  glIndexi((GLint) this->indices[index]);
+}
+
+/*!
+  Returns the current element.
+*/
+const SoGLColorIndexElement *
+SoGLColorIndexElement::getInstance(SoState *state)
+{
+  return (const SoGLColorIndexElement *)
+    SoElement::getConstElement(state, classStackIndex);
+}
