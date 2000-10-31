9f3abdd7290b8283b676667802483988f70d0573
Morten Eriksen <mortene@sim.no>
2000-10-31 14:03:25 +0000

    Autogeneration of the Installshield spec file.

--- build/geniwz.pl
+++ build/geniwz.pl
@@ -8,17 +8,29 @@ use strict;
 
 ### Globals ########################################################
 
-# Arguments are list of header files.
+# Detect if we have ``cygpath'' available.
+my $CYGPATH = "cygpath -w";
+`$CYGPATH /bin > /dev/null 2>&1`;
+if ($? != 0) { $CYGPATH="echo"; }
+
+# 1st arg.
+my $VERSION=$ARGV[0];
+shift(@ARGV);
+# 2nd arg.
+my $DLLFILE=`$CYGPATH $ARGV[0]`;
+chomp($DLLFILE);
+shift(@ARGV);
+# 3rd arg.
+my $LIBFILE=`$CYGPATH $ARGV[0]`;
+chomp($LIBFILE);
+shift(@ARGV);
+
+# Remaining arguments is a list of all the header files.
 my @INC_FILES = @ARGV;
 
 # Global hashish for header file groups.
 my %INCDATA;
 my ($NRBYTES, $NRFILES, $FILES) = (1, 2, 3);
-
-# Detect if we have ``cygpath'' available.
-my $CYGPATH = "cygpath -w";
-`$CYGPATH /bin > /dev/null 2>&1`;
-if ($? != 0) { $CYGPATH="echo"; }
 
 ### Process header files ###########################################
 
@@ -41,8 +53,111 @@ foreach (@INC_FILES) {
 
 ### Dump Installshield template on stdout ##########################
 
+print "
+[InstallShield Wizard]
+iDate=0
+iTime=0
+Flag=fe000000
+ISX.EXE Size=679936
+ISX.EXE Date=11:21:52AM  4/8/1999
+ISX.EXE Ver=2.1.2.62
+SETUP.EXE Size=45312
+SETUP.EXE Date=9:12:24AM  3/23/1999
+SETUP.EXE Ver=3.0.111.0
+SETUP.INS Size=81342
+SETUP.INS Date=11:26:40AM  4/8/1999
+SETUP.INS Ver=Not available
+_INST16.EX_ Size=283522
+_INST16.EX_ Date=9:12:22AM  3/23/1999
+_INST16.EX_ Ver=Not available
+_INST32I.EX_ Size=294079
+_INST32I.EX_ Date=9:12:22AM  3/23/1999
+_INST32I.EX_ Ver=Not available
+ISDEPEND.INI Size=294079
+ISDEPEND.INI Date=9:12:22AM  3/23/1999
+ISDEPEND.INI Ver=Not available
+SWDEPEND.INI Size=294079
+SWDEPEND.INI Date=9:12:22AM  3/23/1999
+SWDEPEND.INI Ver=Not available
+ICOMP.EXE Size=119808
+ICOMP.EXE Date=9:12:24AM  3/23/1999
+ICOMP.EXE Ver=3.00.062
+SPLIT.EXE Size=90624
+SPLIT.EXE Date=9:12:24AM  3/23/1999
+SPLIT.EXE Ver=3.00.060
+PACKLIST.EXE Size=87552
+PACKLIST.EXE Date=9:12:24AM  3/23/1999
+PACKLIST.EXE Ver=3.00.060
+Version=2.12
+DevTool=Generic Windows
+Platform=Win32
+TabsVisit=1111111111111111100011
+LangNum=451
+LangDir=LANG\\ENG\\
+
+[VisualDesign]
+AppName=Coin v$VERSION
+AppExe=
+AppExeFile=
+Version=$VERSION
+Company=Systems in Motion
+Title=Coin v$VERSION Installation
+TitleType=1
+BackgrndBmp=
+BackgrndAlign=4
+Backgrnd=1
+BackgrndColor=10
+Uninstall=1
+UninstallChecked=1
+Silent=1
+SmsMode=0
+
+[Types]
+Types=1
+Type1Components=1
+Type1Name=Complete
+Type1ComponentList=1 
+";
+
 my @SUBDIRS = keys(%INCDATA);
-my $groupnr = 1;
+my $numgroups = scalar(@SUBDIRS) + 2;
+
+print "
+[Components]
+Components=1
+Component1Groups=$numgroups
+Component1Name=Coin Library SDK
+Component1Description=Execution environment and development
+Component1GroupList=";
+
+my $i = 1;
+while ($i <= $numgroups) { print "$i "; $i++; }
+
+my $dllsize = (-e $DLLFILE) ? (-s $DLLFILE) : 0;
+my $libsize = (-e $LIBFILE) ? (-s $LIBFILE) : 0;
+
+print "
+
+[Groups]
+Groups=$numgroups
+Group1Size=$dllsize
+Group1Files=1
+Group1Name=DLL
+Group1Dir=<WINSYSDIR>
+Group1Update=0
+Group1TargetOS=-1
+Group1File1=$DLLFILE
+Group2Size=$libsize
+Group2Files=1
+Group2Name=LIB
+Group2Dir=<INSTALLDIR>\\libs
+Group2Update=0
+Group2TargetOS=-1
+Group2File1=$LIBFILE
+";
+
+
+my $groupnr = 3;
 foreach (@SUBDIRS) {
     print "Group${groupnr}Size=$INCDATA{$_}{$NRBYTES}\n";
 
@@ -71,6 +186,17 @@ foreach (@SUBDIRS) {
 
     $groupnr++;
 }
+
+print "
+[Sequence]
+DestinationLocationDir=<ProgramFilesDir>\\SIM\\Coin
+SelectProgramFolderName=Coin
+DestinationLocation=1
+ProgressBar=1
+
+[Uninstall]
+UserUninstall=0
+";
 
 ### Successful completion ##########################################
 
