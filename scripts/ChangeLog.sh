9e616aa727d6ea0eed0be70bde406da35c349791
Morten Eriksen <mortene@sim.no>
2001-01-05 10:55:41 +0000

    Fixes for correct ChangeLog generation for 2001.

--- scripts/ChangeLog.sh
+++ scripts/ChangeLog.sh
@@ -26,7 +26,7 @@ ENDOFHEADER
 
 cvs update -dP
 
-cvs log -d">= 1 January 2000" | $cvs2cl --stdin --header $headerfile --separate-header --prune \
+cvs log -d">= 1 January 2001" | $cvs2cl --stdin --header $headerfile --separate-header --prune \
   --ignore 'ChangeLog$' \
   --ignore '(Makefile\.in|configure|aclocal\.m4|config\.sub|config\.guess)$' \
   --ignore '(ltconfig|ltmain\.sh|missing|mkinstalldirs|stamp-h.*|install-sh)$' \
