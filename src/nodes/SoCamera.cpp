3402811735d269655530d2ab895f886c41e51db3
Peder Blekken <pederb@sim.no>
2001-02-15 20:23:49 +0000

    Implemented getViewportBounds().

--- src/nodes/SoCamera.cpp
+++ src/nodes/SoCamera.cpp
@@ -46,7 +46,6 @@
 
 #include <Inventor/nodes/SoCamera.h>
 #include <Inventor/nodes/SoSubNodeP.h>
-#include <coindefs.h> // COIN_STUB()
 #include <Inventor/actions/SoCallbackAction.h>
 #include <Inventor/actions/SoGLRenderAction.h>
 #include <Inventor/actions/SoGetBoundingBoxAction.h>
@@ -353,8 +352,28 @@ SoCamera::viewAll(SoPath * const path, const SbViewportRegion & vpregion,
 SbViewportRegion
 SoCamera::getViewportBounds(const SbViewportRegion & region) const
 {
-  COIN_STUB(); // FIXME
-  return SbViewportRegion();
+  SbViewportRegion vp = region;
+  switch (this->viewportMapping.getValue()) {
+  case CROP_VIEWPORT_FILL_FRAME:
+  case CROP_VIEWPORT_LINE_FRAME:
+  case CROP_VIEWPORT_NO_FRAME:
+    {
+      float vpaspect = region.getViewportAspectRatio();
+      float camaspect = this->aspectRatio.getValue();
+      if (vpaspect > camaspect) {
+        vp.scaleWidth(camaspect / vpaspect);
+        return vp;
+      }
+      else if (vpaspect < camaspect) {
+        vp.scaleHeight(vpaspect / camaspect);
+      }
+    }
+    break;
+  default:
+    // do nothing
+    break;
+  }
+  return vp;
 }
 
 // Doc in superclass.
@@ -452,87 +471,87 @@ SoCamera::jitter(int numpasses, int curpass, const SbViewportRegion & vpreg,
                  SbVec3f & jitteramount) const
 {
   // jitter values from OpenGL Programming Guide
-  static float jitter2[] = { 
+  static float jitter2[] = {
     0.25f, 0.77f,
-    0.75f, 0.25f 
+    0.75f, 0.25f
   };
   static float jitter3[] = {
-    0.5033922635f, 0.8317967229f, 
-    0.7806016275f, 0.2504380877f, 
+    0.5033922635f, 0.8317967229f,
+    0.7806016275f, 0.2504380877f,
     0.2261828938f, 0.4131553612f
   };
   static float jitter4[] = {
-    0.375f, 0.25f, 
-    0.125f, 0.75f, 
-    0.875f, 0.25f, 
+    0.375f, 0.25f,
+    0.125f, 0.75f,
+    0.875f, 0.25f,
     0.625f, 0.75f
   };
   static float jitter5[] = {
-    0.5f, 0.5f, 
-    0.3f, 0.1f, 
-    0.7f, 0.9f, 
-    0.9f, 0.3f, 
+    0.5f, 0.5f,
+    0.3f, 0.1f,
+    0.7f, 0.9f,
+    0.9f, 0.3f,
     0.1f, 0.7f
   };
   static float jitter6[] = {
-    0.4646464646f, 0.4646464646f, 
-    0.1313131313f, 0.7979797979f, 
-    0.5353535353f, 0.8686868686f, 
-    0.8686868686f, 0.5353535353f, 
+    0.4646464646f, 0.4646464646f,
+    0.1313131313f, 0.7979797979f,
+    0.5353535353f, 0.8686868686f,
+    0.8686868686f, 0.5353535353f,
     0.7979797979f, 0.1313131313f,
     0.2020202020f, 0.2020202020f
   };
   static float jitter8[] = {
-    0.5625f, 0.4375f, 
-    0.0625f, 0.9375f, 
-    0.3125f, 0.6875f, 
-    0.6875f, 0.8125f, 
-    0.8125f, 0.1875f, 
+    0.5625f, 0.4375f,
+    0.0625f, 0.9375f,
+    0.3125f, 0.6875f,
+    0.6875f, 0.8125f,
+    0.8125f, 0.1875f,
     0.9375f, 0.5625f,
-    0.4375f, 0.0625f, 
-    0.1875f, 0.3125f 
+    0.4375f, 0.0625f,
+    0.1875f, 0.3125f
   };
   static float jitter9[] = {
-    0.5f, 0.5f, 
-    0.1666666666f, 0.9444444444f, 
-    0.5f, 0.1666666666f,  
-    0.5f, 0.8333333333f, 
-    0.1666666666f, 0.2777777777f,  
+    0.5f, 0.5f,
+    0.1666666666f, 0.9444444444f,
+    0.5f, 0.1666666666f,
+    0.5f, 0.8333333333f,
+    0.1666666666f, 0.2777777777f,
     0.8333333333f, 0.3888888888f,
-    0.1666666666f, 0.6111111111f, 
-    0.8333333333f, 0.7222222222f, 
+    0.1666666666f, 0.6111111111f,
+    0.8333333333f, 0.7222222222f,
     0.8333333333f, 0.0555555555f
   };
   static float jitter12[] = {
-    0.4166666666f, 0.625f, 
-    0.9166666666f, 0.875f, 
-    0.25f, 0.375f, 
-    0.4166666666f, 0.125f, 
-    0.75f, 0.125f, 
-    0.0833333333f, 0.125f, 
-    0.75f, 0.625f, 
-    0.25f, 0.875f, 
-    0.5833333333f, 0.375f, 
-    0.9166666666f, 0.375f, 
-    0.0833333333f, 0.625f, 
+    0.4166666666f, 0.625f,
+    0.9166666666f, 0.875f,
+    0.25f, 0.375f,
+    0.4166666666f, 0.125f,
+    0.75f, 0.125f,
+    0.0833333333f, 0.125f,
+    0.75f, 0.625f,
+    0.25f, 0.875f,
+    0.5833333333f, 0.375f,
+    0.9166666666f, 0.375f,
+    0.0833333333f, 0.625f,
     0.583333333f, 0.875f
   };
   static float jitter16[] = {
-    0.375f, 0.4375f, 
-    0.625f, 0.0625f, 
-    0.875f, 0.1875f, 
-    0.125f, 0.0625f,  
-    0.375f, 0.6875f, 
-    0.875f, 0.4375f, 
-    0.625f, 0.5625f, 
-    0.375f, 0.9375f,  
-    0.625f, 0.3125f, 
-    0.125f, 0.5625f, 
-    0.125f, 0.8125f, 
-    0.375f, 0.1875f,  
-    0.875f, 0.9375f, 
-    0.875f, 0.6875f, 
-    0.125f, 0.3125f, 
+    0.375f, 0.4375f,
+    0.625f, 0.0625f,
+    0.875f, 0.1875f,
+    0.125f, 0.0625f,
+    0.375f, 0.6875f,
+    0.875f, 0.4375f,
+    0.625f, 0.5625f,
+    0.375f, 0.9375f,
+    0.625f, 0.3125f,
+    0.125f, 0.5625f,
+    0.125f, 0.8125f,
+    0.375f, 0.1875f,
+    0.875f, 0.9375f,
+    0.875f, 0.6875f,
+    0.125f, 0.3125f,
     0.625f, 0.8125f
   };
 
@@ -556,11 +575,11 @@ SoCamera::jitter(int numpasses, int curpass, const SbViewportRegion & vpreg,
 
   // FIXME: support more rendering passes by generating jitter tables
   // using some clever algorithm.
-  if (numpasses > 16) numpasses = 16; 
+  if (numpasses > 16) numpasses = 16;
   if (curpass >= numpasses) curpass = numpasses - 1;
-  
+
   float * jittab = jittertab[numpasses-2];
-  
+
   SbVec2s vpsize = vpreg.getViewportSizePixels();
   jitteramount[0] = jittab[curpass*2] * 2.0f / float(vpsize[0]);
   jitteramount[1] = jittab[curpass*2+1] * 2.0f / float(vpsize[1]);
