9323a1827d598d84410da92bf87577559a3fda4c
Morten Eriksen <mortene@sim.no>
2001-02-26 09:35:05 +0000

    Prefer "" over \x0 in SO_KIT_ADD_CATALOG_LIST_ENTRY to avoid a warning from SGI MipsPro CC.

--- src/nodekits/SoBaseKit.cpp
+++ src/nodekits/SoBaseKit.cpp
@@ -150,7 +150,7 @@ SoBaseKit::SoBaseKit(void)
   }
 
 
-  SO_KIT_ADD_CATALOG_LIST_ENTRY(callbackList, SoSeparator, TRUE, this, \x0, SoCallback, TRUE);
+  SO_KIT_ADD_CATALOG_LIST_ENTRY(callbackList, SoSeparator, TRUE, this, "", SoCallback, TRUE);
   SO_KIT_ADD_LIST_ITEM_TYPE(callbackList, SoEventCallback);
 
   // this could be created on demand, but will make it more complicated
