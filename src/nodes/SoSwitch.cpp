b1012ef0001047e14821587fb00c5c744af318ca
Peder Blekken <pederb@sim.no>
2001-04-02 15:01:04 +0000

    Fixed buggy SoSwitch IN_PATH traversal code. Bug reported by Jacob Storen.

--- src/nodes/SoSwitch.cpp
+++ src/nodes/SoSwitch.cpp
@@ -133,56 +133,7 @@ SoSwitch::GLRender(SoGLRenderAction * action)
 void
 SoSwitch::getBoundingBox(SoGetBoundingBoxAction * action)
 {
-  // FIXME: update this method to use a doAction() kind of
-  // traversal. This traversal code is not correct i think.
-  // pederb, 990618
-
-  int numIndices;
-  const int * indices;
-  int idx;
-
-  if (action->getPathCode(numIndices, indices) == SoAction::IN_PATH)
-    idx = indices[numIndices-1];
-  else
-    idx = this->whichChild.getValue();
-
-  if (idx == SO_SWITCH_INHERIT) idx = SoSwitchElement::get(action->getState());
-  else SoSwitchElement::set(action->getState(), idx);
-
-  if (idx == SO_SWITCH_NONE) return;
-
-  if (! ((idx < this->getNumChildren() && idx >= 0) || idx == SO_SWITCH_ALL)) {
-#if COIN_DEBUG
-    SoDebugError::post("SoSwitch::getBoundingBox",
-                       "invalid whichChild value (%d) or switch index (%d)",
-                       this->whichChild.getValue(), idx);
-    this->whichChild = SO_SWITCH_NONE;
-#endif // COIN_DEBUG
-    return;
-  }
-
-  if (idx == SO_SWITCH_ALL) {
-    // Initialize accumulation variables.
-    SbVec3f acccenter(0.0f, 0.0f, 0.0f);
-    int numCenters = 0;
-
-    for (int i = 0; i < this->getNumChildren(); i++) {
-      this->getChildren()->traverse(action, i);
-
-      // If center point is set, accumulate.
-      if (action->isCenterSet()) {
-        acccenter += action->getCenter();
-        numCenters++;
-        action->resetCenter();
-      }
-    }
-
-    if (numCenters != 0)
-      action->setCenter(acccenter / float(numCenters), FALSE);
-  }
-  else {
-    this->getChildren()->traverse(action, idx);
-  }
+  SoSwitch::doAction(action);
 }
 
 /*!
@@ -197,11 +148,11 @@ SoSwitch::search(SoSearchAction * action)
   SoNode::search(action);
   if (action->isFound()) return;
 
-  // TRAVERSE
-  if (action->isSearchingAll()) { // traverse all children no matter
-    getChildren()->traverse(action);
-  } else {
-    doAction(action);
+  if (action->isSearchingAll()) {
+    this->children->traverse(action);
+  } 
+  else {
+    SoSwitch::doAction(action);
   }
 }
 
@@ -211,58 +162,80 @@ SoSwitch::search(SoSearchAction * action)
 void
 SoSwitch::doAction(SoAction * action)
 {
-  SoState *state = action->getState();
-
-  int numIndices;
-  const int *indices;
-  int idx;
-
-  idx = whichChild.getValue();
-  SbBool inheritIdx = FALSE;
+  SoState * state = action->getState();
+  int idx = this->whichChild.isIgnored() ? 
+    SO_SWITCH_NONE : this->whichChild.getValue();
   if (idx == SO_SWITCH_INHERIT) {
-    inheritIdx = TRUE;
     idx = SoSwitchElement::get(action->getState());
+    // when we inherit, idx might be out of range. Use modulo.
+    if (idx >= this->getNumChildren()) idx %= this->getNumChildren();
   }
-
-  int startIdx, endIdx;
-
-  switch (action->getPathCode(numIndices, indices)) {
-  case SoAction::IN_PATH:
-    startIdx = 0;
-    endIdx = indices[numIndices-1];
-    break;
-  case SoAction::OFF_PATH:
-  case SoAction::NO_PATH:
-  case SoAction::BELOW_PATH:
-    if (idx == SO_SWITCH_ALL) {
-      startIdx = 0;
-      endIdx = this->getNumChildren() - 1;
-    }
-    else if (idx < 0) {
-      startIdx = 0;
-      endIdx = -1; // don't traverse
-    }
-    else {
-      startIdx = endIdx = idx;
-    }
-    break;
-  default:
-    startIdx = 0;
-    endIdx = -1;
-    assert(0 && "Unknown path code");
+  else {
+    SoSwitchElement::set(state, idx);
   }
-  for (int i = startIdx; i <= endIdx; i++) {
-    if (!inheritIdx) SoSwitchElement::set(state, this, i);
-    if (i < 0 || i >= this->getNumChildren()) {
+  
+  int numindices;
+  const int * indices;
+  SoAction::PathCode pathcode = action->getPathCode(numindices, indices);
+  
+  if (idx == SO_SWITCH_ALL) {
+    if (action->isOfType(SoGetBoundingBoxAction::getClassTypeId())) {
+      // calculate center of bbox if bboxaction. This makes the
+      // switch node behave exactly like a group node
+      SoGetBoundingBoxAction * bbaction = (SoGetBoundingBoxAction*) action;
+      // Initialize accumulation variables.
+      SbVec3f acccenter(0.0f, 0.0f, 0.0f);
+      int numcenters = 0;
+      // only traverse nodes in path(s) for IN_PATH traversal
+      int n = pathcode == SoAction::IN_PATH ? numindices : this->getNumChildren();
+      
+      for (int i = 0; i < n; i++) {
+        this->children->traverse(bbaction, 
+                                 pathcode == SoAction::IN_PATH ? indices[i] : i);
+          
+        // If center point is set, accumulate.
+        if (bbaction->isCenterSet()) {
+          acccenter += bbaction->getCenter();
+          numcenters++;
+          bbaction->resetCenter();
+        }
+      }
+      if (numcenters != 0) {
+        bbaction->setCenter(acccenter / float(numcenters), FALSE);
+      }
+    }
+    else { // not a getBoundingBoxAction
+      if (pathcode == SoAction::IN_PATH) {
+        this->children->traverseInPath(action, numindices, indices);
+      }
+      else {
+        this->children->traverse(action);
+      }
+    }
+  }
+  else if (idx >= 0) { // should only traverse one child
+    if (pathcode == SoAction::IN_PATH) {
+      // traverse only if one path matches idx
+      for (int i = 0; i < numindices; i++) {
+        if (indices[i] == idx) {
+          this->children->traverse(action, idx);
+          break;
+        }
+      }
+    }
+    else { // off, below or no path traversal
+      // be robust for index out of range
+      if (idx >= this->getNumChildren()) {
 #if COIN_DEBUG
-      SoDebugError::post("SoSwitch::doAction",
-                         "invalid whichChild value (%d) or switch index (%d)",
-                         this->whichChild.getValue(), i);
-      this->whichChild = SO_SWITCH_NONE;
+        SoDebugError::post("SoSwitch::doAction",
+                           "whichChild %d out of range (0-%d).",
+                           idx, this->getNumChildren());
 #endif // COIN_DEBUG
-      return;
+      }
+      else {
+        this->children->traverse(action, idx);
+      }
     }
-    this->children->traverse(action, i);
   }
 }
 
@@ -272,7 +245,7 @@ SoSwitch::doAction(SoAction * action)
 SbBool
 SoSwitch::affectsState(void) const
 {
-  int idx = whichChild.getValue();
+  int idx = this->whichChild.getValue();
   if (idx == SO_SWITCH_NONE) return FALSE;
   if (idx >= this->getNumChildren()) return FALSE;
   if (idx >= 0 && !this->getChild(idx)->affectsState()) return FALSE;
