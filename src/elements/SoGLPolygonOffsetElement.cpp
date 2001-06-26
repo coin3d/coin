5f6bc73b562e1190f24086f0350b9aca24616c4f
Peder Blekken <pederb@sim.no>
2001-06-26 13:02:47 +0000

    Fix for polygon offset on the SGI platform.

--- src/elements/SoGLPolygonOffsetElement.cpp
+++ src/elements/SoGLPolygonOffsetElement.cpp
@@ -36,6 +36,7 @@
 #include <GL/gl.h>
 
 #include <assert.h>
+#include <stdlib.h>
 
 SO_ELEMENT_SOURCE(SoGLPolygonOffsetElement);
 
@@ -125,77 +126,100 @@ SoGLPolygonOffsetElement::isLazy(void) const
   return TRUE;
 }
 
-
 //! FIXME: write doc.
 
 void
 SoGLPolygonOffsetElement::updategl(void)
 {
-#if GL_EXT_polygon_offset && !GL_VERSION_1_1
+  // To make the code below a bit more readable, we always use the
+  // COIN_SGI_USE_GLPOLYGONOFFSETEXT varaible, even on non-sgi systems.
+  // It's then just set to the correct value, based on OpenGL 1.1
+  // detection.
+
+  // The reason for all this mess is that some SGI OpenGL
+  // implementations report OpenGL 1.1 without supporting
+  // glPolygonOffset 100%. And, we generally don't want to link agains
+  // glPolygonOffsetEXT if OpenGL 1.1 is detected, since this method
+  // does not exist on most OpenGL 1.1 drivers (e.g. NVidia).
+
+  // Therefore, we only compile in glPolygonOffsetEXT code if OpenGL
+  // 1.1 is not detected, or if we're compiling on the SGI
+  // platform. SGI are not likely to drop support for
+  // glPolygonOffsetEXT since it is a frequently used extension.
+  // pederb, 2001-06-26
+
+  static int COIN_SGI_USE_GLPOLYGONOFFSETEXT = -1;
+  if (COIN_SGI_USE_GLPOLYGONOFFSETEXT < 0) {
+#if !GL_VERSION_1_1 // if not GL 1.1, always use EXT
+    COIN_SGI_USE_GLPOLYGONOFFSETEXT = 1;
+#else // GL_VERSION_1_1
+    COIN_SGI_USE_GLPOLYGONOFFSETEXT = 0; // normally, use OpenGL 1.1
+    // only test environment variable on the sgi platform
+#ifdef __sgi
+    char * env = getenv("COIN_SGI_USE_GLPOLYGONOFFSETEXT");
+    if (env) COIN_SGI_USE_GLPOLYGONOFFSETEXT = atoi(env);
+#endif // __sgi
+#endif // GL_VERSION_1_1
+  }
+
+#if GL_EXT_polygon_offset
   static int polygon_offset_ext_id = -1;
   if (polygon_offset_ext_id == -1) {
     polygon_offset_ext_id =
       SoGLCacheContextElement::getExtID("GL_EXT_polygon_offset");
   }
-#endif GL_EXT_polygon_offset && ! GL_VERSION_1_1
+#endif // GL_EXT_polygon_offset
 
   if (this->active) {
 #if GL_VERSION_1_1
-    if ((this->style & FILLED) && !(this->currentstyles & FILLED))
-      glEnable(GL_POLYGON_OFFSET_FILL);
-    if ((this->style & LINES) && !(this->currentstyles & LINES))
-      glEnable(GL_POLYGON_OFFSET_LINE);
-    if ((this->style & POINTS) && !(this->currentstyles & POINTS))
-      glEnable(GL_POLYGON_OFFSET_POINT);
-    glPolygonOffset(this->currentoffsetfactor,
-                    this->currentoffsetunits);
-#ifdef __sgi
-    // glPolygonOffset() is Buggy on some IRIX GL-implementations that
-    // reports OpenGL 1.1 support, but only has partial
-    // support. Nothing dangerous, but causes GL errors/warning. Just
-    // clear GL errors for now.  Problem reported by
-    // Jacob@fedem.com. pederb 2001-06-25
-    while (glGetError() != GL_NO_ERROR);
-#endif // __sgi
-    
-#elif GL_EXT_polygon_offset
-    if (SoGLCacheContextElement::extSupported(this->state, polygon_offset_ext_id)) {
+    if (!COIN_SGI_USE_GLPOLYGONOFFSETEXT) {
+      if ((this->style & FILLED) && !(this->currentstyles & FILLED))
+        glEnable(GL_POLYGON_OFFSET_FILL);
+      if ((this->style & LINES) && !(this->currentstyles & LINES))
+        glEnable(GL_POLYGON_OFFSET_LINE);
+      if ((this->style & POINTS) && !(this->currentstyles & POINTS))
+        glEnable(GL_POLYGON_OFFSET_POINT);
+      glPolygonOffset(this->currentoffsetfactor,
+                      this->currentoffsetunits);
+    }
+#endif // GL_VERSION_1_1
+
+#if GL_EXT_polygon_offset && (!GL_VERSION_1_1 || defined(__sgi))
+    if (COIN_SGI_USE_GLPOLYGONOFFSETEXT &&
+        SoGLCacheContextElement::extSupported(this->state, polygon_offset_ext_id)) {
       // FIXME: this value (0.0000001) a hack to make it look
       // ok on old SGI HW
-      
+
       // try to detect if user attempted to specify a bias, and not units
-      SbBool isbias = this->currentOffsetunits > 0.0f && this->currentOffsetunits < 0.01f;
+      SbBool isbias = this->currentoffsetunits > 0.0f && this->currentoffsetunits < 0.01f;
       glPolygonOffsetEXT(this->currentoffsetfactor,
                          isbias ? this->currentoffsetunits : 0.000001);
-      if ((this->styles & FILLED) && !(this->currentstyles & FILLED)) {
+      if ((this->style & FILLED) && !(this->currentstyles & FILLED)) {
         glEnable(GL_POLYGON_OFFSET_EXT);
       }
     }
-#endif // GL_EXT_polygon_offset
+#endif // GL_EXT_polygon_offset && (!GL_VERSION_1_1 || defined(__sgi)
     this->currentstyles = this->style;
   }
   else { // ! active
 #if GL_VERSION_1_1
-    if (this->currentstyles & FILLED) 
-      glDisable(GL_POLYGON_OFFSET_FILL);
-    if (this->currentstyles & LINES)
-      glDisable(GL_POLYGON_OFFSET_LINE);
-    if (this->currentstyles & POINTS)
-      glDisable(GL_POLYGON_OFFSET_POINT);
-#ifdef __sgi
-    // glPolygonOffset() is Buggy on some IRIX GL-implementations that
-    // reports OpenGL 1.1 support, but only has partial
-    // support. Nothing dangerous, but causes GL errors/warning. Just
-    // clear GL errors for now.  Problem reported by
-    // Jacob@fedem.com. pederb 2001-06-25
-    while (glGetError() != GL_NO_ERROR);
-#endif // __sgi
-#elif GL_EXT_polygon_offset
-    if (SoGLCacheContextElement::extSupported(this->state, polygon_offset_ext_id)) {
+    if (!COIN_SGI_USE_GLPOLYGONOFFSETEXT) {
+      if (this->currentstyles & FILLED)
+        glDisable(GL_POLYGON_OFFSET_FILL);
+      if (this->currentstyles & LINES)
+        glDisable(GL_POLYGON_OFFSET_LINE);
+      if (this->currentstyles & POINTS)
+        glDisable(GL_POLYGON_OFFSET_POINT);
+    }
+#endif // GL_VERSION_1_1
+
+#if GL_EXT_polygon_offset && (!GL_VERSION_1_1 || defined(__sgi))
+    if (COIN_SGI_USE_GLPOLYGONOFFSETEXT &&
+        SoGLCacheContextElement::extSupported(this->state, polygon_offset_ext_id)) {
       if (this->currentstyles & FILLED)
         glDisable(GL_POLYGON_OFFSET_EXT);
     }
-#endif // GL_EXT_polygon_offset
+#endif // GL_EXT_polygon_offset && (!GL_VERSION_1_1 || defined(__sgi))
     this->currentstyles = (Style) 0;
   }
   
