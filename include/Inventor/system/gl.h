df410d95ad450290999707a770b62b7028155636
Lars J. Aas <larsa@sim.no>
2001-09-13 17:35:29 +0000

    new header for taking care of system anomalies and including gl.h

---
+++ include/Inventor/system/gl.h
@@ -0,0 +1,50 @@
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2001 by Systems in Motion. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  version 2.1 as published by the Free Software Foundation. See the
+ *  file LICENSE.LGPL at the root directory of the distribution for
+ *  more details.
+ *
+ *  If you want to use Coin for applications not compatible with the
+ *  LGPL, please contact SIM to acquire a Professional Edition license.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
+ *  http://www.sim.no support@sim.no Voice: +47 22114160 Fax: +47 22207097
+ *
+\**************************************************************************/
+
+#ifndef COIN_GL_H
+#define COIN_GL_H
+
+/* This header file is supposed to take care of all operating system
+ * dependent anomalies connected to the gl.h include file.  It is only
+ * for use while building the Coin library, and should not be installed
+ * with the rest of the header files (at least not yet).  20010913 larsa */
+
+#ifndef COIN_CONFIGURE_BUILD
+#error "you must include config.h before including Inventor/system/gl.h"
+#endif /* !COIN_CONFIGURE_BUILD */
+
+#ifdef HAVE_WINDOWS_H
+/* on windows, headers do not include their dependencies */
+#include <windows.h>
+#endif /* HAVE_WINDOWS_H */
+
+#ifdef HAVE_GL_GL_H
+/* the preferred gl.h path */
+#include <GL/gl.h>
+#else
+#ifdef HAVE_OPENGL_GL_H
+/* how Mac OS X organizes things - should we now stuff Coin header in
+   OpenInventor/?  (that *was* ironi) */
+#include <OpenGL/gl.h>
+#else
+#error "don't know how to include gl.h header"
+#endif /* !HAVE_GL_GL_H */
+#endif /* !HAVE_OPENGL_GL_H */
+
+#endif /* ! COIN_GL_H */
