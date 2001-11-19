dd432c24ddd170f1fc47f33307087b3c76ff998b
Lars J. Aas <larsa@sim.no>
2001-11-19 18:19:40 +0000

    C++ wrapper includer

---
+++ src/threads/wrappers.cpp
@@ -0,0 +1,7 @@
+#include <Inventor/threads/SbThread.h>
+#include <Inventor/threads/SbMutex.h>
+#include <Inventor/threads/SbRWMutex.h>
+#include <Inventor/threads/SbCondVar.h>
+#include <Inventor/threads/SbBarrier.h>
+#include <Inventor/threads/SbStorage.h>
+#include <Inventor/threads/SbTypedStorage.h>
