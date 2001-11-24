c58282170b70f12416ddf6c9d1a24985c2e73dc0
Marius Kintel <kintel@sim.no>
2001-11-24 01:36:28 +0000

    New and improved GLWrapper

--- src/misc/GLWrapper.cpp
+++ src/misc/GLWrapper.cpp
@@ -21,61 +21,121 @@
  *
 \**************************************************************************/
 
-#include "GLWrapper.h"
+#ifndef COIN_OPENGL_DYNAMIC_BINDING
+#define GL_GLEXT_PROTOTYPES
+#endif
 
-#include <Inventor/SbDict.h>
+#include "GLWrapper.h"
 
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#if HAVE_CONFIG_H
-#include "config.h"
-#endif /* !HAVE_CONFIG_H */
 
-#if HAVE_DLFCN_H
+#ifdef HAVE_DLFCN_H
 #include <dlfcn.h>
 #endif /* HAVE_DLFCN_H */
 
-#ifdef HAVE_GLX
-#include <GL/glx.h>
-#endif /* HAVE_GLX */
-
-#if   defined(HAVE_WGL)
-  #define LIBHANDLE_T void*
-  #define OPEN_RUNTIME_BINDING(LIBNAME) (LIBHANDLE_T)1
-  #define GETPROCADDRESS(DLHANDLE,FUNC)   wglGetProcAddress(FUNC)
-#elif defined(GLX_VERSION_1_4)           // Have glXGetProcAddress()
-  #define LIBHANDLE_T void*
-  #define OPEN_RUNTIME_BINDING(LIBNAME) (LIBHANDLE_T)1
-  #define GETPROCADDRESS(DLHANDLE,FUNC)   glXGetProcAddress((const GLubyte *)FUNC)
-#elif defined(GLX_ARB_get_proc_address)
-  #define LIBHANDLE_T void*
-  #define OPEN_RUNTIME_BINDING(LIBNAME) (LIBHANDLE_T)1
-  #define GETPROCADDRESS(DLHANDLE,FUNC)   glXGetProcAddressARB((const GLubyte *)FUNC)
-#elif defined(HAVE_DL_LIB)
-  #define LIBHANDLE_T void*
-  #define OPEN_RUNTIME_BINDING(LIBNAME)   dlopen(LIBNAME, RTLD_LAZY)
-  #define GETPROCADDRESS(DLHANDLE,FUNC)   dlsym(DLHANDLE,FUNC)
-#else // This platform has no run-time binding capabilities
-  #define LIBHANDLE_T void*
-  #define OPEN_RUNTIME_BINDING(LIBNAME) (LIBHANDLE_T)1
-  #define GETPROCADDRESS(DLHANDLE,FUNC)   (&FUNC)
+/*
+  Define the GETPROCADDRESS macro.
+ */
+#ifdef HAVE_HASH_QUOTING
+#define GETPROCADDRESS(GLWHANDLE,FUNC) \
+  GLWrapper_getProcAddress(GLWHANDLE,#FUNC)
+#elif defined(HAVE_APOSTROPHES_QUOTING)
+#define GETPROCADDRESS(GLWHANDLE,FUNC) \
+  GLWrapper_getProcAddress(GLWHANDLE,"FUNC")
+#else
+#error Unknown quoting.
 #endif
+
+/* 
+   Define GLWRAPPER_REGISTER_FUNC macro. Casting the type is
+   necessary for this file to be compatible with C++ compilers. 
+*/
+#define GLWRAPPER_REGISTER_FUNC(_wrapname_, _funcname_, _funcsig_) \
+  gi->_wrapname_ = (_funcsig_)GETPROCADDRESS(gi, _funcname_)
+    
+void *
+GLWrapper_getProcAddress(GLWrapper_t *gi, const char *fname)
+{
+#ifdef HAVE_WGL
+  return wglGetProcAddress(fname);
+#else
+  if (0) {
+  }
+  //FIXME: Bind this using dlsym() ? (kintel 20011123)
+#ifdef GLX_VERSION_1_4           // Have glXGetProcAddress()
+  else if (GLWrapper_glxVersionMatchesAtLeast(gi, 1,4)) {
+      return (void *)glXGetProcAddress((const GLubyte *)fname);
+  }
+#endif
+#ifdef HAVE_GLX
+  else if (GLWrapper_glxVersionMatchesAtLeast(gi, 1,1) &&
+           gi->glXGetProcAddressARB) {
+    return (void *)gi->glXGetProcAddressARB((const GLubyte *)fname);
+  }
+#endif
+#ifdef HAVE_DL_LIB
+  else if (1) {
+    if (!gi->libhandle) {
+      gi->libhandle = dlopen(NULL, RTLD_LAZY);
+      //      if (!gi->libhandle) FIXME: Error handling (kintel 20011121)
+    }
+    return dlsym(gi->libhandle, fname);
+  }
+#endif
+  return NULL; //FIXME: Assert or run-time error? (kintel 20011121)
+#endif
+}
+
+/*
+ Returns a string describing the method used for dynamic binding
+ */
+const char *
+GLWrapper_getProcAddressMethod(GLWrapper_t *gi)
+{
+#ifdef HAVE_WGL
+  return "wglGetProcAddress() (MS Windows)";
+#else
+  if (0) {
+  }
+#ifdef GLX_VERSION_1_4           // Have glXGetProcAddress()
+  else if (GLWrapper_glxVersionMatchesAtLeast(gi, 1,4)) {
+      return "glXGetProcAddress() (GLX 1.4)"
+  }
+#endif
+#ifdef HAVE_GLX
+  else if (GLWrapper_glxVersionMatchesAtLeast(gi, 1,1) &&
+           gi->glXGetProcAddressARB) {
+    return "glXGetProcAddressARB()";
+  }
+#endif
+#ifdef HAVE_DL_LIB
+  else if (1) {
+    return "dlopen()/dlsym()";
+  }
+#endif
+  return "NULL: Error";
+#endif
+}
 
 /* FIXME: support HP-UX? (Doesn't have dlopen().) 20010626 mortene. */
 
-
-LIBHANDLE_T GL_libhandle = NULL;
+#ifdef COIN_INTERNAL
 static GLWrapper_t * GL_instance = NULL;
 static SbDict * gldict = NULL;
+#else
+static int gldict = 0;
+#endif
 
-
+#ifdef COIN_INTERNAL
 static void
 free_GLWrapper_instance(unsigned long key, void * value)
 {
   free(value);
 }
+#endif
 
 /* Cleans up at exit. */
 static void
@@ -83,9 +143,10 @@ GLWrapper_cleanup(void)
 {
   // FIXME: clean up. 20011115 mortene.
   // if (GL_libhandle) { CLOSE_RUNTIME_BINDING(GL_libhandle); }
-
+#ifdef COIN_INTERNAL
   gldict->applyToAll(free_GLWrapper_instance);
   delete gldict;
+#endif
 }
 
 
@@ -97,16 +158,16 @@ GLWrapper_cleanup(void)
   are made, make sure they are propagated over if necessary.
 */
 static void
-GLWrapper_set_numeric_version(GLWrapper_t * wrapper)
+GLWrapper_set_glVersion(GLWrapper_t * wrapper)
 {
   char buffer[256];
   char * dotptr;
 
   const char * versionstr = (const char *)glGetString(GL_VERSION);
 
-  wrapper->version.major = 0;
-  wrapper->version.minor = 0;
-  wrapper->version.release = 0;
+  wrapper->glVersion.major = 0;
+  wrapper->glVersion.minor = 0;
+  wrapper->glVersion.release = 0;
 
   (void)strncpy(buffer, (const char *)versionstr, 255);
   buffer[255] = '\0'; /* strncpy() will not null-terminate if strlen > 255 */
@@ -115,7 +176,7 @@ GLWrapper_set_numeric_version(GLWrapper_t * wrapper)
     char * spaceptr;
     char * start = buffer;
     *dotptr = '\0';
-    wrapper->version.major = atoi(start);
+    wrapper->glVersion.major = atoi(start);
     start = ++dotptr;
 
     dotptr = strchr(start, '.');
@@ -125,32 +186,142 @@ GLWrapper_set_numeric_version(GLWrapper_t * wrapper)
     if (dotptr) {
       int terminate = *dotptr == ' ';
       *dotptr = '\0';
-      wrapper->version.minor = atoi(start);
+      wrapper->glVersion.minor = atoi(start);
       if (!terminate) {
         start = ++dotptr;
         dotptr = strchr(start, ' ');
         if (dotptr) *dotptr = '\0';
-        wrapper->version.release = atoi(start);
+        wrapper->glVersion.release = atoi(start);
       }
     }
     else {
-      wrapper->version.minor = atoi(start);
+      wrapper->glVersion.minor = atoi(start);
     }
   }
 }
 
+/*
+  Set the GLX version variables in the given GLWrapper_t struct
+  instance. Returns 1 on success, 0 on failure (which means the variables
+  are not set.
+*/
+static int
+GLWrapper_set_glxVersion(GLWrapper_t * gi)
+{
+  int ok = 0;
+#ifdef HAVE_GLX
+  gi->glxVersion.major = 0;
+  gi->glxVersion.minor = 0;
+#ifndef COIN_INTERNAL
+  Display *dpy = XOpenDisplay(NULL);
+  ok = (int)glXQueryVersion(dpy, &gi->glxVersion.major, &gi->glxVersion.minor);
+  XCloseDisplay(dpy);
+#endif
+#endif
+  return ok;
+}
+
 int
-GLWrapper_versionMatchesAtLeast(GLWrapper_t * w,
+GLWrapper_glVersionMatchesAtLeast(const GLWrapper_t * w,
                                 unsigned int major,
                                 unsigned int minor,
                                 unsigned int revision)
 {
-  if (w->version.major < major) return 0;
-  else if (w->version.major > major) return 1;
-  if (w->version.minor < minor) return 0;
-  else if (w->version.minor > minor) return 1;
-  if (w->version.release < revision) return 0;
+  if (w->glVersion.major < major) return 0;
+  else if (w->glVersion.major > major) return 1;
+  if (w->glVersion.minor < minor) return 0;
+  else if (w->glVersion.minor > minor) return 1;
+  if (w->glVersion.release < revision) return 0;
   return 1;
+}
+
+int
+GLWrapper_glxVersionMatchesAtLeast(const GLWrapper_t * w,
+                                   int major,
+                                   int minor)
+{
+#ifdef HAVE_GLX
+  if (w->glxVersion.major < major) return 0;
+  else if (w->glxVersion.major > major) return 1;
+  if (w->glxVersion.minor < minor) return 0;
+  return 1;
+#else
+  return 0;
+#endif
+}
+
+int 
+GLWrapper_glEXTSupported(GLWrapper_t * wrapper, const char * extension)
+{
+  assert(extension);
+
+  static const GLubyte *extensions = NULL;
+  const GLubyte *start;
+  GLubyte *where, *terminator;
+  
+  /* Extension names should not have spaces. */
+  where = (GLubyte *) strchr(extension, ' ');
+  if (where || *extension == '\0')
+    return 0;
+  
+  if (!extensions)
+    extensions = glGetString(GL_EXTENSIONS);
+  start = extensions;
+  for (;;) {
+    where = (GLubyte *) strstr((const char *)start, extension);
+    if (!where)
+      break;
+    terminator = where + strlen(extension);
+    if (where == start || *(where - 1) == ' ') {
+      if (*terminator == ' ' || *terminator == '\0') {
+        return 1;
+      }
+    }
+    start = terminator;
+  }
+  return 0;
+}
+
+int 
+GLWrapper_glxEXTSupported(GLWrapper_t * wrapper, const char * extension)
+{
+#ifdef HAVE_GLX
+  assert(extension);
+
+  static const char *extensions = NULL;
+  const char *start;
+  char *where, *terminator;
+  
+  /* Extension names should not have spaces. */
+  where = (char *) strchr(extension, ' ');
+  if (where || *extension == '\0')
+    return 0;
+  
+  if (!extensions) {
+#ifdef COIN_INTERNAL
+    extensions = NULL;
+#else
+    Display *dpy = XOpenDisplay(NULL);
+    int screen = XDefaultScreen(dpy);
+    extensions = glXQueryExtensionsString(dpy, screen);
+    XCloseDisplay(dpy);
+#endif
+  }
+  start = extensions;
+  for (;;) {
+    where = (char *) strstr((const char *)start, extension);
+    if (!where)
+      break;
+    terminator = where + strlen(extension);
+    if (where == start || *(where - 1) == ' ') {
+      if (*terminator == ' ' || *terminator == '\0') {
+        return 1;
+      }
+    }
+    start = terminator;
+  }
+#endif
+  return 0;
 }
 
 const GLWrapper_t *
@@ -158,44 +329,204 @@ GLWrapper(int contextid)
 {
   if (!gldict) {  /* First invocation, do initializations. */
     (void)atexit(GLWrapper_cleanup);
-
+#ifdef COIN_INTERNAL
     gldict = new SbDict;
-
-    GL_libhandle = OPEN_RUNTIME_BINDING(NULL);
-    if (!GL_libhandle) { } // FIXME: Error handling. 200111xx kintel.
+#endif
   }
 
+  int found;
+#ifdef COIN_INTERNAL
   void * ptr;
-  SbBool found = gldict->find(contextid, ptr);
+  found = (int)gldict->find(contextid, ptr);
+#else
+  static void *ptr = NULL;
+  found = gldict;
+#endif
   GLWrapper_t * gi = NULL;
 
   if (!found) {
     gi = (GLWrapper_t *)malloc(sizeof(GLWrapper_t));
     /* FIXME: handle out-of-memory on malloc(). 20000928 mortene. */
+
+#ifdef COIN_INTERNAL
     ptr = gi;
     gldict->enter(contextid, ptr);
-
-    // Define GLWRAPPER_REGISTER_FUNC macro. Casting the type is
-    // necessary for this file to be compatible with C++ compilers. 
-#ifdef HAVE_HASH_QUOTING
-  #define GLWRAPPER_REGISTER_FUNC(_funcname_, _funcsig_) \
-  gi->_funcname_ = (_funcsig_)GETPROCADDRESS(GL_libhandle, #_funcname_)
-#elif defined(HAVE_APOSTROPHES_QUOTING)
-  #define GLWRAPPER_REGISTER_FUNC(_funcname_, _funcsig_) \
-  gi->_funcname_ = (_funcsig_)GETPROCADDRESS(GL_libhandle, "_funcname_")
 #else
-  #error Unknown quoting.
+    ptr = gi;
+#endif
+    
+    gi->libhandle = NULL;
+    GLWrapper_set_glVersion(gi);
+#ifdef HAVE_GLX
+    GLWrapper_set_glxVersion(gi);
+#endif
+    
+#ifdef HAVE_GLX
+#ifdef HAVE_DL_LIB
+    void *handle = dlopen(NULL, RTLD_LAZY);
+    //FIXME: Error handling (kintel 20011123)
+    gi->glXGetProcAddressARB = (COIN_PFNGLXGETPROCADDRESSARB)dlsym(handle, "glXGetProcAddressARB");
+    dlclose(handle);
+#elif HAVE_GLXGETPROCADDRESSARB
+    gi->glXGetProcAddressARB = &glXGetProcAddressARB;
+#endif
 #endif
 
-    // Resolve our functions 
-    GLWRAPPER_REGISTER_FUNC(glTexImage3D, COIN_PFNGLTEXIMAGE3DPROC);
-    GLWRAPPER_REGISTER_FUNC(glTexImage3DEXT, COIN_PFNGLTEXIMAGE3DEXTPROC);
+    printf("GLWrapper: Using %s\n\n",GLWrapper_getProcAddressMethod(gi));
+    
+    // Initialize everything to zero.
+    gi->COIN_GL_TEXTURE_3D = 0;
+    gi->COIN_GL_PROXY_TEXTURE_3D = 0;
+    gi->COIN_GL_TEXTURE_WRAP_R = 0;
+    gi->COIN_GL_TEXTURE_DEPTH = 0;
+    gi->COIN_GL_MAX_3D_TEXTURE_SIZE = 0;
+    gi->COIN_GL_PACK_IMAGE_HEIGHT = 0;
+    gi->COIN_GL_UNPACK_IMAGE_HEIGHT = 0;
+    gi->COIN_GL_PACK_SKIP_IMAGES = 0;
+    gi->COIN_GL_UNPACK_SKIP_IMAGES = 0;
+    gi->glTexImage3D = NULL;
+    gi->glCopyTexSubImage3D = NULL;
+    gi->glTexSubImage3D = NULL;
+    
+    gi->COIN_GL_CLAMP_TO_EDGE = 0;
+    
+#ifdef HAVE_GLX
+    gi->glXGetCurrentDisplay = NULL;
+#endif
+
+    // Enums and functions are resolved separately because of
+    // better readability in case of static binding.
+
+
+    // Resolve our enum equivalents
+    if (GLWrapper_glVersionMatchesAtLeast(gi,1,2,0)) {
+      gi->COIN_GL_TEXTURE_3D = GL_TEXTURE_3D;
+      gi->COIN_GL_PROXY_TEXTURE_3D = GL_PROXY_TEXTURE_3D;
+      gi->COIN_GL_TEXTURE_WRAP_R = GL_TEXTURE_WRAP_R;
+      gi->COIN_GL_TEXTURE_DEPTH = GL_TEXTURE_DEPTH;
+      gi->COIN_GL_MAX_3D_TEXTURE_SIZE = GL_MAX_3D_TEXTURE_SIZE;
+      gi->COIN_GL_PACK_IMAGE_HEIGHT = GL_PACK_IMAGE_HEIGHT;
+      gi->COIN_GL_UNPACK_IMAGE_HEIGHT = GL_UNPACK_IMAGE_HEIGHT;
+      gi->COIN_GL_PACK_SKIP_IMAGES = GL_PACK_SKIP_IMAGES;
+      gi->COIN_GL_UNPACK_SKIP_IMAGES = GL_UNPACK_SKIP_IMAGES;
+    }
+    else if (GLWrapper_glVersionMatchesAtLeast(gi,1,1,0) &&
+             GLWrapper_glEXTSupported(gi,"GL_EXT_texture3D")) {
+      gi->COIN_GL_TEXTURE_3D = GL_TEXTURE_3D_EXT;
+      gi->COIN_GL_PROXY_TEXTURE_3D = GL_PROXY_TEXTURE_3D;
+      gi->COIN_GL_TEXTURE_WRAP_R = GL_TEXTURE_WRAP_R_EXT;
+      gi->COIN_GL_TEXTURE_DEPTH = GL_TEXTURE_DEPTH_EXT;
+      gi->COIN_GL_MAX_3D_TEXTURE_SIZE = GL_MAX_3D_TEXTURE_SIZE_EXT;
+      gi->COIN_GL_PACK_IMAGE_HEIGHT = GL_PACK_IMAGE_HEIGHT_EXT;
+      gi->COIN_GL_UNPACK_IMAGE_HEIGHT = GL_UNPACK_IMAGE_HEIGHT_EXT;
+      gi->COIN_GL_PACK_SKIP_IMAGES = GL_PACK_SKIP_IMAGES_EXT;
+      gi->COIN_GL_UNPACK_SKIP_IMAGES = GL_UNPACK_SKIP_IMAGES_EXT;
+    }
+
+    if (GLWrapper_glVersionMatchesAtLeast(gi,1,2,0)) {
+      gi->COIN_GL_CLAMP_TO_EDGE = GL_CLAMP_TO_EDGE;
+    }
+    else if (GLWrapper_glEXTSupported(gi, "GL_SGIS_texture_edge_clamp")) {
+      gi->COIN_GL_CLAMP_TO_EDGE = GL_CLAMP_TO_EDGE_SGIS;
+    }
+    //FIXME: Does this extension actually exist?
+//      else if (GLWrapper_glEXTSupported(gi, "GL_EXT_texture_edge_clamp")) {
+//        gi->COIN_GL_CLAMP_TO_EDGE = GL_CLAMP_TO_EDGE_EXT;
+//      }
+
+    // Resolve our functions
+#ifdef COIN_OPENGL_DYNAMIC_BINDING
+    if (GLWrapper_glVersionMatchesAtLeast(gi,1,2,0)) {
+      GLWRAPPER_REGISTER_FUNC(glTexImage3D, glTexImage3D, 
+                              COIN_PFNGLTEXIMAGE3DPROC);
+      GLWRAPPER_REGISTER_FUNC(glCopyTexSubImage3D, glCopyTexSubImage3D, 
+                              COIN_PFNGLCOPYTEXSUBIMAGE3DPROC);
+      GLWRAPPER_REGISTER_FUNC(glTexSubImage3D, glTexSubImage3D,
+                              COIN_PFNGLTEXSUBIMAGE3DPROC);
+    }
+    else if (GLWrapper_glVersionMatchesAtLeast(gi,1,1,0) &&
+             GLWrapper_glEXTSupported(gi,"GL_EXT_texture3D")) {
+      GLWRAPPER_REGISTER_FUNC(glTexImage3D, glTexImage3DEXT, 
+                              COIN_PFNGLTEXIMAGE3DPROC);
+      if (GLWrapper_glEXTSupported(gi, "GL_EXT_copy_texture")) 
+        GLWRAPPER_REGISTER_FUNC(glCopyTexSubImage3D, glCopyTexSubImage3DEXT, 
+                                COIN_PFNGLCOPYTEXSUBIMAGE3DPROC);
+      if (GLWrapper_glEXTSupported(gi, "GL_EXT_subtexture"))
+        GLWRAPPER_REGISTER_FUNC(glTexSubImage3D, glTexSubImage3DEXT,
+                                COIN_PFNGLTEXSUBIMAGE3DPROC);
+    }
+    
+    /* Some SGI OpenGL implementations report OpenGL 1.1 without supporting
+       glPolygonOffset 100%. The do support glPolygonOffsetEXT though, so we
+       hide the checking here. 
+       FIXME: We may want to check this during configure instead. */
+    /* FIXME: Reintroduce the env variable check? */
+    if (0) {
+    }
+#if defined(__sgi)
+    else if (GLWrapper_glVersionMatchesAtLeast(gi,1,1,0) &&
+             !GLWrapper_glVersionMatchesAtLeast(gi,1,2,0)) {
+      GLWRAPPER_REGISTER_FUNC(glPolygonOffset, glPolygonOffsetEXT,
+                              COIN_PFNGLPOLYGONOFFSETPROC);
+    }
+#endif
+    else {
+      GLWRAPPER_REGISTER_FUNC(glPolygonOffset, glPolygonOffset,
+                              COIN_PFNGLPOLYGONOFFSETPROC);
+    }
+#else // Static binding
+    if (0) {
+    }
+#if GL_VERSION_1_2
+    else if (GLWrapper_glVersionMatchesAtLeast(gi,1,2,0)) {
+      gi->glTexImage3D = &glTexImage3D;
+      gi->glCopyTexSubImage3D = &glCopyTexSubImage3D;
+      gi->glTexSubImage3D = &glTexSubImage3D;
+    }
+#endif
+#if GL_VERSION_1_1
+#ifdef GL_EXT_texture3D
+    else if (GLWrapper_glVersionMatchesAtLeast(gi,1,1,0) &&
+             GLWrapper_glEXTSupported(gi,"GL_EXT_texture3D")) {
+      gi->glTexImage3D = (COIN_PFNGLTEXIMAGE3DPROC)&glTexImage3DEXT;
+      //FIXME: #ifdef these extensions as well? (kintel 20011123)
+      if (GLWrapper_glEXTSupported(gi, "GL_EXT_copy_texture")) 
+        gi->glCopyTexSubImage3D = (COIN_PFNGLCOPYTEXSUBIMAGE3DPROC)&glCopyTexSubImage3DEXT;
+      if (GLWrapper_glEXTSupported(gi, "GL_EXT_subtexture"))
+        gi->glTexSubImage3D = (COIN_PFNGLTEXSUBIMAGE3DPROC)&glTexSubImage3DEXT;
+    }
+#endif
+#endif
+    
+    /* Some SGI OpenGL implementations report OpenGL 1.1 without supporting
+       glPolygonOffset 100%. They do support glPolygonOffsetEXT though, so we
+       hide the checking here. 
+       FIXME: We may want to check this during configure instead. */
+    /* FIXME: Reintroduce the env variable check? */
+    if (0) {
+    }
+#if defined(__sgi)
+    else if (GLWrapper_glVersionMatchesAtLeast(gi,1,1,0) &&
+             !GLWrapper_glVersionMatchesAtLeast(gi,1,2,0)) {
+      gi->glPolygonOffset = (COIN_PFNGLPOLYGONOFFSETPROC)&glPolygonOffsetEXT;
+    }
+#endif
+    else {
+      gi->glPolygonOffset = (COIN_PFNGLPOLYGONOFFSETPROC)&glPolygonOffset;
+    }
+#endif
+
+#ifdef HAVE_GLX
+    //FIXME: Check if this function exists as an extension as well.
+    if (GLWrapper_glxVersionMatchesAtLeast(gi,1,2)) {
+      GLWRAPPER_REGISTER_FUNC(glXGetCurrentDisplay, glXGetCurrentDisplay,
+                              COIN_PFNGLXGETCURRENTDISPLAYPROC);
+    }
+#endif
   }
   else {
     gi = (GLWrapper_t *)ptr;
-  }
-
-  GLWrapper_set_numeric_version(gi);
+  }  
 
   return gi;
 }
--- src/misc/GLWrapper.h
+++ src/misc/GLWrapper.h
@@ -1,6 +1,9 @@
 #ifndef COIN_GLWRAPPER_H
 #define COIN_GLWRAPPER_H
 
+// FIXME: sooffscreenrenderer
+// FIXME: soglpolygonoffsetelement
+
 /**************************************************************************\
  *
  *  This file is part of the Coin 3D visualization library.
@@ -37,29 +40,109 @@
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
 
+#ifdef COIN_INTERNAL
 #include <Inventor/system/gl.h>
+#include <Inventor/SbDict.h>
+#else
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+#endif
+
+#ifdef HAVE_GLX
+#include <GL/glx.h>
+#endif /* HAVE_GLX */
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
+/* Our own enum definitions */
+#define GL_PACK_SKIP_IMAGES               0x806B
+#define GL_PACK_SKIP_IMAGES_EXT           0x806B
+#define GL_PACK_IMAGE_HEIGHT              0x806C
+#define GL_PACK_IMAGE_HEIGHT_EXT          0x806C
+#define GL_UNPACK_SKIP_IMAGES             0x806D
+#define GL_UNPACK_SKIP_IMAGES_EXT         0x806D
+#define GL_UNPACK_IMAGE_HEIGHT            0x806E
+#define GL_UNPACK_IMAGE_HEIGHT_EXT        0x806E
+#define GL_TEXTURE_3D                     0x806F
+#define GL_TEXTURE_3D_EXT                 0x806F
+#define GL_PROXY_TEXTURE_3D               0x8070
+#define GL_PROXY_TEXTURE_3D_EXT           0x8070
+#define GL_TEXTURE_DEPTH                  0x8071
+#define GL_TEXTURE_DEPTH_EXT              0x8071
+#define GL_TEXTURE_WRAP_R                 0x8072
+#define GL_TEXTURE_WRAP_R_EXT             0x8072
+#define GL_MAX_3D_TEXTURE_SIZE            0x8073
+#define GL_MAX_3D_TEXTURE_SIZE_EXT        0x8073
+#define GL_CLAMP_TO_EDGE                  0x812F
+#define GL_CLAMP_TO_EDGE_SGIS             0x812F
+//  #define GL_TEXTURE_GEN_R
+//  #define GL_TEXTURE_GEN_Q
+
 /* Our own typedefs for OpenGL functions. These are copied from
    glext.h and prefixed with COIN_ to avoid namespace collisions. */
 typedef void (APIENTRY * COIN_PFNGLTEXIMAGE3DPROC) (GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLsizei depth, GLint border, GLenum format, GLenum type, const GLvoid *pixels);
-typedef void (APIENTRY * COIN_PFNGLTEXIMAGE3DEXTPROC) (GLenum target, GLint level, GLenum internalformat, GLsizei width, GLsizei height, GLsizei depth, GLint border, GLenum format, GLenum type, const GLvoid *pixels);
+typedef void (APIENTRY * COIN_PFNGLTEXSUBIMAGE3DPROC) (GLenum target, GLint level, GLint xoffset, GLint yoffset, GLint zoffset, GLsizei width, GLsizei height, GLsizei depth, GLenum format, GLenum type, const GLvoid *pixels);
+typedef void (APIENTRY * COIN_PFNGLCOPYTEXSUBIMAGE3DPROC) (GLenum target, GLint level, GLint xoffset, GLint yoffset, GLint zoffset, GLint x, GLint y, GLsizei width, GLsizei height);
+typedef void (APIENTRY * COIN_PFNGLPOLYGONOFFSETPROC) (GLfloat factor, GLfloat bias);
+
+#ifdef HAVE_GLX
+/* GLX functions */
+typedef void *(APIENTRY * COIN_PFNGLXGETPROCADDRESSARB) (const GLubyte *procName);
+typedef Display *(APIENTRY * COIN_PFNGLXGETCURRENTDISPLAYPROC) (void);
+#endif
 
 typedef struct {
   /* OpenGL versioning. */
   struct {
     unsigned int major, minor, release;
-  } version;
+  } glVersion;
+
+  struct {
+    int major, minor;
+  } glxVersion;
+
+  void *libhandle; // Only used in case of dlopen()/dlsym() binding
+
+  /* Our own "enums". Will be 0 if not available, otherwise they
+     contain a valid GLenum. */
+  GLenum COIN_GL_TEXTURE_3D;
+  GLenum COIN_GL_PROXY_TEXTURE_3D;
+  GLenum COIN_GL_TEXTURE_WRAP_R;
+  GLenum COIN_GL_TEXTURE_DEPTH;
+  GLenum COIN_GL_MAX_3D_TEXTURE_SIZE;
+  GLenum COIN_GL_PACK_IMAGE_HEIGHT;
+  GLenum COIN_GL_UNPACK_IMAGE_HEIGHT;
+  GLenum COIN_GL_PACK_SKIP_IMAGES;
+  GLenum COIN_GL_UNPACK_SKIP_IMAGES;
+  //FIXME: Check OpenGL 1.1 availability of these
+  GLenum COIN_GL_TEXTURE_GEN_R;
+  GLenum COIN_GL_TEXTURE_GEN_Q;
+
+  GLenum COIN_GL_CLAMP_TO_EDGE;
 
   /* OpenGL calls. Will be NULL if not available, otherwise they
      contain a valid function pointer into the OpenGL library. */
   COIN_PFNGLTEXIMAGE3DPROC glTexImage3D;
-  COIN_PFNGLTEXIMAGE3DEXTPROC glTexImage3DEXT;
+  COIN_PFNGLCOPYTEXSUBIMAGE3DPROC glCopyTexSubImage3D;
+  COIN_PFNGLTEXSUBIMAGE3DPROC glTexSubImage3D; 
+  COIN_PFNGLPOLYGONOFFSETPROC glPolygonOffset;
+
+#ifdef HAVE_GLX
+  COIN_PFNGLXGETPROCADDRESSARB glXGetProcAddressARB;
+  COIN_PFNGLXGETCURRENTDISPLAYPROC glXGetCurrentDisplay;
+#endif
+
 } GLWrapper_t;
 
+/*
+  Returns the wrapper instance for the given context ID (defined
+  by SoGLCacheContextElement).
+ */
 const GLWrapper_t * GLWrapper(int contextid);
 
 /*
@@ -67,10 +150,31 @@ const GLWrapper_t * GLWrapper(int contextid);
   least as "late" as what is given with the input arguments. Otherwise
   returns 0.
  */
-int GLWrapper_versionMatchesAtLeast(GLWrapper_t * wrapper,
-                                    unsigned int major,
-                                    unsigned int minor,
-                                    unsigned int release);
+int GLWrapper_glVersionMatchesAtLeast(const GLWrapper_t * wrapper,
+                                      unsigned int major,
+                                      unsigned int minor,
+                                      unsigned int release);
+
+/*
+  Returns 1 if the GLX implementation of the wrapper context is at
+  least as "late" as what is given with the input arguments. Otherwise
+  returns 0.
+ */
+int GLWrapper_glxVersionMatchesAtLeast(const GLWrapper_t * wrapper,
+                                       int major,
+                                       int minor);
+
+/*
+  Returns 1 if the given extension is supported by this context,
+  0 if not.
+ */
+int GLWrapper_glEXTSupported(GLWrapper_t * wrapper, const char * extname);
+
+/*
+  Returns 1 if the gived GLX extension is supported by this GLX implementation,
+  0 if not.
+ */
+int GLWrapper_glxEXTSupported(GLWrapper_t * wrapper, const char * extname);
 
 #ifdef __cplusplus
 }
