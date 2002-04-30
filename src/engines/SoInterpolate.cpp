2b28b22a51aff7eace896bf60c8e61bd17ef28d1
Peder Blekken <pederb@sim.no>
2002-04-30 10:43:33 +0000

    Adds SoInterpolate::initClasses() implementation.

--- src/engines/SoInterpolate.cpp
+++ src/engines/SoInterpolate.cpp
@@ -2,7 +2,7 @@
  *
  *  This file is part of the Coin 3D visualization library.
  *  Copyright (C) 1998-2001 by Systems in Motion.  All rights reserved.
- *  
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
  *  version 2 as published by the Free Software Foundation.  See the
@@ -91,6 +91,15 @@ void
 SoInterpolate::initClass(void)
 {
   SO_ENGINE_INTERNAL_INIT_ABSTRACT_CLASS(SoInterpolate);
+}
+
+/*!
+  This method is provided only for API compatibility, and does nothing
+  in Coin.
+*/
+void 
+SoInterpolate::initClasses(void)
+{
 }
 
 /*!
