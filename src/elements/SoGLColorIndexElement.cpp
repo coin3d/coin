568d1d0653245bd670b5477c9ff4fddc161d575f
Peder Blekken <pederb@sim.no>
2002-09-11 12:11:27 +0000

    Revive SoGLColorIndexElement.cpp

---
+++ src/elements/SoGLColorIndexElement.cpp
@@ -0,0 +1,116 @@
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2001 by Systems in Motion.  All rights reserved.
+ *  
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  version 2 as published by the Free Software Foundation.  See the
+ *  file LICENSE.GPL at the root directory of this source distribution
+ *  for more details.
+ *
+ *  If you desire to use Coin with software that is incompatible
+ *  licensewise with the GPL, and / or you would like to take
+ *  advantage of the additional benefits with regard to our support
+ *  services, please contact Systems in Motion about acquiring a Coin
+ *  Professional Edition License.  See <URL:http://www.coin3d.org> for
+ *  more information.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
+ *  <URL:http://www.sim.no>, <mailto:support@sim.no>
+ *
+\**************************************************************************/
+
+/*!
+  \class SoGLColorIndexElement Inventor/elements/SoGLColorIndexElement.h
+  \brief The SoGLColorIndexElement class sets the current OpenGL color.
+  \ingroup elements
+
+  This element is only used when the OpenGL canvas is in colorindex
+  mode, ie where colors for individual pixels are fetched from a color
+  lookup table ("CLUT"). The usual thing to do is to set up a canvas
+  in RGBA truecolor mode.
+
+  One common use for colorindex mode OpenGL canvases is to use one in
+  the overlay planes (which are usually limited to only 2 or 4
+  available colors), if supported by the OpenGL hardware and / or
+  driver.
+
+  \sa SoColorIndex
+*/
+
+#include <Inventor/elements/SoGLColorIndexElement.h>
+#include <Inventor/elements/SoGLLazyElement.h>
+#include <Inventor/misc/SoState.h>
+#include <assert.h>
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif // HAVE_CONFIG_H
+
+#include <Inventor/system/gl.h>
+
+static int32_t defaultIndexArray[] = { 1 };
+
+SO_ELEMENT_SOURCE(SoGLColorIndexElement);
+
+// doc in parent
+void
+SoGLColorIndexElement::initClass(void)
+{
+  SO_ELEMENT_INIT_CLASS(SoGLColorIndexElement, inherited);
+}
+
+
+// doc in parent
+void
+SoGLColorIndexElement::init(SoState * state)
+{
+  this->state = state;
+}
+
+/*!
+  The destructor.
+*/
+SoGLColorIndexElement::~SoGLColorIndexElement()
+{
+}
+
+/*!
+  Returns \c TRUE if the current GL context is in color index mode.
+*/
+SbBool
+SoGLColorIndexElement::isColorIndexMode(SoState * state)
+{
+  return SoGLLazyElement::isColorIndex(state);
+}
+
+/*!
+  Sets current color indices.
+*/
+void
+SoGLColorIndexElement::set(SoState * const state, SoNode * const node,
+                           const int32_t numindices,
+                           const int32_t * const indices)
+{
+  SoLazyElement::setColorIndices(state, node, numindices, indices);
+}
+
+/*!
+  Returns number of color indices in element.
+*/
+int32_t
+SoGLColorIndexElement::getNum(void) const
+{
+  return SoLazyElement::getInstance(this->state)->getNumColorIndices();
+}
+
+/*!
+  Returns the current element.
+*/
+const SoGLColorIndexElement *
+SoGLColorIndexElement::getInstance(SoState *state)
+{
+  return (const SoGLColorIndexElement *)
+    state->getElementNoPush(classStackIndex);
+}
