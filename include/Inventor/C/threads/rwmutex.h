af632cc16cc88c044909f3361bd5aa65395ecd45
Morten Eriksen <mortene@sim.no>
2002-09-18 20:27:42 +0000

    Updates copyright header.

--- include/Inventor/C/threads/rwmutex.h
+++ include/Inventor/C/threads/rwmutex.h
@@ -4,21 +4,23 @@
 /**************************************************************************\
  *
  *  This file is part of the Coin 3D visualization library.
- *  Copyright (C) 1998-2001 by Systems in Motion.  All rights reserved.
+ *  Copyright (C) 1998-2002 by Systems in Motion.  All rights reserved.
  *
- *  This library is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.  See the file LICENSE.GPL
- *  at the root directory of this source distribution for more details.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
  *
- *  If you desire to use Coin with software that is incompatible
- *  licensewise with the GPL, and / or you would like to take
- *  advantage of the additional benefits with regard to our support
- *  services, please contact Systems in Motion about acquiring a Coin
- *  Professional Edition License.
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
  *
- *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
- *  www.sim.no, support@sim.no, Voice: +47 22114160, Fax: +47 22207097
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
  *
 \**************************************************************************/
 
--- src/engines/lex.so_eval.c
+++ src/engines/lex.so_eval.c
@@ -504,23 +504,23 @@ char *yytext;
 /**************************************************************************\
  *
  *  This file is part of the Coin 3D visualization library.
- *  Copyright (C) 1998-2001 by Systems in Motion.  All rights reserved.
- *  
+ *  Copyright (C) 1998-2002 by Systems in Motion.  All rights reserved.
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
- *  version 2 as published by the Free Software Foundation.  See the
- *  file LICENSE.GPL at the root directory of this source distribution
- *  for more details.
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
  *
- *  If you desire to use Coin with software that is incompatible
- *  licensewise with the GPL, and / or you would like to take
- *  advantage of the additional benefits with regard to our support
- *  services, please contact Systems in Motion about acquiring a Coin
- *  Professional Edition License.  See <URL:http://www.coin3d.org> for
- *  more information.
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
  *
- *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY
- *  <URL:http://www.sim.no>, <mailto:support@sim.no>
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
  *
 \**************************************************************************/
 
