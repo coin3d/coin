6cc47a66ec3282618f33c5f63acd8d6f9a9034ae
Morten Eriksen <mortene@sim.no>
2002-09-19 12:37:06 +0000

    Cleaned up the interaction with OpenGL by using the functionality of the cc_glglue wrapper.

--- src/elements/SoGLPolygonOffsetElement.cpp
+++ src/elements/SoGLPolygonOffsetElement.cpp
@@ -30,13 +30,13 @@
 */
 
 #include <Inventor/elements/SoGLPolygonOffsetElement.h>
-#include <Inventor/elements/SoGLCacheContextElement.h>
+#include <Inventor/errors/SoDebugError.h>
 
 #if HAVE_CONFIG_H
 #include <config.h>
 #endif // HAVE_CONFIG_H
 
-#include <Inventor/system/gl.h>
+#include <Inventor/misc/SoGL.h>
 
 #include <assert.h>
 #include <stdlib.h>
@@ -129,92 +129,30 @@ SoGLPolygonOffsetElement::setElt(float factor, float units,
 void
 SoGLPolygonOffsetElement::updategl(void)
 {
-  // To make the code below a bit more readable, we always use the
-  // COIN_SGI_USE_GLPOLYGONOFFSETEXT variable, even on non-sgi systems.
-  // It's then just set to the correct value, based on OpenGL 1.1
-  // detection.
-
-  // The reason for all this mess is that some SGI OpenGL
-  // implementations report OpenGL 1.1 without supporting
-  // glPolygonOffset 100%. And, we generally don't want to link
-  // against glPolygonOffsetEXT if OpenGL 1.1 is detected, since this
-  // method does not exist on most OpenGL 1.1 drivers (e.g. NVidia).
-
-  // Therefore, we only compile in glPolygonOffsetEXT code if OpenGL
-  // 1.1 is not detected, or if we're compiling on the SGI
-  // platform. SGI are not likely to drop support for
-  // glPolygonOffsetEXT since it is a frequently used extension.
-  // pederb, 2001-06-26
-
-  static int COIN_SGI_USE_GLPOLYGONOFFSETEXT = -1;
-  if (COIN_SGI_USE_GLPOLYGONOFFSETEXT < 0) {
-#if !GL_VERSION_1_1 // if not GL 1.1, always use EXT
-    COIN_SGI_USE_GLPOLYGONOFFSETEXT = 1;
-#else // GL_VERSION_1_1
-    COIN_SGI_USE_GLPOLYGONOFFSETEXT = 0; // normally, use OpenGL 1.1
-    // only test environment variable on the sgi platform
-#ifdef __sgi
-    // FIXME: I believe the above define is ugly and lousy
-    // coding. Shouldn't we runtime-check the OpenGL vendor string
-    // instead? 20010821 mortene.
-    const char * env = 
-      (const char *) coin_getenv("COIN_SGI_USE_GLPOLYGONOFFSETEXT");
-    if (env) COIN_SGI_USE_GLPOLYGONOFFSETEXT = atoi(env);
-#endif // __sgi
-#endif // GL_VERSION_1_1
+  const cc_glglue * w = sogl_glue_instance(this->state);
+  if (!cc_glglue_has_polygon_offset(w)) {
+    static SbBool first = TRUE;
+    if (first) {
+      SoDebugError::postWarning("SoGLPolygonOffsetElement::updategl",
+                                "OpenGL driver doesn't support z-buffer "
+                                "offsetting");
+      first = FALSE;
+    }
+    return;
   }
 
-#if GL_EXT_polygon_offset
-  static int polygon_offset_ext_id = -1;
-  if (polygon_offset_ext_id == -1) {
-    polygon_offset_ext_id =
-      SoGLCacheContextElement::getExtID("GL_EXT_polygon_offset");
-  }
-#endif // GL_EXT_polygon_offset
 
   if (this->active) {
-#if GL_VERSION_1_1
-    if (!COIN_SGI_USE_GLPOLYGONOFFSETEXT) {
-      if (this->style & FILLED) glEnable(GL_POLYGON_OFFSET_FILL);
-      else glDisable(GL_POLYGON_OFFSET_FILL);
-      if (this->style & LINES) glEnable(GL_POLYGON_OFFSET_LINE);
-      else glDisable(GL_POLYGON_OFFSET_LINE);        
-      if (this->style & POINTS) glEnable(GL_POLYGON_OFFSET_POINT);
-      else glDisable(GL_POLYGON_OFFSET_POINT);
-      glPolygonOffset(this->offsetfactor,
-                      this->offsetunits);
-    }
-#endif // GL_VERSION_1_1
+    int styles = 0;
+    styles |= (this->style & FILLED) ? cc_glglue_FILLED : 0;
+    styles |= (this->style & LINES) ? cc_glglue_LINES : 0;
+    styles |= (this->style & POINTS) ? cc_glglue_POINTS : 0;
+    cc_glglue_glPolygonOffsetEnable(w, TRUE, styles);
 
-#if GL_EXT_polygon_offset && (!GL_VERSION_1_1 || defined(__sgi))
-    if (COIN_SGI_USE_GLPOLYGONOFFSETEXT &&
-        SoGLCacheContextElement::extSupported(this->state, polygon_offset_ext_id)) {
-      // FIXME: this value (0.0000001) a hack to make it look
-      // ok on old SGI HW
-
-      // try to detect if user attempted to specify a bias, and not units
-      SbBool isbias = this->offsetunits > 0.0f && this->offsetunits < 0.01f;
-      glPolygonOffsetEXT(this->offsetfactor,
-                         isbias ? this->offsetunits : 0.000001);
-      if (this->style & FILLED) glEnable(GL_POLYGON_OFFSET_EXT);
-      else glDisable(GL_POLYGON_OFFSET_EXT);
-    }
-#endif // GL_EXT_polygon_offset && (!GL_VERSION_1_1 || defined(__sgi)
+    cc_glglue_glPolygonOffset(w, this->offsetfactor, this->offsetunits);
   }
   else { // ! active
-#if GL_VERSION_1_1
-    if (!COIN_SGI_USE_GLPOLYGONOFFSETEXT) {
-      glDisable(GL_POLYGON_OFFSET_FILL);
-      glDisable(GL_POLYGON_OFFSET_LINE);
-      glDisable(GL_POLYGON_OFFSET_POINT);
-    }
-#endif // GL_VERSION_1_1
-
-#if GL_EXT_polygon_offset && (!GL_VERSION_1_1 || defined(__sgi))
-    if (COIN_SGI_USE_GLPOLYGONOFFSETEXT &&
-        SoGLCacheContextElement::extSupported(this->state, polygon_offset_ext_id)) {
-      glDisable(GL_POLYGON_OFFSET_EXT);
-    }
-#endif // GL_EXT_polygon_offset && (!GL_VERSION_1_1 || defined(__sgi))
+    int all = cc_glglue_FILLED | cc_glglue_LINES | cc_glglue_POINTS;
+    cc_glglue_glPolygonOffsetEnable(w, FALSE, all);
   }
 }
