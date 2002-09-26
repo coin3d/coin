86111d7546502065db60644e4940875f8fdf8ba5
Peder Blekken <pederb@sim.no>
2002-09-26 12:13:29 +0000

    Support for cc_storage_apply_to_all().

--- include/Inventor/threads/SbStorage.h
+++ include/Inventor/threads/SbStorage.h
@@ -34,6 +34,9 @@ public:
   ~SbStorage(void) { cc_storage_destruct(this->storage); }
 
   void * get(void) { return cc_storage_get(this->storage); }
+  void applyToAll(cc_storage_apply_func * func, void * closure) {
+    cc_storage_apply_to_all(this->storage, func, closure);
+  }
 
 private:
   cc_storage * storage;
