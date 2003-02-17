74e647f2c279aba33c29760f2aa3dd7d189d85fb
Lars J. Aas <larsa@sim.no>
2003-02-17 13:46:47 +0000

    move GL elements to subdir

---
+++ src/elements/GL/all-cpp.cpp
@@ -0,0 +1,58 @@
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+#include "SoGLAmbientColorElement.cpp"
+#include "SoGLCacheContextElement.cpp"
+#include "SoGLClipPlaneElement.cpp"
+#include "SoGLColorIndexElement.cpp"
+#include "SoGLCoordinateElement.cpp"
+#include "SoGLDiffuseColorElement.cpp"
+#include "SoGLDisplayList.cpp"
+#include "SoGLDrawStyleElement.cpp"
+#include "SoGLEmissiveColorElement.cpp"
+#include "SoGLEnvironmentElement.cpp"
+#include "SoGLLazyElement.cpp"
+#include "SoGLLightIdElement.cpp"
+#include "SoGLLightModelElement.cpp"
+#include "SoGLLinePatternElement.cpp"
+#include "SoGLLineWidthElement.cpp"
+#include "SoGLModelMatrixElement.cpp"
+#include "SoGLNormalElement.cpp"
+#include "SoGLNormalizeElement.cpp"
+#include "SoGLPointSizeElement.cpp"
+#include "SoGLPolygonOffsetElement.cpp"
+#include "SoGLPolygonStippleElement.cpp"
+#include "SoGLProjectionMatrixElement.cpp"
+#include "SoGLRenderPassElement.cpp"
+#include "SoGLShadeModelElement.cpp"
+#include "SoGLShapeHintsElement.cpp"
+#include "SoGLShininessElement.cpp"
+#include "SoGLSpecularColorElement.cpp"
+#include "SoGLTextureCoordinateElement.cpp"
+#include "SoGLTextureEnabledElement.cpp"
+#include "SoGLTexture3EnabledElement.cpp"
+#include "SoGLTextureImageElement.cpp"
+#include "SoGLTextureMatrixElement.cpp"
+#include "SoGLUpdateAreaElement.cpp"
+#include "SoGLViewingMatrixElement.cpp"
+#include "SoGLViewportRegionElement.cpp"
