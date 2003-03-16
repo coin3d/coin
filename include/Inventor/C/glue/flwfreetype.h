20310fa441f3e558a55ae6d8b7778158c0cf06d1
Morten Eriksen <mortene@sim.no>
2003-03-16 14:19:02 +0000

    Clean up layout of include files, removes C++ comments, adds copyright headers...

--- include/Inventor/C/glue/flwfreetype.h
+++ include/Inventor/C/glue/flwfreetype.h
@@ -1,5 +1,30 @@
-#ifndef FLW_FREETYPE_H
-#define FLW_FREETYPE_H
+#ifndef COIN_FLWFREETYPE_H
+#define COIN_FLWFREETYPE_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+#include <Inventor/C/glue/fontlib_wrapper.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -12,61 +37,31 @@ extern "C" {
     the FreeType font library.
   */
   
-#include <Inventor/C/glue/fontlib_wrapper.h>
-  
-  int
-  flwftInitialize();
-  
-  void
-  flwftExit();
-  
-  FLWfont 
-  flwftGetFont(const char * fontname);
-  
-  int
-  flwftGetFontName(FLWfont font, char * buffer, int bufsize);
+  int flwftInitialize(void);
+  void flwftExit(void);
 
-  int
-  flwftGetFontStyle(FLWfont font, char * buffer, int bufsize);
+  FLWfont flwftGetFont(const char * fontname);
+  int flwftGetFontName(FLWfont font, char * buffer, int bufsize);
+  int flwftGetFontStyle(FLWfont font, char * buffer, int bufsize);
+  void flwftDoneFont(FLWfont font);
 
-  void 
-  flwftDoneFont(FLWfont font);
-  
-  int
-  flwftGetNumCharmaps(FLWfont font);
-  
-  int
-  flwftGetCharmapName(FLWfont font, int charmap, char * buffer, int bufsize);
-  
-  int 
-  flwftSetCharmap(FLWfont font, int charmap);
-  
-  int
-  flwftSetCharSize(FLWfont font, int width, int height);
-  
-  int
-  flwftSetFontRotation(FLWfont font, float angle);
-  
-  FLWglyph
-  flwftGetGlyph(FLWfont font, unsigned int charidx);
-  
-  int
-  flwftGetAdvance(FLWfont font, FLWglyph glyph, float *x, float *y);
-  
-  int
-  flwftGetKerning(FLWfont font, FLWglyph glyph1, FLWglyph glyph2, float *x, float *y);
-  
-  void 
-  flwftDoneGlyph(FLWfont font, FLWglyph glyph);
-  
-  FLWbitmap *
-  flwftGetBitmap(FLWfont font, FLWglyph glyph);
-  
-  int 
-  flwftGetOutline(FLWfont font, FLWglyph glyph);
-  
+  int flwftGetNumCharmaps(FLWfont font);
+  int flwftGetCharmapName(FLWfont font, int charmap, char * buffer, int bufsize);
+  int flwftSetCharmap(FLWfont font, int charmap);
+
+  int flwftSetCharSize(FLWfont font, int width, int height);
+  int flwftSetFontRotation(FLWfont font, float angle);
+
+  FLWglyph flwftGetGlyph(FLWfont font, unsigned int charidx);
+  int flwftGetAdvance(FLWfont font, FLWglyph glyph, float *x, float *y);
+  int flwftGetKerning(FLWfont font, FLWglyph glyph1, FLWglyph glyph2, float *x, float *y);
+  void flwftDoneGlyph(FLWfont font, FLWglyph glyph);
+
+  FLWbitmap * flwftGetBitmap(FLWfont font, FLWglyph glyph);
+  int flwftGetOutline(FLWfont font, FLWglyph glyph);
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // FLW_FREETYPE_H
+#endif /* !COIN_FLWFREETYPE_H */
--- include/Inventor/C/glue/fontlib_wrapper.h
+++ include/Inventor/C/glue/fontlib_wrapper.h
@@ -1,5 +1,28 @@
-#ifndef FLW_H
-#define FLW_H
+#ifndef COIN_FLW_H
+#define COIN_FLW_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,9 +40,6 @@ extern "C" {
 
     See http://www.freetype.org for more information about
     the FreeType font library.
-    
-    Search the net for "libFL" for an example of a font 
-    library wrapper between SGI OIV and FreeType.
   */
 
   
@@ -27,73 +47,43 @@ extern "C" {
   typedef int FLWglyph;
   
   typedef struct FLWbitmap {
-    int bearingX;     // left side of bitmap relative to pen
-    int bearingY;     // top of bitmap relative to pen
-    unsigned int rows;         // height of bitmap
-    unsigned int width;        // width of bitmap
-    unsigned int pitch;        // number of bytes occupied by each row (rows are padded to nearest byte)
-    int advanceX;     // where to position pen for next glyph
+    int bearingX; /* left side of bitmap relative to pen */
+    int bearingY; /* top of bitmap relative to pen */
+    unsigned int rows; /* height of bitmap */
+    unsigned int width; /* width of bitmap */
+    unsigned int pitch; /* number of bytes occupied by each row (rows are padded to nearest byte) */
+    int advanceX; /* where to position pen for next glyph */
     int advanceY;
-    unsigned char * buffer;    // bitmap data
+    unsigned char * buffer; /* bitmap data */
   } FLWbitmap;
-  
-  int
-  flwInitialize();
-  
-  void
-  flwExit();
-  
-  int 
-  flwCreateFont(const char * fontname, char * outname, const int outnamelen, const int sizex, const int sizey);
-  
-  int 
-  flwGetFont(const char * fontname, const int sizex, const int ysizey);
-  
-  void 
-  flwDoneFont(int font);
-  
-  int
-  flwGetNumCharmaps(int font);
-  
-  int
-  flwGetCharmapName(int font, int charmap, char * buffer, int bufsize);
-  
-  int 
-  flwSetCharmap(int font, int charmap);
-  
-  int
-  flwSetCharSize(int font, int width, int height);
-  
-  int
-  flwGetFontName(int font, char * buffer, int bufsize);
-  
-  int
-  flwSetFontRotation(int font, float angle);
-  
-  int
-  flwGetGlyph(int font, int charidx);
-  
-  int
-  flwGetAdvance(int font, int glyph, float *x, float *y);
-  
-  int
-  flwGetKerning(int font, int glyph1, int glyph2, float *x, float *y);
-  
-  void 
-  flwDoneGlyph(int font, int glyph);
-  
-  FLWbitmap *
-  flwGetBitmap(int font, int glyph);
-  
-  void
-  flwDoneBitmap(FLWbitmap * bitmap);
-  
-  int 
-  flwGetOutline(int font, int glyph);
-  
-  
+
+int flwInitialize(void);
+void flwExit(void);
+
+int flwCreateFont(const char * fontname, char * outname, const int outnamelen, const int sizex, const int sizey);
+  int flwGetFont(const char * fontname, const int sizex, const int ysizey);
+  void flwDoneFont(int font);
+
+  int flwGetNumCharmaps(int font);
+  int flwGetCharmapName(int font, int charmap, char * buffer, int bufsize);
+  int flwSetCharmap(int font, int charmap);
+
+  int flwSetCharSize(int font, int width, int height);
+  int flwGetFontName(int font, char * buffer, int bufsize);
+  int flwSetFontRotation(int font, float angle);
+
+  int flwGetGlyph(int font, int charidx);
+  int flwGetAdvance(int font, int glyph, float *x, float *y);
+  int flwGetKerning(int font, int glyph1, int glyph2, float *x, float *y);
+  void flwDoneGlyph(int font, int glyph);
+
+  FLWbitmap * flwGetBitmap(int font, int glyph);
+  void flwDoneBitmap(FLWbitmap * bitmap);
+
+  int flwGetOutline(int font, int glyph);
+
 #ifdef __cplusplus
 }
 #endif
-  
-#endif  // FLW_H
+
+#endif /* !COIN_FLW_H */
