35a4e973da0d34fa7c6e6656f5fb0fac866b4eec
Morten Eriksen <mortene@sim.no>
2003-04-03 08:08:38 +0000

    Complete set of inclusions. Problem reported by Josh Flowers.

--- include/Inventor/So.h
+++ include/Inventor/So.h
@@ -26,11 +26,39 @@
 
 #include <Inventor/SoDB.h>
 #include <Inventor/SoInput.h>
-#include <Inventor/SoLists.h>
-#include <Inventor/errors/SoDebugError.h>
+#include <Inventor/SoOffscreenRenderer.h>
+#include <Inventor/SoOutput.h>
+#include <Inventor/SoPath.h>
+#include <Inventor/SoPickedPoint.h>
+#include <Inventor/SoPrimitiveVertex.h>
+
 #include <Inventor/misc/SoState.h>
+
+#include <Inventor/SoLists.h>
+#include <Inventor/actions/SoActions.h>
+#include <Inventor/details/SoDetails.h>
 #include <Inventor/elements/SoElements.h>
+#include <Inventor/engines/SoEngines.h>
+#include <Inventor/errors/SoErrors.h>
+#include <Inventor/events/SoEvents.h>
 #include <Inventor/fields/SoFields.h>
 #include <Inventor/nodes/SoNodes.h>
+#include <Inventor/sensors/SoSensorManager.h>
+#include <Inventor/sensors/SoSensors.h>
+
+// We choose to not include classes that are "mostly internal":
+// bundles, caches, elements, and projectors.
+//
+// Base classes are not included. They should for the most part be
+// included by the other classes using them.
+//
+// Classes for interaction is not included either, as they can be seen
+// as a later add-on to the core library, and it's not unlikely that
+// we'll eventually make their inclusion a configure/build
+// option. That goes for nodekits, draggers, and manips.
+//
+// The library can be built without VRML97 nodes, and without the
+// thread abstraction classes, so they are also not part of this
+// header file.
 
 #endif // !COIN_SO_H
