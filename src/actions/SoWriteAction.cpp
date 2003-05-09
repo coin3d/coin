ebe1e28c9e1c5daf9f72e874cebe65d97b6e8b3d
Morten Eriksen <mortene@sim.no>
2003-05-09 08:07:37 +0000

    Doxygen API doc additions.

--- src/actions/SoWriteAction.cpp
+++ src/actions/SoWriteAction.cpp
@@ -40,6 +40,60 @@
   SoDB::readAll() method (SoDB also contains a few other import
   methods you can use).
 
+  Here's a complete, stand-alone usage example which shows how to
+  write a scene graph to a memory buffer:
+
+  \code
+  #include <Inventor/SoDB.h>
+  #include <Inventor/actions/SoWriteAction.h>
+  #include <Inventor/nodes/SoCone.h>
+  #include <Inventor/nodes/SoSeparator.h>
+  
+  static char * buffer;
+  static size_t buffer_size = 0;
+  
+  static void *
+  buffer_realloc(void * bufptr, size_t size)
+  {
+    buffer = (char *)realloc(bufptr, size);
+    buffer_size = size;
+    return buffer;
+  }
+  
+  static SbString
+  buffer_writeaction(SoNode * root)
+  {
+    SoOutput out;
+    buffer = (char *)malloc(1024);
+    buffer_size = 1024;
+    out.setBuffer(buffer, buffer_size, buffer_realloc);
+  
+    SoWriteAction wa(&out);
+    wa.apply(root);
+  
+    SbString s(buffer);
+    free(buffer);
+    return s;
+  }
+  
+  int
+  main(int argc, char ** argv)
+  {
+    SoDB::init();
+  
+    SoSeparator * root = new SoSeparator;
+    root->ref();
+  
+    root->addChild(new SoCone);
+  
+    SbString s = buffer_writeaction(root);
+    (void)fprintf(stdout, "%s\n", s.getString());
+  
+    root->unref();
+    return 0;
+  }
+  \endcode
+
   \sa SoOutput
 */
 
--- src/misc/SoOutput.cpp
+++ src/misc/SoOutput.cpp
@@ -359,6 +359,60 @@ SoOutput::closeFile(void)
   make it a habit to always use getBuffer() to retrieve the memory
   buffer pointer after write operations.
 
+  Here's a complete, stand-alone usage example which shows how to
+  write a scene graph to a memory buffer:
+
+  \code
+  #include <Inventor/SoDB.h>
+  #include <Inventor/actions/SoWriteAction.h>
+  #include <Inventor/nodes/SoCone.h>
+  #include <Inventor/nodes/SoSeparator.h>
+  
+  static char * buffer;
+  static size_t buffer_size = 0;
+  
+  static void *
+  buffer_realloc(void * bufptr, size_t size)
+  {
+    buffer = (char *)realloc(bufptr, size);
+    buffer_size = size;
+    return buffer;
+  }
+  
+  static SbString
+  buffer_writeaction(SoNode * root)
+  {
+    SoOutput out;
+    buffer = (char *)malloc(1024);
+    buffer_size = 1024;
+    out.setBuffer(buffer, buffer_size, buffer_realloc);
+  
+    SoWriteAction wa(&out);
+    wa.apply(root);
+  
+    SbString s(buffer);
+    free(buffer);
+    return s;
+  }
+  
+  int
+  main(int argc, char ** argv)
+  {
+    SoDB::init();
+  
+    SoSeparator * root = new SoSeparator;
+    root->ref();
+  
+    root->addChild(new SoCone);
+  
+    SbString s = buffer_writeaction(root);
+    (void)fprintf(stdout, "%s\n", s.getString());
+  
+    root->unref();
+    return 0;
+  }
+  \endcode
+
   \sa getBuffer(), getBufferSize(), resetBuffer()
 */
 void
