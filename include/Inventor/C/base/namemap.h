e73f75e30131bd7f788db07795a62d8dac29f963
Morten Eriksen <mortene@sim.no>
2003-06-08 12:54:40 +0000

    C ADT for a name hash map with permanent string addresses.

---
+++ include/Inventor/C/base/namemap.h
@@ -0,0 +1,54 @@
+#ifndef COIN_NAMEMAP_H
+#define COIN_NAMEMAP_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+/* This is the interface for a C ADT for unique string mappings. */
+
+/*************************************************************************/
+
+#ifndef COIN_INTERNAL
+#error Only for internal use.
+#endif /* COIN_INTERNAL */
+
+/*************************************************************************/
+
+#include <Inventor/C/basic.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* ********************************************************************** */
+
+  const char * cc_namemap_get_address(const char * str);
+  const char * cc_namemap_peek_string(const char * str);
+
+/* ********************************************************************** */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* ! COIN_NAMEMAP_H */
