66e188f2818eeeba677973fde237deaaaeb6f677
Lars J. Aas <larsa@sim.no>
2003-10-17 11:41:27 +0000

    doc fix

--- src/collision/SoIntersectionDetectionAction.cpp
+++ src/collision/SoIntersectionDetectionAction.cpp
@@ -96,6 +96,18 @@
   \since TGS Inventor 2.4
 */
 
+/*!
+  \page intersection_overview Intersection Detection
+
+  See the documentation for the SoIntersectionDetectionAction class
+  for example code on how to do intersection detection in a Coin scene.
+
+  \sa SoIntersectionDetectionAction
+
+  \since Coin 2.1
+  \since TGS Inventor 2.4
+*/
+
 // FIXME: intersection with lines not implemented yet. 20030507 mortene.
 
 #include <Inventor/collision/SoIntersectionDetectionAction.h>
