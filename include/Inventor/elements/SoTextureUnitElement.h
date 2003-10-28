8f6e2e4ccbb9e04b4fea20f58320a001dbf4e064
Peder Blekken <pederb@sim.no>
2003-10-28 11:34:28 +0000

    SoTextureUnitElement implementation.

--- include/Inventor/elements/SoTextureUnitElement.h
+++ include/Inventor/elements/SoTextureUnitElement.h
@@ -1,1 +1,53 @@
-// soon to come
+#ifndef COIN_SOTEXTUREUNITELEMENT_H
+#define COIN_SOTEXTUREUNITELEMENT_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+#include <Inventor/elements/SoInt32Element.h>
+
+class SoTextureUnitElementP;
+
+class COIN_DLL_API SoTextureUnitElement : public SoInt32Element {
+  typedef SoInt32Element inherited;
+
+  SO_ELEMENT_HEADER(SoTextureUnitElement);
+public:
+  static void initClass(void);
+protected:
+  virtual ~SoTextureUnitElement();
+
+public:
+
+  virtual void init(SoState * state);
+  static void set(SoState * const state, SoNode * const node,
+                  const int units);
+  static int get(SoState * state);
+  
+private:
+  // just in case it is needed at a later stage
+  SoTextureUnitElementP * pimpl;
+};
+
+#endif // !COIN_SOTEXTUREUNITELEMENT_H
+
--- src/elements/SoTextureUnitElement.cpp
+++ src/elements/SoTextureUnitElement.cpp
@@ -1,1 +1,84 @@
-// soon to come
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+/*!
+  \class SoTextureUnitElement Inventor/elements/SoTextureUnitElement.h
+  \brief The SoTextureUnitElement class is yet to be documented.
+  \ingroup elements
+
+  FIXME: write doc.
+
+  \since 2003-10-27
+*/
+
+#include <Inventor/elements/SoTextureUnitElement.h>
+#include <assert.h>
+
+SO_ELEMENT_SOURCE(SoTextureUnitElement);
+
+/*!
+  This static method initializes static data for the SoTextureUnitElement class.
+*/
+
+void
+SoTextureUnitElement::initClass(void)
+{
+  SO_ELEMENT_INIT_CLASS(SoTextureUnitElement, inherited);
+}
+
+/*!
+  The destructor.
+*/
+
+SoTextureUnitElement::~SoTextureUnitElement(void)
+{
+}
+
+//! FIXME: write doc.
+
+void
+SoTextureUnitElement::set(SoState * const state,
+                          SoNode * const node,
+                          const int unit)
+{
+  SoInt32Element::set(classStackIndex, state, node, unit);
+}
+
+//! FIXME: write doc.
+
+void
+SoTextureUnitElement::init(SoState * state)
+{
+  inherited::init(state);
+  this->data = 0;
+}
+
+//! FIXME: write doc.
+
+int
+SoTextureUnitElement::get(SoState * const state)
+{
+  return (int) SoInt32Element::get(classStackIndex, state);
+}
+
+
