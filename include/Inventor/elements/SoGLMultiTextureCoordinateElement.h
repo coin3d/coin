d1ed1f844c3cc07a8fb005eba0ec9e6cda3931f8
Peder Blekken <pederb@sim.no>
2003-11-03 13:57:59 +0000

    SoGLMultiTextureCoordinateElement implementation.

--- include/Inventor/elements/SoGLMultiTextureCoordinateElement.h
+++ include/Inventor/elements/SoGLMultiTextureCoordinateElement.h
@@ -1,1 +1,78 @@
-// soon to come
+#ifndef COIN_SOGLMULTITEXTURECOORDINATEELEMENT_H
+#define COIN_SOGLMULTITEXTURECOORDINATEELEMENT_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+#include <Inventor/elements/SoMultiTextureCoordinateElement.h>
+#include <Inventor/elements/SoGLTextureCoordinateElement.h>
+
+class SoGLMultiTextureCoordinateElementP;
+
+class COIN_DLL_API SoGLMultiTextureCoordinateElement : public SoMultiTextureCoordinateElement {
+  typedef SoMultiTextureCoordinateElement inherited;
+
+  SO_ELEMENT_HEADER(SoGLMultiTextureCoordinateElement);
+public:
+  static void initClass(void);
+protected:
+  virtual ~SoGLMultiTextureCoordinateElement();
+
+public:
+  virtual void init(SoState * state);
+  virtual void push(SoState * state);
+  virtual void pop(SoState * state,
+                   const SoElement * prevTopElement);
+
+  static  void setTexGen(SoState * const state, SoNode * const node,
+                         const int unit,
+                         SoTexCoordTexgenCB * const texgenFunc,
+                         void * const texgenData = NULL,
+                         SoTextureCoordinateFunctionCB * const func = NULL,
+                         void * const funcData = NULL);
+
+  virtual SoTextureCoordinateElement::CoordType getType(const int unit) const;
+
+  static const SoGLMultiTextureCoordinateElement * getInstance(SoState * const state);
+
+  void send(const int unit, const int index) const;
+  void send(const int unit, const int index, const SbVec3f &c, const SbVec3f &n) const;
+
+  class GLUnitData {
+  public:
+    SoTexCoordTexgenCB * texgenCB;
+    void * texgenData;
+  };
+
+protected:
+  virtual void setElt(const int unit,
+                      SoTexCoordTexgenCB *func,
+                      void *data = NULL);
+
+private:
+  void doCallback(const int unit) const;
+  SoGLMultiTextureCoordinateElementP * pimpl;
+};
+
+#endif // !COIN_SOGLMULTITEXTURECOORDINATEELEMENT_H
+
--- src/elements/GL/SoGLMultiTextureCoordinateElement.cpp
+++ src/elements/GL/SoGLMultiTextureCoordinateElement.cpp
@@ -1,1 +1,335 @@
-// soon to come
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+/*!
+  \class SoGLMultiTextureCoordinateElement Inventor/elements/SoGLMultiTextureCoordinateElement.h
+  \brief The SoGLMultiTextureCoordinateElement class stores the current gltexture coordinates for several units.
+  \ingroup elements
+*/
+
+#include <Inventor/elements/SoGLMultiTextureCoordinateElement.h>
+#include <Inventor/actions/SoGLRenderAction.h>
+#include <Inventor/misc/SoState.h>
+
+#include <assert.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif // HAVE_CONFIG_H
+
+#include <Inventor/system/gl.h>
+#include <Inventor/C/glue/gl.h>
+
+#define MAX_UNITS 16 // FIXME: make dynamic? pederb, 2003-11-03
+
+class SoGLMultiTextureCoordinateElementP {
+public:
+  SoGLMultiTextureCoordinateElement::GLUnitData unitdata[MAX_UNITS];
+  const cc_glglue * glue;
+};
+
+#define PRIVATE(obj) obj->pimpl
+
+SO_ELEMENT_CUSTOM_CONSTRUCTOR_SOURCE(SoGLMultiTextureCoordinateElement);
+
+/*!
+  This static method initializes static data for the
+  SoGLMultiTextureCoordinateElement class.
+*/
+
+void
+SoGLMultiTextureCoordinateElement::initClass(void)
+{
+  SO_ELEMENT_INIT_CLASS(SoGLMultiTextureCoordinateElement, inherited);
+}
+
+/*!
+  The constructor.
+*/
+SoGLMultiTextureCoordinateElement::SoGLMultiTextureCoordinateElement(void)
+{
+  PRIVATE(this) = new SoGLMultiTextureCoordinateElementP;
+
+  this->setTypeId(SoGLMultiTextureCoordinateElement::classTypeId);
+  this->setStackIndex(SoGLMultiTextureCoordinateElement::classStackIndex);
+}
+
+/*!
+  The destructor.
+*/
+
+SoGLMultiTextureCoordinateElement::~SoGLMultiTextureCoordinateElement()
+{
+  delete PRIVATE(this);
+}
+
+//!  FIXME: write doc.
+
+void
+SoGLMultiTextureCoordinateElement::init(SoState * state)
+{
+  SoAction * action = state->getAction();
+  assert(action->isOfType(SoGLRenderAction::getClassTypeId()));
+  // fetch cache context from action since SoGLCacheContextElement
+  // might not be initialized yet.
+  SoGLRenderAction * glaction = (SoGLRenderAction*) action;
+  PRIVATE(this)->glue = cc_glglue_instance(glaction->getCacheContext());
+
+  inherited::init(state);
+  for (int i = 0; i < MAX_UNITS; i++) {
+    GLUnitData & ud = PRIVATE(this)->unitdata[i];
+    ud.texgenCB = NULL;
+    ud.texgenData = NULL;
+  }
+}
+
+//!  FIXME: write doc.
+
+void
+SoGLMultiTextureCoordinateElement::push(SoState * state)
+{
+  inherited::push(state);
+  SoGLMultiTextureCoordinateElement * prev = (SoGLMultiTextureCoordinateElement*)this->getNextInStack();
+
+  PRIVATE(this)->glue = PRIVATE(prev)->glue;
+  for (int i = 0; i < MAX_UNITS; i++) {
+    PRIVATE(this)->unitdata[i] = PRIVATE(prev)->unitdata[i];
+  }
+  // capture previous element since we might or might not change the
+  // GL state in set/pop
+  prev->capture(state);
+}
+
+//!  FIXME: write doc.
+
+void
+SoGLMultiTextureCoordinateElement::pop(SoState * state,
+                                       const SoElement * prevTopElement)
+{
+  inherited::pop(state, prevTopElement);
+  SoGLMultiTextureCoordinateElement * prev = (SoGLMultiTextureCoordinateElement*) prevTopElement;
+
+  for (int i = 0; i < MAX_UNITS; i++) {
+    const GLUnitData & thisud = PRIVATE(this)->unitdata[i];
+    const GLUnitData & prevud = PRIVATE(prev)->unitdata[i];
+
+    SbBool enablegen = FALSE;
+    SbBool disablegen = FALSE;
+    SbBool docallback = FALSE;
+
+    if (thisud.texgenCB && !prevud.texgenCB) {enablegen = TRUE; docallback = TRUE;}
+    else if (!thisud.texgenCB && prevud.texgenCB) disablegen = TRUE;
+    else if (thisud.texgenCB != prevud.texgenCB) docallback = TRUE;
+
+    if (enablegen || disablegen || docallback) {
+      // must change texture unit while updating OpenGL
+      cc_glglue_glActiveTexture(PRIVATE(this)->glue, (GLenum) (int(GL_TEXTURE0) + i));
+    }
+    if (enablegen) {
+      glEnable(GL_TEXTURE_GEN_S);
+      glEnable(GL_TEXTURE_GEN_T);
+      glEnable(GL_TEXTURE_GEN_R);
+      glEnable(GL_TEXTURE_GEN_Q);
+    }
+    if (disablegen) {
+      glDisable(GL_TEXTURE_GEN_S);
+      glDisable(GL_TEXTURE_GEN_T);
+      glDisable(GL_TEXTURE_GEN_R);
+      glDisable(GL_TEXTURE_GEN_Q);
+    }
+    if (docallback) {
+      this->doCallback(i);
+    }
+    // restore default unit
+    if (enablegen || disablegen || docallback) {
+      cc_glglue_glActiveTexture(PRIVATE(this)->glue, (GLenum) GL_TEXTURE0);
+    }
+  }
+}
+
+//!  FIXME: write doc.
+
+void
+SoGLMultiTextureCoordinateElement::setTexGen(SoState * const state,
+                                             SoNode * const node,
+                                             const int unit,
+                                             SoTexCoordTexgenCB * const texgenFunc,
+                                             void * const texgenData,
+                                             SoTextureCoordinateFunctionCB * const func,
+                                             void * const funcData)
+{
+  SoMultiTextureCoordinateElement::setFunction(state, node, unit, func, funcData);
+
+  SoGLMultiTextureCoordinateElement *element = (SoGLMultiTextureCoordinateElement *)
+    SoElement::getElement(state, classStackIndex);
+  if (element) {
+    element->setElt(unit, texgenFunc, texgenData);
+  }
+}
+
+//!  FIXME: write doc.
+
+SoTextureCoordinateElement::CoordType
+SoGLMultiTextureCoordinateElement::getType(const int unit) const
+{
+  assert(unit >= 0 && unit < MAX_UNITS);
+  if (PRIVATE(this)->unitdata[unit].texgenCB) return SoTextureCoordinateElement::NONE;
+  return inherited::getType(unit);
+}
+
+//!  FIXME: write doc.
+
+const SoGLMultiTextureCoordinateElement *
+SoGLMultiTextureCoordinateElement::getInstance(SoState * const state)
+{
+  return (SoGLMultiTextureCoordinateElement*)
+    SoElement::getConstElement(state, classStackIndex);
+}
+
+//!  FIXME: write doc.
+
+void
+SoGLMultiTextureCoordinateElement::send(const int unit, const int index) const
+{
+  const UnitData & ud = this->getUnitData(unit);
+  GLenum glunit = (GLenum) (int(GL_TEXTURE0) + unit);
+
+  assert(ud.whatKind == SoTextureCoordinateElement::EXPLICIT);
+  assert(index < ud.numCoords);
+  switch (ud.coordsDimension) {
+  case 2:
+    cc_glglue_glMultiTexCoord2fv(PRIVATE(this)->glue, glunit, ud.coords2[index].getValue());
+    break;
+  case 3:
+    cc_glglue_glMultiTexCoord3fv(PRIVATE(this)->glue, glunit, ud.coords3[index].getValue());
+    break;
+  case 4:
+    cc_glglue_glMultiTexCoord4fv(PRIVATE(this)->glue, glunit, ud.coords4[index].getValue());
+    break;
+  default:
+    assert(0 && "should not happen");
+    break;
+  }
+}
+
+//!  FIXME: write doc.
+
+void
+SoGLMultiTextureCoordinateElement::send(const int unit,
+                                        const int index,
+                                        const SbVec3f &c,
+                                        const SbVec3f &n) const
+{
+  const UnitData & ud = this->getUnitData(unit);
+  GLenum glunit = (GLenum) (int(GL_TEXTURE0) + unit);
+
+  if (ud.whatKind == SoTextureCoordinateElement::FUNCTION) {
+    assert(ud.funcCB);
+    const SbVec4f & tc = ud.funcCB(ud.funcCBData, c, n);
+    cc_glglue_glMultiTexCoord4fv(PRIVATE(this)->glue, glunit, tc.getValue());
+  }
+  else {
+    assert(ud.whatKind == SoTextureCoordinateElement::EXPLICIT);
+    //
+    // FIXME: these tests are just here to avoid crashes when illegal
+    // files are rendered. Will remove later, when we implement a
+    // SoVerifyGraphAction or something. pederb, 20000218
+    //
+    if (index < 0) return;
+    if (index >= ud.numCoords) return;
+
+    switch (ud.coordsDimension) {
+    case 2:
+      cc_glglue_glMultiTexCoord2fv(PRIVATE(this)->glue, glunit, ud.coords2[index].getValue());
+      break;
+    case 3:
+      cc_glglue_glMultiTexCoord3fv(PRIVATE(this)->glue, glunit, ud.coords3[index].getValue());
+      break;
+    case 4:
+      cc_glglue_glMultiTexCoord4fv(PRIVATE(this)->glue, glunit, ud.coords4[index].getValue());
+      break;
+    default:
+      assert(0 && "should not happen");
+      break;
+    }
+  }
+}
+
+//!  FIXME: write doc.
+
+void
+SoGLMultiTextureCoordinateElement::setElt(const int unit,
+                                          SoTexCoordTexgenCB * func,
+                                          void *data)
+{
+  assert(unit >= 0 && unit < MAX_UNITS);
+  GLUnitData & ud = PRIVATE(this)->unitdata[unit];
+
+  SbBool enablegen = FALSE;
+  SbBool disablegen = FALSE;
+  SbBool docallback = FALSE;
+
+  if (func && !ud.texgenCB) {enablegen = TRUE; docallback = TRUE;}
+  else if (!func && ud.texgenCB) disablegen = TRUE;
+  else if (func && func != ud.texgenCB) docallback = TRUE;
+
+  if (func) {
+    // update SoMultiTextureCoordinateElement type
+    this->getUnitData(unit).whatKind = SoTextureCoordinateElement::FUNCTION;
+  }
+  ud.texgenCB = func;
+  ud.texgenData = data;
+
+  if (enablegen || disablegen || docallback) {
+    cc_glglue_glActiveTexture(PRIVATE(this)->glue, (GLenum) (int(GL_TEXTURE0) + unit));
+  }
+
+  if (enablegen) {
+    glEnable(GL_TEXTURE_GEN_S);
+    glEnable(GL_TEXTURE_GEN_T);
+    glEnable(GL_TEXTURE_GEN_R);
+    glEnable(GL_TEXTURE_GEN_Q);
+  }
+  if (disablegen) {
+    glDisable(GL_TEXTURE_GEN_S);
+    glDisable(GL_TEXTURE_GEN_T);
+    glDisable(GL_TEXTURE_GEN_R);
+    glDisable(GL_TEXTURE_GEN_Q);
+  }
+  if (docallback) this->doCallback(unit);
+
+  if (enablegen || disablegen || docallback) {
+    cc_glglue_glActiveTexture(PRIVATE(this)->glue, (GLenum) GL_TEXTURE0);
+  }
+}
+
+void
+SoGLMultiTextureCoordinateElement::doCallback(const int unit) const
+{
+  if (PRIVATE(this)->unitdata[unit].texgenCB) {
+    PRIVATE(this)->unitdata[unit].texgenCB(PRIVATE(this)->unitdata[unit].texgenData);
+  }
+}
+
+#undef PRIVATE
+#undef MAX_UNITS
