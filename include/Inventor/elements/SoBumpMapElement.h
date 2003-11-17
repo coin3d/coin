5f7fdce2a42b68b4abedce703e5eca5c9b60ad8d
Peder Blekken <pederb@sim.no>
2003-11-17 10:51:59 +0000

    SoBumpMapElement implementation.

--- include/Inventor/elements/SoBumpMapElement.h
+++ include/Inventor/elements/SoBumpMapElement.h
@@ -1,1 +1,56 @@
-// coming soon
+#ifndef COIN_SOBUMPMAPELEMENT_H
+#define COIN_SOBUMPMAPELEMENT_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+#include <Inventor/elements/SoReplacedElement.h>
+#include <Inventor/SbVec2s.h>
+
+class SoState;
+class SoGLImage;
+
+class COIN_DLL_API SoBumpMapElement : public SoReplacedElement {
+  typedef SoReplacedElement inherited;
+
+  SO_ELEMENT_HEADER(SoBumpMapElement);
+public:
+  static void initClass(void);
+protected:
+  virtual ~SoBumpMapElement();
+
+public:
+  virtual void init(SoState * state);
+  static void set(SoState * state, SoNode * node,
+                  SoGLImage * image);
+  
+  static SoGLImage * get(SoState * const state);
+
+protected:
+  virtual void setElt(SoGLImage * image);
+
+  SoGLImage * image;
+
+};
+
+#endif // !COIN_SOBUMPMAPELEMENT_H
--- src/elements/SoBumpMapElement.cpp
+++ src/elements/SoBumpMapElement.cpp
@@ -1,1 +1,112 @@
-// coming soon
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+/*!
+  \class SoTextureImageElement Inventor/elements/SoTextureImageElement.h
+  \brief The SoTextureImageElement class is yet to be documented.
+  \ingroup elements
+
+  FIXME: write doc.
+*/
+
+#include <Inventor/elements/SoBumpMapElement.h>
+#include <assert.h>
+
+/*!
+  \fn SoBumpMapElement::size
+
+  FIXME: write doc.
+*/
+
+/*!
+  \fn SoBumpMapElement::numcomponents
+
+  FIXME: write doc.
+*/
+
+/*!
+  \fn SoBumpMapElement::bytes
+
+  FIXME: write doc.
+*/
+
+
+SO_ELEMENT_SOURCE(SoBumpMapElement);
+
+/*!
+  This static method initializes static data for the
+  SoBumpMapElement class.
+*/
+
+void
+SoBumpMapElement::initClass(void)
+{
+  SO_ELEMENT_INIT_CLASS(SoBumpMapElement, inherited);
+}
+
+/*!
+  The destructor.
+*/
+
+SoBumpMapElement::~SoBumpMapElement()
+{
+}
+
+//! FIXME: write doc.
+
+void
+SoBumpMapElement::init(SoState * state)
+{
+  inherited::init(state);
+  this->image = NULL;
+}
+
+//! FIXME: write doc.
+
+void
+SoBumpMapElement::set(SoState * state, SoNode * node,
+                      SoGLImage * image)
+{
+  SoBumpMapElement * elem = (SoBumpMapElement *)
+    SoReplacedElement::getElement(state, classStackIndex, node);
+  elem->setElt(image);
+}
+
+//! FIXME: write doc.
+
+SoGLImage *
+SoBumpMapElement::get(SoState * state)
+{
+  SoBumpMapElement * elem = (SoBumpMapElement *)
+    state->getConstElement(classStackIndex);
+
+  return elem->image;
+}
+
+//! FIXME: write doc.
+
+void
+SoBumpMapElement::setElt(SoGLImage * image)
+{
+  this->image = image;
+}
