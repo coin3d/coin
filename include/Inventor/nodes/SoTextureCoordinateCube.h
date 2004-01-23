179ae54b6a55cf967c1fd083dc22d7b77e5ac0cd
?ystein Handegard <handegar@sim.no>
2004-01-23 16:03:47 +0000

    First version of texture coordinate generation using cubemapping.

--- include/Inventor/nodes/SoTextureCoordinateCube.h
+++ include/Inventor/nodes/SoTextureCoordinateCube.h
@@ -1,1 +1,52 @@
-// coming soon
+#ifndef COIN_SOTEXTURECOORDINATECUBE_H
+#define COIN_SOTEXTURECOORDINATECUBE_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2004 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+#include <Inventor/nodes/SoSubNode.h>
+
+class COIN_DLL_API SoTextureCoordinateCube : public SoNode {
+  typedef SoNode inherited;
+
+  SO_NODE_HEADER(SoTextureCoordinateCube);
+
+public:
+  static void initClass(void);
+  SoTextureCoordinateCube(void);
+
+  // FIXME: Add the origo as a public field? (20040122 handegar)
+
+  virtual void doAction(SoAction * action);
+  virtual void GLRender(SoGLRenderAction * action);
+  virtual void callback(SoCallbackAction * action);
+  virtual void pick(SoPickAction * action);
+
+protected:
+  virtual ~SoTextureCoordinateCube();
+  class SoTextureCoordinateCubeP * pimpl;
+  friend class SoTextureCoordinateCubeP;
+
+};
+
+#endif // !COIN_SOTEXTURECOORDINATECUBE_H
--- src/nodes/SoTextureCoordinateCube.cpp
+++ src/nodes/SoTextureCoordinateCube.cpp
@@ -1,1 +1,211 @@
-// coming soon
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2004 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+#include <Inventor/nodes/SoTextureCoordinateCube.h>
+#include <Inventor/nodes/SoSubNodeP.h>
+
+#include <Inventor/actions/SoGLRenderAction.h>
+#include <Inventor/elements/SoGLTextureCoordinateElement.h>
+#include <Inventor/elements/SoGLMultiTextureCoordinateElement.h>
+#include <Inventor/elements/SoTextureUnitElement.h>
+#include <Inventor/actions/SoCallbackAction.h>
+#include <Inventor/actions/SoPickAction.h>
+#include <Inventor/misc/SoState.h>
+#include <Inventor/nodes/SoNode.h>
+#include <Inventor/nodes/SoShape.h>
+#include <Inventor/SbBox3f.h>
+#include <Inventor/SoFullPath.h>
+
+/*!
+  \class SoTextureCoordinateCube include/Inventor/nodes/SoTextureCoordinateCube.h
+  \brief The SoTextureCoordinateCube class autogenerates cubemapped texture coordinated for shapes.
+  \ingroup bundles
+
+  The cube used for reference when mapping is the boundingbox for the shape.
+*/
+
+// FIXME: Add a better class description (20040123 handegar)
+
+SO_NODE_SOURCE(SoTextureCoordinateCube);
+
+class SoTextureCoordinateCubeP {
+
+public:
+  SoTextureCoordinateCubeP(SoTextureCoordinateCube * texturenode) 
+    : master(texturenode) { }
+
+  SbVec3f origo;
+  SbBox3f boundingbox;
+  SoNode * currentshape;
+  SoState * currentstate;
+  
+  SbVec4f calculateTextureCoordinate(SbVec3f point, SbVec3f n);
+  
+private:
+  SoTextureCoordinateCube * master;
+};
+
+
+const SbVec4f & textureCoordinateCallback(void * userdata, const SbVec3f & point, const SbVec3f & normal);
+
+#undef PRIVATE
+#undef PUBLIC
+#define PRIVATE(p) (p->pimpl)
+#define PUBLIC(p) (p->master)
+
+
+/*!
+  Constructor.
+*/
+SoTextureCoordinateCube::SoTextureCoordinateCube(void)
+{
+
+  PRIVATE(this) = new SoTextureCoordinateCubeP(this);
+
+  SO_NODE_INTERNAL_CONSTRUCTOR(SoTextureCoordinateCube);
+
+  PRIVATE(this)->currentshape = NULL;
+  PRIVATE(this)->currentstate = NULL;
+  PRIVATE(this)->origo = SbVec3f(0,0,0);
+
+}
+
+/*!
+  Destructor.
+*/
+SoTextureCoordinateCube::~SoTextureCoordinateCube()
+{
+}
+
+// Documented in superclass.
+void
+SoTextureCoordinateCube::initClass(void)
+{
+  // FIXME: What should this one say?? (20040122 handegar)
+  SO_NODE_INTERNAL_INIT_CLASS(SoTextureCoordinateCube, SO_FROM_INVENTOR_1|SoNode::VRML1);
+  
+  SO_ENABLE(SoGLRenderAction, SoGLTextureCoordinateElement);
+  SO_ENABLE(SoCallbackAction, SoTextureCoordinateElement);
+  SO_ENABLE(SoPickAction, SoTextureCoordinateElement);
+
+}
+
+const SbVec4f &
+textureCoordinateCallback(void * userdata, 
+                          const SbVec3f & point, 
+                          const SbVec3f & normal)
+{
+  
+  SoTextureCoordinateCubeP * pimpl = (SoTextureCoordinateCubeP *) userdata;
+  
+  SoState * state = pimpl->currentstate;
+  SoFullPath * path = (SoFullPath *) state->getAction()->getCurPath();
+  SoNode * node = path->getTail();
+
+
+  if (!node->isOfType(SoShape::getClassTypeId())) {
+    // FIXME: A better way to handle this? (20040122 handegar)
+    assert(FALSE && "TextureCoordinateCube callback called for a non-SoShape node.");
+  }
+
+  // Cast the node into a shape
+  SoShape * shape = (SoShape *) node;
+
+  if (shape != pimpl->currentshape) {       
+    pimpl->boundingbox.makeEmpty();
+    shape->computeBBox(state->getAction(), pimpl->boundingbox, pimpl->origo);
+    pimpl->origo = pimpl->boundingbox.getCenter();
+    pimpl->currentshape = shape;
+  }
+
+  const SbVec4f & ret = pimpl->calculateTextureCoordinate(point, normal);
+  return ret;
+  
+}
+
+SbVec4f
+SoTextureCoordinateCubeP::calculateTextureCoordinate(SbVec3f point, SbVec3f n)
+{
+
+  double maxv = fabs(n[0]);
+  int maxi = 0;
+      
+  if (fabs(n[1]) > maxv) { maxi = 1; maxv = fabs(n[1]); }
+  if (fabs(n[2]) > maxv) { maxi = 2; }  
+  
+  int i0 = (maxi + 1) % 3;
+  int i1 = (maxi + 2) % 3;
+
+  // FIXME: Is this the correct way to make the bbox equally large in
+  // every direction? (20040123 handegar)
+  SbVec3f bmax = this->boundingbox.getMax();
+  SbVec3f bmin = -bmax;
+
+  float d0 = bmax[i0] - bmin[i0];
+  float d1 = bmax[i1] - bmin[i1];
+
+  if (d0 == 0.0f) d0 = 1.0f;
+  if (d1 == 0.0f) d1 = 1.0f;
+
+  float s = (point[i0] - bmin[i0]) / d0; 
+  float t = (point[i1] - bmin[i1]) / d1;
+
+  return SbVec4f(s, t, 0.0f, 1.0f);
+ 
+}
+
+
+// Documented in superclass.
+void
+SoTextureCoordinateCube::doAction(SoAction * action)
+{  
+}
+
+// Documented in superclass.
+void
+SoTextureCoordinateCube::GLRender(SoGLRenderAction * action)
+{
+  
+  if (PRIVATE(this)->currentstate != action->getState()) 
+    PRIVATE(this)->currentstate = action->getState();    
+
+  PRIVATE(this)->currentshape = NULL;  
+
+  SoTextureCoordinateElement::setFunction(PRIVATE(this)->currentstate, 
+                                          this, textureCoordinateCallback, PRIVATE(this));
+}
+
+// Documented in superclass.
+void
+SoTextureCoordinateCube::callback(SoCallbackAction * action)
+{
+  SoTextureCoordinateCube::doAction((SoAction *)action);
+}
+
+// Documented in superclass.
+void
+SoTextureCoordinateCube::pick(SoPickAction * action)
+{
+  SoTextureCoordinateCube::doAction((SoAction *)action);
+}
+
