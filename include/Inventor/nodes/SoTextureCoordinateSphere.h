18aefd789d346f8cd54baac44fda252b562a8c2e
?ystein Handegard <handegar@sim.no>
2004-01-26 14:57:38 +0000

    New header for sphere texture coordinate mapping.

--- include/Inventor/nodes/SoTextureCoordinateSphere.h
+++ include/Inventor/nodes/SoTextureCoordinateSphere.h
@@ -1,1 +1,50 @@
-// coming soon
+#ifndef COIN_SOTEXTURECOORDINATESPHERE_H
+#define COIN_SOTEXTURECOORDINATESPHERE_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2004 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+#include <Inventor/nodes/SoSubNode.h>
+
+class COIN_DLL_API SoTextureCoordinateSphere : public SoNode {
+  typedef SoNode inherited;
+
+  SO_NODE_HEADER(SoTextureCoordinateSphere);
+
+public:
+  static void initClass(void);
+  SoTextureCoordinateSphere(void);
+
+  virtual void doAction(SoAction * action);
+  virtual void GLRender(SoGLRenderAction * action);
+  virtual void callback(SoCallbackAction * action);
+  virtual void pick(SoPickAction * action);
+
+protected:
+  virtual ~SoTextureCoordinateSphere();
+  class SoTextureCoordinateSphereP * pimpl;
+  friend class SoTextureCoordinateSphereP;
+
+};
+
+#endif // !COIN_SOTEXTURECOORDINATESPHERE_H
