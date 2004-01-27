f0f8738af697eae90f3e8c32150aa86e60bd7a2b
?ystein Handegard <handegar@sim.no>
2004-01-27 15:45:04 +0000

    Added header for cylinder texture mapping

--- include/Inventor/nodes/SoTextureCoordinateCylinder.h
+++ include/Inventor/nodes/SoTextureCoordinateCylinder.h
@@ -1,1 +1,50 @@
-// coming soon
+#ifndef COIN_SOTEXTURECOORDINATECYLINDER_H
+#define COIN_SOTEXTURECOORDINATECYLINDER_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2004 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+#include <Inventor/nodes/SoSubNode.h>
+
+class COIN_DLL_API SoTextureCoordinateCylinder : public SoNode {
+  typedef SoNode inherited;
+
+  SO_NODE_HEADER(SoTextureCoordinateCylinder);
+
+public:
+  static void initClass(void);
+  SoTextureCoordinateCylinder(void);
+
+  virtual void doAction(SoAction * action);
+  virtual void GLRender(SoGLRenderAction * action);
+  virtual void callback(SoCallbackAction * action);
+  virtual void pick(SoPickAction * action);
+
+protected:
+  virtual ~SoTextureCoordinateCylinder();
+  class SoTextureCoordinateCylinderP * pimpl;
+  friend class SoTextureCoordinateCylinderP;
+
+};
+
+#endif // !COIN_SOTEXTURECOORDINATECYLINDER_H
