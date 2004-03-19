cd0a0d22916ffe8706b80c95c93a2efbba2c7422
Lars J. Aas <larsa@sim.no>
2004-03-19 16:28:22 +0000

    prefer fpclass() to _fpclass()

--- src/tidbits.c
+++ src/tidbits.c
@@ -53,6 +53,9 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif /* HAVE_UNISTD_H */
+#ifdef HAVE_IEEEFP_H
+#include <ieeefp.h>
+#endif /* HAVE_IEEEFP_H */
 
 #ifdef HAVE_DIRECT_H
 #include <direct.h> /* _getcwd() */
@@ -1388,6 +1391,10 @@ coin_isinf(double value)
 {
 #ifdef HAVE_ISINF
   return isinf(value);
+#elif defined(HAVE_FPCLASS)
+  if (fpclass(value) == FP_NINF) { return -1; }
+  if (fpclass(value) == FP_PINF) { return +1; }
+  return 0;
 #elif defined(HAVE__FPCLASS)
   if (_fpclass(value) == _FPCLASS_NINF) { return -1; }
   if (_fpclass(value) == _FPCLASS_PINF) { return +1; }
@@ -1414,6 +1421,10 @@ coin_isnan(double value)
   return isnan(value);
 #elif defined(HAVE__ISNAN)
   return _isnan(value);
+#elif defined(HAVE_FPCLASS)
+  if (fpclass(value) == FP_SNAN) { return 1; }
+  if (fpclass(value) == FP_QNAN) { return 1; }
+  return 0;
 #elif defined(HAVE__FPCLASS)
   if (_fpclass(value) == _FPCLASS_SNAN) { return 1; }
   if (_fpclass(value) == _FPCLASS_QNAN) { return 1; }
