01d6db55070058b38d14fccd31189cdde41944ab
Morten Eriksen <mortene@sim.no>
2004-06-15 13:04:41 +0000

    Build fixes: SbStorage / cc_storage should now be built even though thread-abstractions are not available.

--- src/threads/all-threads-c.c
+++ src/threads/all-threads-c.c
@@ -22,14 +22,21 @@
 \**************************************************************************/
 
 #include "common.c"
+#include "storage.c" /* cc_storage ADT works without the thread abstractions */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#ifdef HAVE_THREADS
 #include "thread.c"
 #include "mutex.c"
 #include "condvar.c"
 #include "rwmutex.c"
 #include "recmutex.c"
-#include "storage.c"
 #include "sched.c"
 #include "sync.c"
 #include "wpool.c"
 #include "worker.c"
 /* #include "barrier.c" */
+#endif /* HAVE_THREADS */
