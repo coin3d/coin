9c3c4eb2a033941606b08499d4cfca3747d10c5f
Morten Eriksen <mortene@sim.no>
2004-07-01 11:45:44 +0000

    Various fixes: avoid mem-leak, be more lenient towards client code, improve API documentation.

--- src/misc/SoContextHandler.cpp
+++ src/misc/SoContextHandler.cpp
@@ -31,8 +31,15 @@
 // since it's a public class (and possibly useful
 // externally). 20030225 mortene.
 
+// *************************************************************************
+
 #include <Inventor/misc/SoContextHandler.h>
+
+#include <Inventor/C/tidbitsp.h>
+#include <Inventor/errors/SoDebugError.h>
 #include <Inventor/lists/SbList.h>
+
+// *************************************************************************
 
 class socontexthandler_cbitem {
 public:
@@ -48,25 +55,60 @@ public:
   void * closure;
 };
 
+// *************************************************************************
 
 static SbList <socontexthandler_cbitem> * socontexthandler_cblist;
 
+// *************************************************************************
+
+static void
+socontexthandler_cleanup(void)
+{
+#if COIN_DEBUG  
+  const int len = socontexthandler_cblist->getLength();
+  if (len > 0) {
+    SoDebugError::postWarning("socontexthandler_cleanup",
+                              "%d context-bound resources not free'd "
+                              "before exit.", len);
+  }
+#endif // COIN_DEBUG  
+  delete socontexthandler_cblist;
+}
+
+// *************************************************************************
+
 /*!
-  This method \e must be called by client code which destruct a
+  This method \e must be called by client code which destructs a
   context, to guarantee that there are no memory leaks upon context
-  destruction (texture objects and display lists must be deleted). 
+  destruction.
+
+  This will take care of correctly freeing context-bound resources,
+  like OpenGL texture objects and display lists.
+
+  Before calling this function, the context \e must be made current.
+
+  Note that if you are using one of the standard GUI-binding libraries
+  from Systems in Motion, this is taken care of automatically for
+  contexts for canvases set up by SoQt, SoWin, etc.
 */
 void
 SoContextHandler::destructingContext(uint32_t contextid)
 {
-  const int n = socontexthandler_cblist ? 
+  const int n = socontexthandler_cblist ?
     socontexthandler_cblist->getLength() : 0;
+
+  // Work on a copy of the list, to allow
+  // removeContextDestructionCallback() to be safely invoked while
+  // iterating over the list.
+  SbList <socontexthandler_cbitem> listcopy = *socontexthandler_cblist;
+
   for (int i = 0; i < n; i++) {
-    const socontexthandler_cbitem & item = 
-      (*socontexthandler_cblist)[i];
+    const socontexthandler_cbitem & item = listcopy[i];
     item.func(contextid, item.closure);
   }
 }
+
+// *************************************************************************
 
 void
 SoContextHandler::addContextDestructionCallback(ContextDestructionCB * func,
@@ -74,7 +116,9 @@ SoContextHandler::addContextDestructionCallback(ContextDestructionCB * func,
 {
   if (socontexthandler_cblist == NULL) {
     socontexthandler_cblist = new SbList <socontexthandler_cbitem>;
+    coin_atexit((coin_atexit_f *)socontexthandler_cleanup, 0);
   }
+
   socontexthandler_cbitem item;
   item.func = func;
   item.closure = closure;
@@ -84,17 +128,15 @@ SoContextHandler::addContextDestructionCallback(ContextDestructionCB * func,
 void
 SoContextHandler::removeContextDestructionCallback(ContextDestructionCB * func, void * closure)
 {
-  if (socontexthandler_cblist) {
-    socontexthandler_cbitem item;
-    item.func = func;
-    item.closure = closure;
+  assert(socontexthandler_cblist);
 
-    int idx = socontexthandler_cblist->find(item);
-    if (idx >= 0) {
-      socontexthandler_cblist->removeFast(idx);
-    }
-    else {
+  socontexthandler_cbitem item;
+  item.func = func;
+  item.closure = closure;
 
-    }
-  }
+  const int idx = socontexthandler_cblist->find(item);
+  assert(idx >= 0);
+  socontexthandler_cblist->removeFast(idx);
 }
+
+// *************************************************************************
