3e2b2cc97b13ccc03e775c5a88993ae532c2cb32
Lars J. Aas <larsa@sim.no>
2004-12-14 11:19:08 +0000

    autogenerated files

---
+++ build/msvc7/data/draggerDefaults/rotateSphericalDragger.h
@@ -0,0 +1,94 @@
+/************************************************************************** *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2004 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+#ifndef SO_ROTATESPHERICALDRAGGER_IV_H
+#define SO_ROTATESPHERICALDRAGGER_IV_H
+
+static const char ROTATESPHERICALDRAGGER_draggergeometry[] =
+  "#Inventor V2.1 ascii\n"
+  "\n"
+  "\n"
+  "\n"
+  "DEF ROTATESPHERICAL_INACTIVE_MATERIAL Material { diffuseColor 0.5 0.5 0.5  emissiveColor 0.5 0.5 0.5 }\n"
+  "DEF ROTATESPHERICAL_ACTIVE_MATERIAL Material { diffuseColor 0.5 0.5 0  emissiveColor 0.5 0.5 0 }\n"
+  "DEF ROTATESPHERICAL_FEEDBACK_MATERIAL Material { diffuseColor 0.5 0 0.5  emissiveColor 0.5 0 0.5 }\n"
+  "\n"
+  "\n"
+  "\n"
+  "\n"
+  "DEF ROTATESPHERICAL_FEEDBACK Group {\n"
+  "   Coordinate3 { point [ 1.3 0 0, -1.3 0 0, 0 1.3 0, 0 -1.3 0, 0 0 1.3, 0 0 -1.3 ] }\n"
+  "   IndexedLineSet { coordIndex [ 0, 1, -1, 2, 3, -1, 4, 5, -1 ] }\n"
+  "}\n"
+  "\n"
+  "DEF rotateSphericalFeedback Separator {\n"
+  "   USE ROTATESPHERICAL_FEEDBACK_MATERIAL\n"
+  "   USE ROTATESPHERICAL_FEEDBACK\n"
+  "}\n"
+  "DEF rotateSphericalFeedbackActive Separator {\n"
+  "   USE ROTATESPHERICAL_FEEDBACK_MATERIAL\n"
+  "   USE ROTATESPHERICAL_FEEDBACK\n"
+  "}\n"
+  "\n"
+  "\n"
+  "\n"
+  "\n"
+  "DEF ROTATESPHERICAL_RING Group {\n"
+  "\n"
+  "\n"
+  "   ShapeHints {\n"
+  "      shapeType UNKNOWN_SHAPE_TYPE\n"
+  "      vertexOrdering UNKNOWN_ORDERING\n"
+  "   }\n"
+  "\n"
+  "   DrawStyle { style LINES lineWidth 2 }\n"
+  "   Cylinder { parts SIDES height 0 }\n"
+  "}\n"
+  "\n"
+  "DEF ROTATESPHERICAL_BALL Group {\n"
+  "\n"
+  "\n"
+  "   Scale { scaleFactor 1.733 1.733 1.733 }\n"
+  "\n"
+  "\n"
+  "   DrawStyle { style INVISIBLE }\n"
+  "   Sphere { }\n"
+  "\n"
+  "\n"
+  "   USE ROTATESPHERICAL_RING\n"
+  "   Rotation { rotation 0 0 1 1.57 }\n"
+  "   USE ROTATESPHERICAL_RING\n"
+  "   Rotation { rotation 1 0 0  1.57 }\n"
+  "   USE ROTATESPHERICAL_RING\n"
+  "}\n"
+  "\n"
+  "DEF rotateSphericalRotator Separator {\n"
+  "   USE ROTATESPHERICAL_INACTIVE_MATERIAL\n"
+  "   USE ROTATESPHERICAL_BALL\n"
+  "}\n"
+  "\n"
+  "DEF rotateSphericalRotatorActive Separator {\n"
+  "   USE ROTATESPHERICAL_ACTIVE_MATERIAL\n"
+  "   USE ROTATESPHERICAL_BALL\n"
+  "}\n";
+
+#endif /* ! SO_ROTATESPHERICALDRAGGER_IV_H */
---
+++ build/msvc7/data/draggerDefaults/spotLightDragger.h
@@ -0,0 +1,107 @@
+/************************************************************************** *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2004 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+#ifndef SO_SPOTLIGHTDRAGGER_IV_H
+#define SO_SPOTLIGHTDRAGGER_IV_H
+
+static const char SPOTLIGHTDRAGGER_draggergeometry[] =
+  "#Inventor V2.1 ascii\n"
+  "\n"
+  "\n"
+  "DEF SPOTLIGHT_INACTIVE_MATERIAL Material { diffuseColor 0.5 0.5 0.5  emissiveColor 0.5 0.5 0.5 }\n"
+  "DEF SPOTLIGHT_ACTIVE_MATERIAL Material { diffuseColor 0.5 0.5 0  emissiveColor 0.5 0.5 0 }\n"
+  "\n"
+  "\n"
+  "\n"
+  "\n"
+  "DEF spotLightOverallMaterial Material { diffuseColor 0.5 0.5 0.5  emissiveColor 0.5 0.5 0.5 }\n"
+  "\n"
+  "\n"
+  "DEF SPOTLIGHT_ARROW Separator {\n"
+  "\n"
+  "   Rotation { rotation 1 0 0  -1.57 }\n"
+  "\n"
+  "   Coordinate3 { point [ 0 0 0, 0 9 0 ] }\n"
+  "   IndexedLineSet { coordIndex [ 0, 1, -1 ] }\n"
+  "   Translation { translation 0 10 0 }\n"
+  "   Cone { }\n"
+  "   Translation { translation 0 -11 0 }\n"
+  "   Cone { bottomRadius 0.1 }\n"
+  "}\n"
+  "\n"
+  "DEF spotLightRotatorRotator Separator {\n"
+  "   USE SPOTLIGHT_INACTIVE_MATERIAL\n"
+  "   USE SPOTLIGHT_ARROW\n"
+  "}\n"
+  "DEF spotLightRotatorRotatorActive Separator {\n"
+  "   USE SPOTLIGHT_ACTIVE_MATERIAL\n"
+  "   USE SPOTLIGHT_ARROW\n"
+  "}\n"
+  "\n"
+  "DEF spotLightRotatorFeedback Separator { }\n"
+  "DEF spotLightRotatorFeedbackActive Separator { }\n"
+  "\n"
+  "\n"
+  "DEF SPOTLIGHT_TRANSLATE_AXIS Group {\n"
+  "   Rotation { rotation 0 0 1  1.57 }\n"
+  "   Cylinder { height 3.0  radius 0.2 }\n"
+  "}\n"
+  "\n"
+  "DEF spotLightTranslatorLineTranslator Separator {\n"
+  "   USE SPOTLIGHT_INACTIVE_MATERIAL\n"
+  "   USE SPOTLIGHT_TRANSLATE_AXIS\n"
+  "}\n"
+  "DEF spotLightTranslatorLineTranslatorActive Separator {\n"
+  "   USE SPOTLIGHT_ACTIVE_MATERIAL\n"
+  "   USE SPOTLIGHT_TRANSLATE_AXIS\n"
+  "}\n"
+  "\n"
+  "DEF spotLightTranslatorPlaneTranslator Separator {\n"
+  "   USE SPOTLIGHT_INACTIVE_MATERIAL\n"
+  "   Sphere { }\n"
+  "}\n"
+  "DEF spotLightTranslatorPlaneTranslatorActive Separator {\n"
+  "   USE SPOTLIGHT_ACTIVE_MATERIAL\n"
+  "   Sphere { }\n"
+  "}\n"
+  "\n"
+  "\n"
+  "DEF SPOTLIGHT_CONEANGLE Separator {\n"
+  "   Rotation { rotation 1 0 0  1.57 }\n"
+  "   ShapeHints { vertexOrdering UNKNOWN_ORDERING }\n"
+  "   Cone { parts SIDES  bottomRadius 2.0 }\n"
+  "}\n"
+  "\n"
+  "DEF spotLightBeam Separator {\n"
+  "   USE SPOTLIGHT_INACTIVE_MATERIAL\n"
+  "   DrawStyle { style LINES  lineWidth 2 }\n"
+  "   USE SPOTLIGHT_CONEANGLE\n"
+  "}\n"
+  "DEF spotLightBeamActive Separator {\n"
+  "   USE SPOTLIGHT_ACTIVE_MATERIAL\n"
+  "   DrawStyle { style LINES  lineWidth 3 }\n"
+  "   USE SPOTLIGHT_CONEANGLE\n"
+  "}\n"
+  "\n"
+  "DEF spotLightBeamPlacement Translation { translation 0 0 -1.5 }\n";
+
+#endif /* ! SO_SPOTLIGHTDRAGGER_IV_H */
