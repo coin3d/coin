b6e64716b0f6445f2070e0f46e19178a9bce47dc
Morten Eriksen <mortene@sim.no>
2005-03-10 13:08:19 +0000

    Fix URLs to web3d.org.

--- src/misc/SoProto.cpp
+++ src/misc/SoProto.cpp
@@ -63,7 +63,7 @@
   the node returned from SoProtoInstance::getRootNode().
 
   See
-  http://www.web3d.org/technicalinfo/specifications/ISO_IEC_14772-All/part1/concepts.html#4.8
+  http://www.web3d.org/technicalinfo/specifications/ISO-IEC-14772-IS-VRML97WithAmendment1/part1/concepts.html#4.8
   for more information about PROTOs in VRML97.
 
 */
