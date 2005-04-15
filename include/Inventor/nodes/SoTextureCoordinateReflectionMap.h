dfb363dccb0679b9acac15d2d0f6ade86c9e053c
Peder Blekken <pederb@sim.no>
2005-04-15 14:59:03 +0000

    A first version of this node. Not tested much.

--- include/Inventor/nodes/SoTextureCoordinateReflectionMap.h
+++ include/Inventor/nodes/SoTextureCoordinateReflectionMap.h
@@ -1,1 +1,59 @@
-// coming soon
+
+#ifndef COIN_SOTEXTURECOORDINATEREFLECTION_MAP_H
+#define COIN_SOTEXTURECOORDINATEREFLECTION_MAP_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+#include <Inventor/nodes/SoSubNode.h>
+#include <Inventor/nodes/SoTextureCoordinateFunction.h>
+#include <Inventor/fields/SoSFVec3f.h>
+#include <Inventor/SbVec4f.h>
+#include <Inventor/SbVec3f.h>
+
+class COIN_DLL_API SoTextureCoordinateReflectionMap : public SoTextureCoordinateFunction {
+  typedef SoTextureCoordinateFunction inherited;
+
+  SO_NODE_HEADER(SoTextureCoordinateReflectionMap);
+
+public:
+  static void initClass(void);
+  SoTextureCoordinateReflectionMap(void);
+
+  virtual void doAction(SoAction * action);
+  virtual void GLRender(SoGLRenderAction * action);
+  virtual void pick(SoPickAction * action);
+  virtual void callback(SoCallbackAction * action);
+
+protected:
+  virtual ~SoTextureCoordinateReflectionMap();
+
+private:
+  static const SbVec4f & generate(void *userdata,
+                                  const SbVec3f &p,
+                                  const SbVec3f &n);
+  static void handleTexgen(void *data);
+
+};
+
+#endif // !COIN_SOTEXTURECOORDINATEREFLECTION_MAP_H
--- src/nodes/SoTextureCoordinateReflectionMap.cpp
+++ src/nodes/SoTextureCoordinateReflectionMap.cpp
@@ -1,1 +1,211 @@
-// coming soon
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+/*!
+  \class SoTextureCoordinateReflectionMap SoTextureCoordinateReflectionMap.h Inventor/nodes/SoTextureCoordinateReflectionMap.h
+  \brief The SoTextureCoordinateReflectionMap class generates 3D reflection texture coordinates.
+  \ingroup nodes
+
+  This node is usually used along with a SoCubeMapTexture node...
+  
+  FIXME: more doc.
+
+*/
+
+
+#include <Inventor/nodes/SoTextureCoordinateReflectionMap.h>
+#include <Inventor/nodes/SoSubNodeP.h>
+#include <Inventor/SbVec3f.h>
+#include <Inventor/actions/SoGLRenderAction.h>
+#include <Inventor/elements/SoGLTextureCoordinateElement.h>
+#include <Inventor/elements/SoGLMultiTextureCoordinateElement.h>
+#include <Inventor/elements/SoModelMatrixElement.h>
+#include <Inventor/elements/SoViewingMatrixElement.h>
+#include <Inventor/elements/SoTextureUnitElement.h>
+#include <Inventor/C/tidbitsp.h>
+#include <stdlib.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif // HAVE_CONFIG_H
+#include <Inventor/system/gl.h>
+#include <float.h>
+
+// *************************************************************************
+
+class SoTextureCoordinateReflectionMapP {
+public:
+  static SbVec4f * dummy_texcoords;
+  static void cleanup_func(void);
+};
+
+SbVec4f * SoTextureCoordinateReflectionMapP::dummy_texcoords = NULL;
+
+void
+SoTextureCoordinateReflectionMapP::cleanup_func(void)
+{
+  delete SoTextureCoordinateReflectionMapP::dummy_texcoords;
+}
+
+// *************************************************************************
+
+SO_NODE_SOURCE(SoTextureCoordinateReflectionMap);
+
+/*!
+  Constructor.
+*/
+SoTextureCoordinateReflectionMap::SoTextureCoordinateReflectionMap()
+{
+  SO_NODE_INTERNAL_CONSTRUCTOR(SoTextureCoordinateReflectionMap);
+}
+
+/*!
+  Destructor.
+*/
+SoTextureCoordinateReflectionMap::~SoTextureCoordinateReflectionMap()
+{
+}
+
+// doc in super
+void
+SoTextureCoordinateReflectionMap::initClass(void)
+{
+  SO_NODE_INTERNAL_INIT_CLASS(SoTextureCoordinateReflectionMap, SO_FROM_INVENTOR_1);
+
+  SoTextureCoordinateReflectionMapP::dummy_texcoords = new SbVec4f(0.0f, 0.0f, 0.0f, 1.0f);
+  coin_atexit((coin_atexit_f *)SoTextureCoordinateReflectionMapP::cleanup_func, 0);
+}
+
+// generates texture coordinates for GLRender, callback and pick actions
+const SbVec4f &
+SoTextureCoordinateReflectionMap::generate(void *userdata,
+                                         const SbVec3f & /* p */,
+                                         const SbVec3f &n)
+{
+  SoState *state = (SoState*)userdata;
+  SbVec3f wn; // normal in world (eye) coordinates
+  SoModelMatrixElement::get(state).multDirMatrix(n, wn);
+  SbVec3f u = n;
+
+  u.normalize();
+  wn.normalize();
+
+  // reflection vector
+  SbVec3f r = u - SbVec3f(2.0f*wn[0]*wn[0]*u[0],
+                          2.0f*wn[1]*wn[1]*u[1],
+                          2.0f*wn[2]*wn[2]*u[2]);
+  r.normalize();
+
+  (*SoTextureCoordinateReflectionMapP::dummy_texcoords)[0] = r[0];
+  (*SoTextureCoordinateReflectionMapP::dummy_texcoords)[1] = r[1];
+  (*SoTextureCoordinateReflectionMapP::dummy_texcoords)[2] = r[2];
+  (*SoTextureCoordinateReflectionMapP::dummy_texcoords)[3] = 1.0f;
+  return *SoTextureCoordinateReflectionMapP::dummy_texcoords;
+}
+
+// doc from parent
+void
+SoTextureCoordinateReflectionMap::doAction(SoAction * action)
+{
+  SoTextureCoordinateElement::setFunction(action->getState(), this,
+                                          generate,
+                                          action->getState());
+}
+
+// doc from parent
+void
+SoTextureCoordinateReflectionMap::GLRender(SoGLRenderAction * action)
+{
+  SoState * state = action->getState();
+  int unit = SoTextureUnitElement::get(state);
+  if (unit == 0) {
+    SoTextureCoordinateReflectionMap::doAction((SoAction *)action);
+    SoGLTextureCoordinateElement::setTexGen(action->getState(),
+                                            this, handleTexgen,
+                                            action,
+                                            generate,
+                                            action->getState());
+  }
+  else {
+    SoMultiTextureCoordinateElement::setFunction(action->getState(), this,
+                                                 unit,
+                                                 generate,
+                                                 action->getState());
+    SoGLMultiTextureCoordinateElement::setTexGen(action->getState(),
+                                                 this, unit, handleTexgen, 
+                                                 action,
+                                                 generate,
+                                                 action->getState());
+
+  }
+}
+
+// doc from parent
+void
+SoTextureCoordinateReflectionMap::callback(SoCallbackAction * action)
+{
+  SoTextureCoordinateReflectionMap::doAction((SoAction *)action);
+}
+
+// doc from parent
+void
+SoTextureCoordinateReflectionMap::pick(SoPickAction * action)
+{
+  SoTextureCoordinateReflectionMap::doAction((SoAction *)action);
+}
+
+void
+SoTextureCoordinateReflectionMap::handleTexgen(void * data)
+{
+#if 0 // FIXME: testing, pederb, 2005-04-15
+  SoGLRenderAction * action = (SoGLRenderAction*)  data;
+
+  SbMatrix m = SoViewingMatrixElement::get(action->getState());
+ 
+  m = m.inverse();
+
+  float s_plane[4];
+  float t_plane[4];
+  float r_plane[4];
+  float q_plane[] = {0.0f, 0.0f, 0.0f, 1.0f};
+
+  for (int i = 0; i < 3; i++) {
+    s_plane[i] = m[0][i];
+    t_plane[i] = m[1][i];
+    r_plane[i] = m[2][i];
+  }
+  
+  s_plane[3] = t_plane[3] = r_plane[3] = 0.0f;
+  
+  glPushMatrix();
+  glLoadIdentity();
+  glTexGenfv(GL_S, GL_EYE_PLANE, s_plane);
+  glTexGenfv(GL_T, GL_EYE_PLANE, t_plane);
+  glTexGenfv(GL_R, GL_EYE_PLANE, r_plane);
+  glTexGenfv(GL_Q, GL_EYE_PLANE, q_plane);
+  glPopMatrix();
+#endif // testing
+  glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
+  glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);  
+  glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
+}
