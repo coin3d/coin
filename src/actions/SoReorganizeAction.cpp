cb64444588ada9b02c53cdbc0c3c2cb4c0021823
Peder Blekken <pederb@sim.no>
2005-04-28 15:37:00 +0000

    Documentation.

--- src/actions/SoReorganizeAction.cpp
+++ src/actions/SoReorganizeAction.cpp
@@ -23,10 +23,122 @@
 
 /*!
   \class SoReorganizeAction Inventor/include/SoReorganizeAction.h
-  \brief The SoReorganizeAction class ...
+  \brief The SoReorganizeAction class reorganizes your scene graph to optimize traversal/rendering.
 
   Note. This is work-in-progress. pederb, 2005-04-05.
 
+  The code below is an example of a program that applies a
+  SoReorganizeAction on a scene graph, converting all shapes into
+  shapes that can be rendered using vertex array rendering.  VBO
+  rendering for these shapes is not implemented yet. Coming soon
+  though.
+
+  \verbatim
+
+  #include <Inventor/SoDB.h>
+  #include <Inventor/nodes/SoSeparator.h>
+  #include <Inventor/nodes/SoCoordinate3.h>
+  #include <Inventor/nodes/SoCoordinate4.h>
+  #include <Inventor/nodes/SoNormal.h>
+  #include <Inventor/nodes/SoTextureCoordinate2.h>
+  #include <Inventor/nodes/SoSeparator.h>
+  #include <Inventor/actions/SoWriteAction.h>
+  #include <Inventor/actions/SoSearchAction.h>
+  #include <Inventor/errors/SoDebugError.h>
+  #include <Inventor/nodes/SoShapeHints.h>
+  #include <Inventor/SoInput.h>
+  #include <Inventor/SoOutput.h>
+  #include <Inventor/SoInteraction.h>
+  #include <Inventor/actions/SoReorganizeAction.h>
+  #include <assert.h>
+  #include <stdio.h>
+  
+  static void strip_node(SoType type, SoNode * root) 
+  {
+    SoSearchAction sa;
+    sa.setType(type);
+    sa.setSearchingAll(TRUE);
+    sa.setInterest(SoSearchAction::ALL);
+    sa.apply(root);
+
+    SoPathList & pl = sa.getPaths();
+    for (int i = 0; i < pl.getLength(); i++) {
+      SoFullPath * p = (SoFullPath*) pl[i];
+        if (p->getTail()->isOfType(type)) {
+          SoGroup * g = (SoGroup*) p->getNodeFromTail(1);
+          g->removeChild(p->getIndexFromTail(0));
+        }
+      } 
+    sa.reset();  
+  }
+
+  int
+  main(int argc, char ** argv )
+  {
+    if (argc < 3) {
+      fprintf(stderr,"Usage: reorganize <infile> <outfile> [nostrip]\n");
+      return -1;
+    }
+
+    SbBool strip = TRUE;
+    if (argc > 3) {
+      if (strcmp(argv[3], "nostrip") == 0) strip = FALSE;
+      else {
+        fprintf(stderr,"Usage: reorganize <infile> <outfile> [nostrip]\n");
+        return -1;
+      }
+    }
+
+    SoDB::init();
+    SoInteraction::init();
+    
+    SoInput input;
+    SbBool ok = input.openFile(argv[1]);
+    if (!ok) {
+      fprintf(stderr,"Unable to open file.\n");
+      return -1;
+    }
+    SoSeparator * root = SoDB::readAll(&input); 
+  
+    SbBool vrml1 = input.isFileVRML1();
+    SbBool vrml2 = input.isFileVRML2();
+
+    if (vrml2) {
+      fprintf(stderr,"VRML2 not supported yet\n");
+      return -1;
+    }
+  
+    if (!root) {
+      fprintf(stderr,"Unable to read file.\n");
+      return -1;
+    }
+    root->ref();
+
+    fprintf(stderr,"Applying SoReorganizeAction...");
+    SoReorganizeAction reorg;
+    reorg.apply(root);
+    fprintf(stderr,"done\n");
+
+    SoOutput out;
+    if (out.openFile(argv[2])) {      
+      if (strip) { // strip coord3, texcoord and normal nodes
+        fprintf(stderr,"stripping old nodes from scene graph\n");
+        strip_node(SoCoordinate3::getClassTypeId(), root);
+        strip_node(SoCoordinate4::getClassTypeId(), root);
+        strip_node(SoNormal::getClassTypeId(), root);
+        strip_node(SoTextureCoordinate2::getClassTypeId(), root);
+      }
+      fprintf(stderr,"writing target\n");
+      SoWriteAction wa(&out);
+      wa.apply(root);
+    }
+    root->unref();
+    return 0;
+  } // main()
+
+  \endvarbatim
+
+  \since Coin 3.0
 */
 
 #include <Inventor/SbName.h>
