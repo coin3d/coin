550d9329ed3405647050e194ae411a5cd743f4f2
Peder Blekken <pederb@sim.no>
2005-06-17 16:33:21 +0000

    First version of the SoGLVBOElement. Comments and suggestions for improvements welcome.

--- include/Inventor/elements/SoGLVBOElement.h
+++ include/Inventor/elements/SoGLVBOElement.h
@@ -1,1 +1,71 @@
-// coming soon
+#ifndef COIN_SOGLVBOELEMENT_H
+#define COIN_SOGLVBOELEMENT_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+#include <Inventor/elements/SoSubElement.h>
+
+class SoVBO;
+class SoGLVBOElementP;
+
+class SoGLVBOElement : public SoElement {
+  typedef SoElement inherited;
+
+  SO_ELEMENT_HEADER(SoGLVBOElement);
+
+ public:
+  static void initClass(void);
+ protected:
+  virtual ~SoGLVBOElement();
+
+ public:
+
+  static void setVertexVBO(SoState * state, SoVBO * vbo);
+  static void setNormalVBO(SoState * state, SoVBO * vbo);
+  static void setColorVBO(SoState * state, SoVBO * vbo);
+  static void setTexCoordVBO(SoState * state, const int unit, SoVBO * vbo);
+
+  static const SoGLVBOElement * getInstance(SoState * state);
+
+ public:
+  virtual void init(SoState *state);
+  virtual void push(SoState *state);
+  virtual void pop(SoState *state, const SoElement * prevtopelement);
+  virtual SbBool matches(const SoElement * elt) const;
+  virtual SoElement * copyMatchInfo(void) const;
+
+  SoVBO * getVertexVBO(void) const;
+  SoVBO * getNormalVBO(void) const;
+  SoVBO * getColorVBO(void) const;
+  int getNumTexCoordVBO(void) const;
+  SoVBO * getTexCoordVBO(const int idx) const;
+
+ protected:  
+  static SoGLVBOElement * getElement(SoState * state);
+
+ private:
+  SoGLVBOElementP * pimpl;
+};
+
+#endif // COIN_SOGLVBOELEMENT_H
--- src/elements/GL/SoGLVBOElement.cpp
+++ src/elements/GL/SoGLVBOElement.cpp
@@ -1,1 +1,228 @@
-// coming soon
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+/*!
+  \class SoGLVBOElement Inventor/elements/SoGLVBOElement.h
+  \brief The SoGLVBOElement class is used to store VBO state.
+  \ingroup elements
+
+  FIXME: write doc.
+
+  \COIN_CLASS_EXTENSION
+
+  \since Coin 2.5
+*/
+
+#include <Inventor/elements/SoGLVBOElement.h>
+#include <Inventor/nodes/SoNode.h>
+#include <Inventor/lists/SbList.h>
+#include <assert.h>
+
+#define PRIVATE(obj) obj->pimpl
+
+class SoGLVBOElementP {
+ public:
+
+  SoVBO * vertexvbo;
+  SoVBO * normalvbo;
+  SoVBO * colorvbo;
+  SbList <SoVBO*> texcoordvbo;
+};
+
+SO_ELEMENT_CUSTOM_CONSTRUCTOR_SOURCE(SoGLVBOElement);
+
+/*!
+  This static method initializes static data for the
+  SoMultiTextureCoordinateElement class.
+*/
+
+void
+SoGLVBOElement::initClass()
+{
+  SO_ELEMENT_INIT_CLASS(SoGLVBOElement, inherited);
+}
+
+
+/*!
+  The constructor.
+*/
+SoGLVBOElement::SoGLVBOElement(void)
+{
+  PRIVATE(this) = new SoGLVBOElementP;
+  
+  this->setTypeId(SoGLVBOElement::classTypeId);
+  this->setStackIndex(SoGLVBOElement::classStackIndex);
+}
+
+/*!
+  The destructor.
+*/
+
+SoGLVBOElement::~SoGLVBOElement()
+{
+  delete PRIVATE(this);
+}
+
+/*!
+  Sets the vertex VBO.
+*/
+void 
+SoGLVBOElement::setVertexVBO(SoState * state, SoVBO * vbo)
+{
+  SoGLVBOElement * elem = getElement(state);
+  PRIVATE(elem)->vertexvbo = vbo;
+}
+
+/*!
+  Sets the normal VBO.
+*/
+void 
+SoGLVBOElement::setNormalVBO(SoState * state, SoVBO * vbo)
+{
+  SoGLVBOElement * elem = getElement(state);
+  PRIVATE(elem)->normalvbo = vbo;
+}
+
+/*!
+  Sets the color VBO.
+*/
+void 
+SoGLVBOElement::setColorVBO(SoState * state, SoVBO * vbo)
+{
+  SoGLVBOElement * elem = getElement(state);
+  PRIVATE(elem)->colorvbo = vbo;
+}
+
+/*!
+  Sets the texture coordinate VBO.
+*/
+void 
+SoGLVBOElement::setTexCoordVBO(SoState * state, const int unit, SoVBO * vbo)
+{
+  SoGLVBOElement * elem = getElement(state);
+  const int n = PRIVATE(elem)->texcoordvbo.getLength();
+  for (int i = n; i < unit; i++) {
+    PRIVATE(elem)->texcoordvbo.append(NULL);
+  }
+  PRIVATE(elem)->texcoordvbo.append(vbo);
+}
+
+// doc in parent
+void 
+SoGLVBOElement::init(SoState *state)
+{
+  PRIVATE(this)->vertexvbo = NULL;
+  PRIVATE(this)->normalvbo = NULL;
+  PRIVATE(this)->colorvbo = NULL;
+  PRIVATE(this)->texcoordvbo.truncate(0);
+}
+
+// doc in parent
+void 
+SoGLVBOElement::push(SoState *state)
+{
+  SoGLVBOElement * prev = (SoGLVBOElement *) 
+    this->getNextInStack();
+  
+  PRIVATE(this)->vertexvbo = PRIVATE(prev)->vertexvbo;
+  PRIVATE(this)->normalvbo = PRIVATE(prev)->normalvbo;
+  PRIVATE(this)->colorvbo = PRIVATE(prev)->colorvbo;
+  PRIVATE(this)->texcoordvbo.truncate(0);
+
+  for (int i = 0; i < PRIVATE(prev)->texcoordvbo.getLength(); i++) {
+    PRIVATE(this)->texcoordvbo.append(PRIVATE(prev)->texcoordvbo[i]);
+  }
+}
+
+// doc in parent
+void 
+SoGLVBOElement::pop(SoState *state, const SoElement * prevtopelement)
+{
+  // nothing to do
+}
+
+// doc in parent
+SbBool 
+SoGLVBOElement::matches(const SoElement * elt) const
+{
+  assert(0 && "should never get here");
+  return TRUE;
+}
+
+// doc in parent
+SoElement * 
+SoGLVBOElement::copyMatchInfo(void) const
+{
+  assert(0 && "should never get here");
+  return NULL;
+}
+
+/*!
+  Returns a writable element instance.
+*/
+SoGLVBOElement * 
+SoGLVBOElement::getElement(SoState * state)
+{
+  return (SoGLVBOElement*) state->getElement(classStackIndex);
+}
+
+/*!
+  Returns a read-only element instance.
+*/
+const SoGLVBOElement * 
+SoGLVBOElement::getInstance(SoState * state)
+{
+  return (SoGLVBOElement*) state->getConstElement(classStackIndex);
+}
+
+SoVBO * 
+SoGLVBOElement::getVertexVBO(void) const
+{
+  return PRIVATE(this)->vertexvbo;
+}
+
+SoVBO * 
+SoGLVBOElement::getNormalVBO(void) const
+{
+  return PRIVATE(this)->normalvbo;
+}
+
+SoVBO * 
+SoGLVBOElement::getColorVBO(void) const
+{
+  return PRIVATE(this)->colorvbo;
+}
+
+int 
+SoGLVBOElement::getNumTexCoordVBO(void) const
+{
+  return PRIVATE(this)->texcoordvbo.getLength();
+}
+
+SoVBO * 
+SoGLVBOElement::getTexCoordVBO(const int idx) const
+{
+  assert((idx >= 0) && (idx < PRIVATE(this)->texcoordvbo.getLength()));
+  return PRIVATE(this)->texcoordvbo[idx];
+}
+
