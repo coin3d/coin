e69a0e0ed14f41cab68ebbe805b6782088e54de0
Karin (kyrah) Kosina <kyrah@sim.no>
2005-07-04 10:38:16 +0000

    Changed condition to assert, as suggested by mortene. If we don't have a valid OpenGL context, something is seriously wrong anyway that will only hit us later on.

--- src/elements/GL/SoGLDisplayList.cpp
+++ src/elements/GL/SoGLDisplayList.cpp
@@ -84,7 +84,8 @@ SoGLDisplayList::SoGLDisplayList(SoState * state, Type type, int allocnum,
 
   // Check for known buggy OpenGL driver.
   const char * versionstr = (const char *)glGetString(GL_VERSION);
-  if (versionstr && (strcmp(versionstr, "1.3.1 NVIDIA 28.02") == 0)) {
+  assert(versionstr && "glGetString() returned 0 -- no valid GL context?");
+  if (strcmp(versionstr, "1.3.1 NVIDIA 28.02") == 0) {
     // (From NVidia's changelog, it looks like the problem we've been
     // seeing with the 28.02 driver and displaylists *might* have been
     // fixed for the next version (28.80)).
