55526b9bdb2bfdb518e89bebab22cc0408c65021
Lars J. Aas <larsa@sim.no>
2005-07-06 13:43:19 +0000

    docs and cosmetics

--- include/SoDebug.h
+++ include/SoDebug.h
@@ -1,11 +1,36 @@
 #ifndef COIN_SODEBUG_H
 #define COIN_SODEBUG_H
 
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
 class SoBase;
 class SoNode;
 class SoField;
 
 #include <Inventor/SbBasic.h>
+
+// *************************************************************************
 
 class COIN_DLL_API SoDebug {
 public:
@@ -18,5 +43,7 @@ public:
   static void writeField(SoField * field);
   static void printName(SoBase * base);
 }; // SoDebug
+
+// *************************************************************************
 
 #endif // !COIN_SODEBUG_H
--- src/misc/SoDebug.cpp
+++ src/misc/SoDebug.cpp
@@ -1,3 +1,25 @@
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
 
 #include <stdarg.h>
 #include <assert.h>
@@ -11,6 +33,13 @@
 
 // *************************************************************************
 
+/*!
+  \class SoDebug SoDebug.h SoDebug.h
+  The SoDebug class is a small collection of debugging-related functions.
+*/
+
+// *************************************************************************
+
 struct SoDebug_internal {
   static SbHash<char *, void *> * namedict;
   static void delete_namedict(void);
@@ -19,11 +48,24 @@ struct SoDebug_internal {
 
 SbHash<char *, void *> * SoDebug_internal::namedict = NULL;
 
+/*!
+  This is a portable getenv-wrapper.
+
+  \sa coin_getenv()
+*/
+
 const char *
 SoDebug::GetEnv(const char * var)
 {
   return coin_getenv(var);
 }
+
+/*!
+  Real-time printf designed for use when use of standard printf() would
+  cause timing problems.
+
+  NOT IMPLEMENTED.  Currently it just forwards to printf().
+*/
 
 void
 SoDebug::RTPrintf(const char * formatstr, ...)
@@ -35,6 +77,13 @@ SoDebug::RTPrintf(const char * formatstr, ...)
   vprintf(formatstr, args);
   va_end(args);
 }
+
+/*!
+  Associate a name with an arbitrary pointer.  You can fetch the name of
+  the pointer later with PtrName().
+
+  \sa PtrName()
+*/
 
 void
 SoDebug::NamePtr(const char * name, void * ptr)
@@ -52,6 +101,14 @@ SoDebug::NamePtr(const char * name, void * ptr)
   SoDebug_internal::namedict->put(ptr, data);
 }
 
+/*!
+  Returns the name set on a pointer with NamePtr().  If no name has been
+  set, "<unnamed>" is returned.
+
+  \sa NamePtr()
+*/
+
+
 const char *
 SoDebug::PtrName(void * ptr)
 {
@@ -66,11 +123,19 @@ SoDebug::PtrName(void * ptr)
   return fallback;
 }
 
+/*!
+  Not implemented.
+*/
+
 void
 SoDebug::write(SoNode * node)
 {
   // FIXME: stub
 }
+
+/*!
+  Not implemented.
+*/
 
 void
 SoDebug::writeToFile(SoNode * node, const char * filename)
@@ -78,17 +143,27 @@ SoDebug::writeToFile(SoNode * node, const char * filename)
   // FIXME: stub
 }
 
+/*!
+  Not implemented.
+*/
+
 void
 SoDebug::writeField(SoField * field)
 {
   // FIXME: stub
 }
 
+/*!
+  Not implemented.
+*/
+
 void
 SoDebug::printName(SoBase * base)
 {
   // FIXME: stub
 }
+
+// *************************************************************************
 
 void
 SoDebug_internal::delete_namedict(void)
