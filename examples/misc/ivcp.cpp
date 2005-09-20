737dbe940d7793e22801dd4ac1b0c4caae8d806a
Lars J. Aas <larsa@sim.no>
2005-09-20 16:01:52 +0000

    beautify source

--- examples/misc/ivcp.cpp
+++ examples/misc/ivcp.cpp
@@ -1,3 +1,38 @@
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+/*
+ * Simple example that demonstrates how to read in and write out an
+ * Open Inventor model, essentially copying it.  This process has no use
+ * per se, but can be used to sanity-check the import/export-related
+ * parts of Coin, and can be used as a starting point for utilities doing
+ * scene graph rewrites.
+ *
+ * Build the example using this command:
+ *
+ *   coin-config --build ivcp ivcp.cpp
+ *
+ */
 
 #include <assert.h>
 #include <stdio.h>
