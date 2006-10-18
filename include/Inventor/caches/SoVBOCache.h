b133ff0bb9fe91524f943281772d0de89b0e2a6c
Peder Blekken <pederb@sim.no>
2006-10-18 10:50:03 +0000

    First implementation of the VBO cache.

--- include/Inventor/caches/SoVBOCache.h
+++ include/Inventor/caches/SoVBOCache.h
@@ -1,1 +1,51 @@
-// coming soon
+#ifndef COIN_VBO_CACHE_H
+#define COIN_VBO_CACHE_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+#include <Inventor/caches/SoCache.h>
+
+class SoVBOCacheP;
+class SoVBO;
+class SoVertexArrayIndexer;
+
+class SoVBOCache : public SoCache {
+  typedef SoCache inherited;
+public:
+  SoVBOCache(SoState * state);
+  virtual ~SoVBOCache();
+
+  SoVertexArrayIndexer * getVertexArrayIndexer(const SbBool createifnull = TRUE);
+  SoVBO * getCoordVBO(const SbBool createifnull = TRUE);
+  SoVBO * getNormalVBO(const SbBool createifnull = TRUE);
+  SoVBO * getColorVBO(const SbBool createifnull = TRUE);
+  SoVBO * getTexCoordVBO(const int unit, const SbBool createifnull = TRUE);
+  
+private:
+  SoVBOCacheP * pimpl;
+};
+
+
+
+#endif // COIN_VBO_CACHE_H
--- src/caches/SoVBOCache.cpp
+++ src/caches/SoVBOCache.cpp
@@ -1,1 +1,151 @@
-// coming soon
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+/*!
+  \class SoVBOCache SoVBOCache.h
+  
+  \brief The SoVBOCache is used to organize pointers to SoVBO and SoVertexArrayIndexer instances.
+  
+*/
+
+#include <Inventor/caches/SoVBOCache.h>
+#include "../misc/SoVBO.h"
+#include "../misc/SoVertexArrayIndexer.h"
+#include <Inventor/lists/SbList.h>
+
+class SoVBOCacheP {
+public:
+  SoVBOCacheP(void) {
+    this->vaindexer = NULL;
+    this->coordvbo = NULL;
+    this->colorvbo = NULL;
+    this->normalvbo = NULL;
+  }
+  ~SoVBOCacheP() {
+    delete this->vaindexer;
+    delete this->coordvbo;
+    delete this->colorvbo;
+    delete this->normalvbo;
+    
+    for (int i = 0; i < this->texcoordvbo.getLength(); i++) {
+      delete this->texcoordvbo[i];
+    }
+  }
+  
+  SoVertexArrayIndexer * vaindexer;
+  SoVBO * coordvbo;
+  SoVBO * colorvbo;
+  SoVBO * normalvbo;
+  SbList <SoVBO*> texcoordvbo;
+};
+
+/*!
+  Constructor.
+*/
+SoVBOCache::SoVBOCache(SoState * state)
+  : SoCache(state)
+{
+  this->pimpl = new SoVBOCacheP();
+}
+
+/*!
+  Destructor.
+*/
+SoVBOCache::~SoVBOCache()
+{
+  delete this->pimpl;
+}
+
+/*!
+  Returns the vertex array indexer. If \a createifnull is TRUE, a
+  indexer will be created if it doesn't exist.
+*/
+SoVertexArrayIndexer * 
+SoVBOCache::getVertexArrayIndexer(const SbBool createifnull)
+{
+  if (createifnull && (this->pimpl->vaindexer == NULL)) {
+    this->pimpl->vaindexer = new SoVertexArrayIndexer;
+  }
+  return this->pimpl->vaindexer;
+}
+
+/*!
+  Returns the coordinate VBO instance. If \a createifnull is TRUE, a
+  VBO instance will be created if it doesn't exist.
+*/
+SoVBO * 
+SoVBOCache::getCoordVBO(const SbBool createifnull)
+{
+  if (createifnull && (this->pimpl->coordvbo == NULL)) {
+    this->pimpl->coordvbo = new SoVBO;
+  }
+  return this->pimpl->coordvbo;
+}
+
+/*!
+  Returns the normal vertex VBO instance. If \a createifnull is TRUE, a
+  VBO instance will be created if it doesn't exist.
+*/
+
+SoVBO * 
+SoVBOCache::getNormalVBO(const SbBool createifnull)
+{
+  if (createifnull && (this->pimpl->normalvbo == NULL)) {
+    this->pimpl->normalvbo = new SoVBO;
+  }
+  return this->pimpl->normalvbo;
+}
+
+/*!
+  Returns the color VBO instance. If \a createifnull is TRUE, a
+  VBO instance will be created if it doesn't exist.
+*/
+SoVBO * 
+SoVBOCache::getColorVBO(const SbBool createifnull)
+{
+  if (createifnull && (this->pimpl->colorvbo == NULL)) {
+    this->pimpl->colorvbo = new SoVBO;
+  }
+  return this->pimpl->colorvbo;
+}
+
+/*!
+  Returns the texture coordinate VBO instance. If \a createifnull is TRUE, a
+  VBO instance will be created if it doesn't exist.
+*/
+SoVBO * 
+SoVBOCache::getTexCoordVBO(const int unit, const SbBool createifnull)
+{
+  if (createifnull) {
+    while (this->pimpl->texcoordvbo.getLength() <= unit) {
+      this->pimpl->texcoordvbo.append(NULL);
+    }
+    if (this->pimpl->texcoordvbo[unit] == NULL) {
+      this->pimpl->texcoordvbo[unit] = new SoVBO;
+    }
+  }
+  if (this->pimpl->texcoordvbo.getLength() > unit) {
+    return this->pimpl->texcoordvbo[unit];
+  }
+  return NULL;
+}
