3f186f34c4f6f3f7a9f98857a3320ad7ee0f5308
Peder Blekken <pederb@sim.no>
2007-03-15 10:40:24 +0000

    Implemented this shader utility class.

--- include/Inventor/misc/SoShaderGenerator.h
+++ include/Inventor/misc/SoShaderGenerator.h
@@ -1,1 +1,62 @@
-// coming soon
+#ifndef COIN_SOSHADERGENERATOR_H
+#define COIN_SOSHADERGENERATOR_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+#ifndef COIN_INTERNAL
+#error this is a private header file
+#endif /* !COIN_INTERNAL */
+
+#include <Inventor/SbString.h>
+
+class SbName;
+
+class SoShaderGenerator {
+public:
+
+  SoShaderGenerator(void);
+  ~SoShaderGenerator();
+
+  void reset(const SbBool freeoldstrings);
+
+  void addDefine(const SbString & str, const SbBool checkexists);
+  void addDeclaration(const SbString & str, const SbBool checkexists);
+  void addFunction(const SbString & str, const SbBool checkexists);
+  void addNamedFunction(const SbName & name, const SbBool checkexists);
+  
+  void addMainStatement(const SbString & str);
+
+  const SbString & getShaderProgram(void);
+
+private:
+  SbString defines;
+  SbString declarations;
+  SbString functions;
+  SbString main;
+  
+  SbBool dirty;
+  SbString combined;
+};
+
+#endif // COIN_SOSHADERGENERATOR_H
--- src/misc/SoShaderGenerator.cpp
+++ src/misc/SoShaderGenerator.cpp
@@ -1,1 +1,148 @@
-// coming soon
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2005 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org/> for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no/>.
+ *
+\**************************************************************************/
+
+/*!
+  \class SoShaderGenerator SoShaderGenerator.h Inventor/misc/SoShaderGenerator.h
+  \brief The SoShaderGenerator class is used for simplifying the process of generating shader scripts.
+
+  Currently only GLSL scripts are supported.
+*/
+
+#include <Inventor/misc/SoShaderGenerator.h>
+#include <Inventor/SbName.h>
+#include <Inventor/nodes/SoShader.h>
+#include <Inventor/errors/SoDebugError.h>
+
+/*!
+  Constructor.
+*/
+SoShaderGenerator::SoShaderGenerator(void)
+  : dirty(FALSE)
+{
+}
+
+/*!
+  Destructor.
+*/
+SoShaderGenerator::~SoShaderGenerator()
+{
+}
+
+void 
+SoShaderGenerator::reset(const SbBool freeoldstrings)
+{
+  this->defines.makeEmpty(freeoldstrings);
+  this->declarations.makeEmpty(freeoldstrings);
+  this->functions.makeEmpty(freeoldstrings);
+  this->main.makeEmpty(freeoldstrings);
+  this->combined.makeEmpty(freeoldstrings);
+  this->dirty = FALSE;
+}
+
+
+/*!
+  Adds a define to the shader program.
+*/
+void 
+SoShaderGenerator::addDefine(const SbString & str, const SbBool checkexists)
+{
+  if (!checkexists || (this->defines.find(str) < 0)) {
+    this->dirty = TRUE;
+    this->defines += str;
+    this->defines += "\n";
+  }
+}
+
+/*!
+  Adds a declaration (varying or uniform) to the script.
+*/
+void 
+SoShaderGenerator::addDeclaration(const SbString & str, const SbBool checkexists)
+{
+  if (!checkexists || (this->declarations.find(str) < 0)) {
+    this->dirty = TRUE;
+    this->declarations += str;
+    this->declarations += "\n";
+  }
+}
+
+/*!
+  Adds a function to the script.
+*/
+void 
+SoShaderGenerator::addFunction(const SbString & str, const SbBool checkexists)
+{
+  if (!checkexists || (this->functions.find(str) < 0)) {
+    this->dirty = TRUE;
+    this->functions += str;
+    this->functions += "\n";
+  }
+}
+
+/*!
+  Adds a named function to the script.
+*/
+void 
+SoShaderGenerator::addNamedFunction(const SbName & name, const SbBool checkexists)
+{
+  const char * func = SoShader::getNamedScript(name, SoShader::GLSL_SHADER);
+  
+  if (func) {
+    this->addFunction(SbString(func), checkexists);
+  }
+  else {
+    SoDebugError::postWarning("SoShaderGenerator::addNamedFunction",
+                              "Unknown named script: %s",
+                              name.getString());
+  }
+}
+
+/*!
+  Add a statment to the main function.
+*/
+void 
+SoShaderGenerator::addMainStatement(const SbString & str)
+{
+  this->dirty = TRUE;
+  this->main += str;
+  this->main += "\n";
+}
+
+/*!
+  Returns the complete shader program.
+*/
+const SbString &
+SoShaderGenerator::getShaderProgram(void)
+{
+  if (this->dirty) {
+    this->combined.makeEmpty(FALSE);
+    this->combined += this->defines;
+  
+    this->combined += this->declarations;
+    this->combined += this->functions;
+    this->combined += SbString("void main(void) {\n");
+    this->combined += this->main;
+    this->combined += "}\n";
+  }
+  return this->combined;
+}
