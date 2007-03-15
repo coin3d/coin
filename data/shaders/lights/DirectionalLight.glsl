960aaf6399db32b4eb1186eb1f467be3e30e28a4
Peder Blekken <pederb@sim.no>
2007-03-15 10:46:21 +0000

    Some shader programs.

---
+++ data/shaders/lights/DirectionalLight.glsl
@@ -0,0 +1,25 @@
+
+void DirectionalLight(in int i,
+                      in vec3 normal,
+                      inout vec4 ambient,
+                      inout vec4 diffuse,
+                      inout vec4 specular)
+{
+  float nDotVP; // normal . light direction
+  float nDotHV; // normal . light half vector
+  float pf;     // power factor
+  
+  nDotVP = max(0.0, dot(normal, normalize(vec3(gl_LightSource[i].position))));
+  nDotHV = max(0.0, dot(normal, vec3(gl_LightSource[i].halfVector)));
+  
+  if (nDotVP == 0.0) 
+    pf = 0.0;
+  else
+    pf = pow(nDotHV, gl_FrontMaterial.shininess);
+  
+  ambient += gl_LightSource[i].ambient;
+  diffuse += gl_LightSource[i].diffuse * nDotVP;
+  specular += gl_LightSource[i].specular * pf;
+}
+
+
---
+++ data/shaders/lights/SpotLight.glsl
@@ -0,0 +1,48 @@
+void SpotLight(in int i,
+               in vec3 eye,
+               in vec3 ecPosition3,
+               in vec3 normal,
+               inout vec4 ambient,
+               inout vec4 diffuse,
+               inout vec4 specular)
+{
+  float nDotVP;
+  float nDotHV;
+  float pf;  
+  float att;
+  float spotDot;
+  float spotAtt;
+  float d;          
+  vec3 VP;
+  vec3 halvfvec;
+
+  VP = vec3(gl_LightSource[i].position - ecPosition3);
+  d = length(VP);
+  VP = normalize(VP);
+
+  att = 1.0 / (gl_LightSource[i].constantAttenuation +
+               gl_LightSource[i].linearAttenuation * d +
+               gl_LightSource[i].quadraticAttenuation * d * d);
+  
+  spotDot = dot(-VP, normalize(gl_LightSource[i].spotDirection));
+  
+  if (spotDot < gl_LightSource[i].spotCosCutOff)
+    spotAtt = 0.0;
+  else
+    spotAtt = pow(spotDot, gl_LightSource[i].spotExponent);
+  
+  att *= spotAtt;
+
+  halfvec = normalize(VP + eye);
+  nDotVP = max(0.0, dot(normal, VP));
+  nDotHV = max(0.0, dot(normal, halfvec));
+
+  if (nDotVP == 0.0)
+    pf = 0.0;
+  else 
+    pf = pow(nDotHV, gl_FrontMaterial.shininess);
+  
+  ambient += gl_LightSource[i].ambient + att;
+  diffuse += gl_LightSource[i].diffuse * nDotVP * att;
+  specular += gl_LightSource[i].specular * pf * att;
+}
