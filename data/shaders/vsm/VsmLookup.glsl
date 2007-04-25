44d9b43ba0b6c7e958337c96bb9708e14884a2b1
Peder Blekken <pederb@sim.no>
2007-04-25 08:26:06 +0000

    Some comments in the vsm lookup code.

--- data/shaders/vsm/VsmLookup.glsl
+++ data/shaders/vsm/VsmLookup.glsl
@@ -1,6 +1,9 @@
 float VsmLookup(in vec4 map, in float dist, in float epsilon) 
 {
+#if 1 // Vsm smoothing version
   float mapdist = map.x;
+
+  // replace 0.0 with some factor > 0.0 to make the light affect even parts in shadow
   float lit_factor = dist <= mapdist ? 1.0 : 0.0;
   float E_x2 = map.y;
   float Ex_2 = mapdist * mapdist;
@@ -8,4 +11,8 @@ float VsmLookup(in vec4 map, in float dist, in float epsilon)
   float m_d = mapdist - dist;
   float p_max = variance / (variance + m_d * m_d);
   return max(lit_factor, p_max);
+#else // use this version for old fashioned shadow mapping with no variance
+  float mapdist = map.x;
+  return dist - epsilon <= mapdist ? 1.0 : 0.0;
+#endif
 }
