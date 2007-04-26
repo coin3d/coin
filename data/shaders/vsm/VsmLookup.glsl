c250ad86b5b9b310d68f4eed1dafbed49e6dbfab
Peder Blekken <pederb@sim.no>
2007-04-26 07:44:40 +0000

    Default to zero contribution from parts in shadow.

--- data/shaders/vsm/VsmLookup.glsl
+++ data/shaders/vsm/VsmLookup.glsl
@@ -3,7 +3,7 @@ float VsmLookup(in vec4 map, in float dist, in float epsilon)
   float mapdist = map.x;
 
   // replace 0.0 with some factor > 0.0 to make the light affect even parts in shadow
-  float lit_factor = dist <= mapdist ? 1.0 : 0.2;
+  float lit_factor = dist <= mapdist ? 1.0 : 0.0;
   float E_x2 = map.y;
   float Ex_2 = mapdist * mapdist;
   float variance = min(max(E_x2 - Ex_2, 0.0) + epsilon, 1.0);
