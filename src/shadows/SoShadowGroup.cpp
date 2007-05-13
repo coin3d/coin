4c2b06eb987d2577d9dc576b13dc11b1bdeba843
Tamer Fahmy <tamer@sim.no>
2007-05-13 18:11:30 +0000

    typo...

--- src/shadows/SoShadowGroup.cpp
+++ src/shadows/SoShadowGroup.cpp
@@ -503,7 +503,7 @@ SoShadowGroup::~SoShadowGroup()
 void
 SoShadowGroup::initClass(void)
 {
-'  SO_NODE_INTERNAL_INIT_CLASS(SoShadowGroup, SO_FROM_COIN_2_5);
+  SO_NODE_INTERNAL_INIT_CLASS(SoShadowGroup, SO_FROM_COIN_2_5);
 }
 
 void 
