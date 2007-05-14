1b5523938711630a10b9c88cb3994067879df4d1
Peder Blekken <pederb@sim.no>
2007-05-14 14:36:24 +0000

    Implementation.

--- include/Inventor/elements/SoGeoElement.h
+++ include/Inventor/elements/SoGeoElement.h
@@ -1,1 +1,56 @@
-// coming soon
+#ifndef COIN_SOGEOELEMENT_H
+#define COIN_SOGEOELEMENT_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2007 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See http://www.coin3d.org/ for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  http://www.sim.no/  sales@sim.no  coin-support@coin3d.org
+ *
+\**************************************************************************/
+
+#include <Inventor/elements/SoReplacedElement.h>
+
+class SoGeoOrigin;
+class SoGeoElementP;
+
+class COIN_DLL_API SoGeoElement : public SoReplacedElement {
+  typedef SoReplacedElement inherited;
+
+  SO_ELEMENT_HEADER(SoGeoElement);
+public:
+  static void initClass(void);
+protected:
+  virtual ~SoGeoElement();
+
+public:
+
+  virtual void init(SoState * state);
+  
+  static void set(SoState * const state, SoGeoOrigin * origin);
+  static SoGeoOrigin * get(SoState * const state);
+  
+protected:
+  
+  virtual void setElt(SoGeoOrigin * origin);
+  
+private:
+  SoGeoElementP * pimpl;
+};
+
+#endif // !COIN_SOGEOELEMENT_H
--- src/geo/SoGeoElement.cpp
+++ src/geo/SoGeoElement.cpp
@@ -1,1 +1,107 @@
-// coming soon
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2007 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See http://www.coin3d.org/ for more information.
+ *
+ *  Systems in Motion, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  http://www.sim.no/  sales@sim.no  coin-support@coin3d.org
+ *
+\**************************************************************************/
+
+/*!
+  \class SoGeoElement Inventor/elements/SoGeoElement.h
+  \brief The SoGeoElement class is yet to be documented.
+  \ingroup elements
+
+  FIXME: write doc.
+*/
+
+#include <Inventor/elements/SoGeoElement.h>
+#include <Inventor/nodes/SoGeoOrigin.h>
+#include <assert.h>
+
+
+class SoGeoElementP {
+public:
+  SoGeoOrigin * origin;
+};
+
+#define PRIVATE(obj) obj->pimpl
+
+SO_ELEMENT_SOURCE(SoGeoElement);
+
+/*!
+  This static method initializes static data for the
+  SoGeoElement class.
+*/
+
+void
+SoGeoElement::initClass(void)
+{
+  SO_ELEMENT_INIT_CLASS(SoGeoElement, inherited);
+}
+
+/*!
+  The destructor.
+*/
+
+SoGeoElement::~SoGeoElement(void)
+{
+}
+
+//! FIXME: write doc.
+
+void
+SoGeoElement::set(SoState * const state,
+                  SoGeoOrigin * origin)
+{
+  SoGeoElement * element = (SoGeoElement *)
+    SoReplacedElement::getElement(state, classStackIndex, origin);
+  
+  if (element) {
+    element->setElt(origin);
+  }
+}
+
+//! FIXME: write doc.
+
+SoGeoOrigin *
+SoGeoElement::get(SoState * const state)
+{
+  SoGeoElement * element = (SoGeoElement *)
+    SoElement::getConstElement(state, classStackIndex);
+ 
+  return PRIVATE(element)->origin;
+}
+
+//! FIXME: write doc.
+
+void
+SoGeoElement::init(SoState * state)
+{
+  inherited::init(state);
+  PRIVATE(this)->origin = NULL;
+}
+
+//! FIXME: doc
+void
+SoGeoElement::setElt(SoGeoOrigin * origin)
+{
+  PRIVATE(this)->origin = origin;
+}
+
+#undef PRIVATE
+
