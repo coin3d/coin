5affdac070b794ea18636e3a25da14eb93c3dba2
Peder Blekken <pederb@sim.no>
2007-05-18 15:36:39 +0000

    Support for lat/long coordinates in SoGeoLocation and SoGeoSeparator.

--- src/geo/SoGeo.cpp
+++ src/geo/SoGeo.cpp
@@ -37,8 +37,9 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void 
+void
 SoGeo::init(void)
 {
   SoGeoElement::initClass();
@@ -46,6 +47,15 @@ SoGeo::init(void)
   SoGeoLocation::initClass();
   SoGeoSeparator::initClass();
   SoGeoCoordinate::initClass();
+}
+
+// return zone number stored in string, or 0 if parsing failed
+static int find_utm_zone(const SbString & s)
+{
+  if (s.getLength() < 2) return 0;
+  if (s[0] != 'Z' && s[1] != 'z') return 0;
+  
+  return (int) atoi(s.getString()+1);
 }
 
 SbMatrix
@@ -56,12 +66,6 @@ SoGeo::calculateTransform(const SbString * originsystem,
                           const int numtargetsys,
                           const SbString & targetcoords)
 {
-  
-  assert(numoriginsys == 2);
-  assert(numtargetsys == 2);
-
-  assert(targetsystem[0] == "UTM");
-  assert(originsystem[0] == "UTM");
 
   double originpos[3];
   double targetpos[3];
@@ -72,11 +76,48 @@ SoGeo::calculateTransform(const SbString * originsystem,
 
   num = sscanf(targetcoords.getString(), "%lf%lf%lf",
                &targetpos[0], &targetpos[1], &targetpos[2]);
-  
+
+
+  assert(numoriginsys == 2);
+  assert(numtargetsys == 2);
+
+  // only support projecting to UTM for now
+  assert(originsystem[0] == "UTM" && "unsupported projection");
+
+  if (targetsystem[0] == "UTM") {
+    if (targetsystem[1] != originsystem[1]) { // different UTM zones
+      SbUTMProjection targetproj(find_utm_zone(targetsystem[1]), SbGeoEllipsoid("WGS84"));
+      SbUTMProjection proj(find_utm_zone(originsystem[1]), SbGeoEllipsoid("WGS84"));
+      
+      SbGeoAngle lat, lng;
+
+      // unconvert target to geo coordinates
+      targetproj.unproject(targetpos[0], targetpos[1], &lat, &lng);
+
+      // project coordinates into the original utm system
+      proj.project(lat,lng, &targetpos[0], &targetpos[1]);
+    }
+  }
+  else if (targetsystem[1] == "GD") {
+    int zone = find_utm_zone(originsystem[1]);
+    SbUTMProjection proj(zone, SbGeoEllipsoid("WGS84"));
+
+    SbGeoAngle lat(targetpos[0], 0.0, 0.0, 'N');
+    SbGeoAngle lng(targetpos[1], 0.0, 0.0, 'N');
+    
+    // just write back into targetpos
+    proj.project(lat, lng, &targetpos[0], &targetpos[1]);
+  }
+  else {
+    assert(0 && "unsupported projection");
+  }
+
+
+
   SbMatrix mat = SbMatrix::identity();
   mat.setTranslate(SbVec3f((float)(targetpos[0]-originpos[0]),
                            (float)(targetpos[1]-originpos[1]),
                            (float)(targetpos[2]-originpos[2])));
-  
+
   return mat;
 }
