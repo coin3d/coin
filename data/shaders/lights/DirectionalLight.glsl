9462370e5233925bb61cd45b0860915b9660badc
Peder Blekken <pederb@sim.no>
2007-06-12 12:14:25 +0000

    read OpenGL state outside if statements (ATi workaround).

--- data/shaders/lights/DirectionalLight.glsl
+++ data/shaders/lights/DirectionalLight.glsl
@@ -11,11 +11,12 @@ void DirectionalLight(in int i,
   
   nDotVP = max(0.0, dot(normal, normalize(vec3(gl_LightSource[i].position))));
   nDotHV = max(0.0, dot(normal, vec3(gl_LightSource[i].halfVector)));
-  
+
+  float shininess = gl_FrontMaterial.shininess;   
   if (nDotVP == 0.0) 
     pf = 0.0;
   else
-    pf = pow(nDotHV, gl_FrontMaterial.shininess);
+    pf = pow(nDotHV, shininess);
   
   ambient += gl_LightSource[i].ambient;
   diffuse += gl_LightSource[i].diffuse * nDotVP;
