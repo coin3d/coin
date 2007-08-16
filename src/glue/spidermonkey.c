2bd5f50f14447bc7168ad68eec3016c0fbdd1668
Tarjei Kvamme L?set <tarjei@sim.no>
2007-08-16 08:10:11 +0000

    Added JS_DestroyIdArray.
    NOTE: To avoid memory leaks, always use JS_DestroyIdArray following a JS_Enumerate call.

--- src/glue/spidermonkey.c
+++ src/glue/spidermonkey.c
@@ -311,6 +311,7 @@ spidermonkey(void)
   REGISTER_FUNC(JS_IdToValue, JS_IdToValue_t);
   REGISTER_FUNC(JS_GetFunctionName, JS_GetFunctionName_t);
   REGISTER_FUNC(JS_GetConstructor, JS_GetConstructor_t);
+  REGISTER_FUNC(JS_DestroyIdArray, JS_DestroyIdArray_t);
 
 wrapperexit:
   CC_SYNC_END(spidermonkey);
