d9bd29df48d5529a9dc4f7038d88bc11fad492b0
Lars J. Aas <larsa@sim.no>
2008-03-25 12:49:10 +0000

    initially omitted files in the SCXML framework

---
+++ include/Inventor/scxml/ScXMLOnEntry.h
@@ -0,0 +1,60 @@
+#ifndef COIN_SCXMLONENTRY_H
+#define COIN_SCXMLONENTRY_H
+
+/**************************************************************************\
+ *
+ *  This file is part of the Coin 3D visualization library.
+ *  Copyright (C) 1998-2008 by Kongsberg SIM.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using Coin with software that can not be combined with the GNU
+ *  GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Kongsberg SIM about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See http://www.coin3d.org/ for more information.
+ *
+ *  Kongsberg SIM, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  http://www.sim.no/  sales@sim.no  coin-support@coin3d.org
+ *
+\**************************************************************************/
+
+#include <Inventor/scxml/ScXMLObject.h>
+
+#include <vector>
+
+class ScXMLInvoke;
+class ScXMLEvent;
+class ScXMLStateMachine;
+
+class COIN_DLL_API ScXMLOnEntry : public ScXMLObject {
+  typedef ScXMLObject inherited;
+  SCXML_OBJECT_HEADER(ScXMLOnEntry);
+
+public:
+  static void initClass(void);
+
+  ScXMLOnEntry(void);
+  virtual ~ScXMLOnEntry(void);
+
+  // executable content
+  virtual int getNumInvokes(void) const;
+  virtual const ScXMLInvoke * getInvoke(int idx) const;
+  virtual void addInvoke(const ScXMLInvoke * invoke);
+  virtual void removeInvoke(const ScXMLInvoke * invoke);
+  virtual void clearAllInvokes(void);
+
+  // invoke
+  virtual void invoke(ScXMLStateMachine * statemachine);
+  
+protected:
+  std::vector<const ScXMLInvoke *> invokelist;
+
+}; // ScXMLOnEntry
+
+#endif // !COIN_SCXMLONENTRY_H
