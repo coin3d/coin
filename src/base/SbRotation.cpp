0f1c2961a50b81d160889bf32debd67ee20b112c
Tom Fredrik Blenning Klaussen <bfg@sim.no>
2010-02-18 00:28:55 +0100

    This function is unneeded, and wrong anyway.

--- src/base/SbRotation.cpp
+++ src/base/SbRotation.cpp
@@ -734,26 +734,6 @@ SbRotation::print(FILE * fp) const
 #include <boost/lexical_cast.hpp>
 #include <cassert>
 
-// Test for almostEquality
-static bool floatEquals(float A, float B, int maxUlps)
-{
-    // Make sure maxUlps is non-negative and small enough that the
-    // default NAN won't compare as equal to anything.
-    assert(maxUlps > 0 && maxUlps < 4 * 1024 * 1024);
-    int aInt = *(int*)&A;
-    // Make aInt lexicographically ordered as a twos-complement int
-    if (aInt < 0)
-        aInt = 0x80000000 - aInt;
-    // Make bInt lexicographically ordered as a twos-complement int
-    int bInt = *(int*)&B;
-    if (bInt < 0)
-        bInt = 0x80000000 - bInt;
-    int intDiff = abs(aInt - bInt);
-    if (intDiff <= maxUlps)
-        return true;
-    return false;
-}
-
 typedef SbRotation ToTest;
 BOOST_AUTO_TEST_CASE(operatorBrackets)
 {
