bed32d3c569055037c4ec73911af1425fa7eaed9
Jostein <josteint@sim.no>
2010-03-08 11:53:43 +0100

    updated copyright headers

--- src/elements/SoElement.cpp
+++ src/elements/SoElement.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************\
  *
  *  This file is part of the Coin 3D visualization library.
- *  Copyright (C) 1998-2009 by Kongsberg SIM.  All rights reserved.
+ *  Copyright (C) by Kongsberg Oil & Gas Technologies.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
@@ -11,12 +11,12 @@
  *
  *  For using Coin with software that can not be combined with the GNU
  *  GPL, and for taking advantage of the additional benefits of our
- *  support services, please contact Kongsberg SIM about acquiring
- *  a Coin Professional Edition License.
+ *  support services, please contact Kongsberg Oil & Gas Technologies
+ *  about acquiring a Coin Professional Edition License.
  *
  *  See http://www.coin3d.org/ for more information.
  *
- *  Kongsberg SIM, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  Kongsberg Oil & Gas Technologies, Bygdoy Alle 5, 0257 Oslo, NORWAY.
  *  http://www.sim.no/  sales@sim.no  coin-support@coin3d.org
  *
 \**************************************************************************/
@@ -99,8 +99,8 @@
   // The purpose of the code in this file is to demonstrate how you can
   // make your own elements for scene graph traversals.
   //
-  // Code by Peder Blekken <pederb@sim.no>, 1999-12-09. Copyright
-  // Kongsberg SIM.
+  // Code by Peder Blekken <pederb@sim.no>. Copyright (C)
+  // Kongsberg Oil & Gas Technologies.
 
   #include "texturefilenameelement.h"
 
@@ -174,7 +174,7 @@
   //
   // Code by Peder Blekken <pederb@sim.no>. Cleaned up, integrated in
   // Coin distribution and commented by Morten Eriksen
-  // <mortene@sim.no>. 1999-12-09. Copyright Kongsberg SIM.
+  // <mortene@sim.no>. Copyright (C) Kongsberg Oil & Gas Technologies.
 
   #include <Inventor/SoDB.h>
   #include <Inventor/SoInput.h>
--- src/nodes/SoTexture2.cpp
+++ src/nodes/SoTexture2.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************\
  *
  *  This file is part of the Coin 3D visualization library.
- *  Copyright (C) 1998-2009 by Kongsberg SIM.  All rights reserved.
+ *  Copyright (C) by Kongsberg Oil & Gas Technologies.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
@@ -11,12 +11,12 @@
  *
  *  For using Coin with software that can not be combined with the GNU
  *  GPL, and for taking advantage of the additional benefits of our
- *  support services, please contact Kongsberg SIM about acquiring
- *  a Coin Professional Edition License.
+ *  support services, please contact Kongsberg Oil & Gas Technologies
+ *  about acquiring a Coin Professional Edition License.
  *
  *  See http://www.coin3d.org/ for more information.
  *
- *  Kongsberg SIM, Postboks 1283, Pirsenteret, 7462 Trondheim, NORWAY.
+ *  Kongsberg Oil & Gas Technologies, Bygdoy Alle 5, 0257 Oslo, NORWAY.
  *  http://www.sim.no/  sales@sim.no  coin-support@coin3d.org
  *
 \**************************************************************************/
@@ -290,8 +290,8 @@
   FIXME: Section about simage is outdated.
 
   For reading texture image files from disk, Coin uses the "simage"
-  library from Kongsberg SIM. This library must be installed for
-  it to be possible to load any image files at all.
+  library from Kongsberg Oil & Gas Technologies. This library must be
+  installed for it to be possible to load any image files at all.
 
   The set of image formats you can actually read with the simage
   library depends on how the simage library was built, as simage is
