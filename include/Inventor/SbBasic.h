13726f3b0cf300657bc2ea2a9f9deb5126f3f3f4
TheHubbit <TheHubbit@nospam.org>
2014-06-03 22:42:37 +0200

    Added missing include for cc_debugerror_post() as discussed in pull request 3 (for separate pull request).

--- include/Inventor/SbBasic.h
+++ include/Inventor/SbBasic.h
@@ -97,9 +97,13 @@ inline Type SbSqr(const Type val) {
 // warning if so for debug builds.  inlined like this to not take much
 // screenspace in inline functions.
 
-// cc_debugerror_post() is not attempted resolved before the template is
-// used, hence the missing Inventor/errors/SoDebugError.h #include. This
-// "trick" does only work *portably* for functions in the global namespace.
+// Missing include for cc_debugerror_post() added here. The previous "trick"
+// for not needing to resolve symbols in global namespace no longer works
+// with newer compilers.
+#ifndef NDEBUG
+#include <Inventor/C/errors/debugerror.h>
+#endif // !NDEBUG
+
 
 template <typename Type>
 inline void SbDividerChk(const char * funcname, Type divider) {
