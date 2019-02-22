3edeb8a332d245f0b88c41e42d59462870ef18c4
Volker Enderlein <volkerenderlein@hotmail.com>
2019-02-22 23:29:12 +0100

    Removed non ASCII characters at end of line causing problems with Chinese simplified and Visual Studio 2017

--- src/projectors/SbSphereSheetProjector.cpp
+++ src/projectors/SbSphereSheetProjector.cpp
@@ -190,7 +190,7 @@ SbSphereSheetProjector::project(const SbVec2f & point)
 
   // distance from plane hit point to plane center in the projector
   planardist = (planehit - this->planePoint).length();
-  // let sphere and hyperbolic sheet meet at 45°
+  // let sphere and hyperbolic sheet meet at 45 degree
   meetdist =  this->sphere.getRadius() * (float) cos(M_PI / 4.0);
 
   if (planardist < meetdist) {
@@ -198,7 +198,7 @@ SbSphereSheetProjector::project(const SbVec2f & point)
     projpt = spherehit;
   }
   else {
-    // By Pythagoras' we know that the value of the sphere at 45°
+    // By Pythagoras' we know that the value of the sphere at 45 degree
     // angle from the groundplane will be (radius^2 * 0.5).
     float v = (this->sphere.getRadius() * this->sphere.getRadius()) * 0.5f;
 
