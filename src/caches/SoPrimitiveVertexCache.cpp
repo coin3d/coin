959d344996db1979f7ccd71d9056709d43df5f0f
Volker Enderlein <volkerenderlein@hotmail.com>
2021-03-15 23:45:50 +0100

    Restructure to fix Resource not released in destructor

--- src/caches/SoPrimitiveVertexCache.cpp
+++ src/caches/SoPrimitiveVertexCache.cpp
@@ -105,6 +105,26 @@ public:
       rgbavbo(NULL),
       tangentvbo(NULL)
   { }
+  ~SoPrimitiveVertexCacheP()
+  {
+    delete triangleindexer;
+    delete lineindexer;
+    delete pointindexer;
+    delete vertexvbo;
+    delete normalvbo;
+    delete texcoord0vbo;
+    delete rgbavbo;
+    delete rgbavbo;
+    delete tangentvbo;
+
+    for (int i = 0; i < multitexvbo.getLength(); i++) {
+      delete multitexvbo[i];
+    }
+    if (lastenabled >= 1) {
+      delete[] multitexcoords;
+    }
+    delete[] deptharray;
+  }
 
   class Vertex {
   public:
@@ -304,22 +324,6 @@ SoPrimitiveVertexCache::~SoPrimitiveVertexCache()
 
   }
 #endif // debug
-
-  delete PRIVATE(this)->triangleindexer;
-  delete PRIVATE(this)->lineindexer;
-  delete PRIVATE(this)->pointindexer;
-  delete PRIVATE(this)->vertexvbo;
-  delete PRIVATE(this)->normalvbo;
-  delete PRIVATE(this)->texcoord0vbo;
-  delete PRIVATE(this)->rgbavbo;
-
-  for (int i = 0; i < PRIVATE(this)->multitexvbo.getLength(); i++) {
-    delete PRIVATE(this)->multitexvbo[i];
-  }
-  if (PRIVATE(this)->lastenabled >= 1) {
-    delete[] PRIVATE(this)->multitexcoords;
-  }
-  delete [] PRIVATE(this)->deptharray;
 }
 
 SbBool 
