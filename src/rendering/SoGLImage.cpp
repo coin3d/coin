e3d0a8aa67097988976ca41d18c30161a4ff6f74
Volker Enderlein <volkerenderlein@hotmail.com>
2022-10-09 21:58:20 +0200

    Unify comment with other occurrences

--- src/rendering/SoGLImage.cpp
+++ src/rendering/SoGLImage.cpp
@@ -227,7 +227,7 @@
 #include "coindefs.h"
 
 #if BOOST_WORKAROUND(COIN_MSVC, <= COIN_MSVC_6_0_VERSION)
-// truncating symbol length
+// symbol length truncation
 #pragma warning(disable:4786)
 #endif // VC6.0
 
