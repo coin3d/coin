a2d766f9fea95f43d35217d21c21b3c383e83725
Bastiaan Veelo <Bastiaan@SARC.nl>
2026-06-21 01:12:59 +0200

    Demo loading scene graph from file.

--- examples/helloworld/coin_helloworld.cpp
+++ examples/helloworld/coin_helloworld.cpp
@@ -5,12 +5,27 @@
 
 int main(int argc, char** argv)
 {
-	
 	QWidget* mWindow = SoQt::init(argc, argv, argv[0]);
-	SoSeparator* mRoot = new SoSeparator;
-	mRoot->ref();
-	SoCone* mCone = new SoCone;
-	mRoot->addChild(mCone);
+	SoSeparator* mRoot = nullptr;
+	if (argc < 2) {
+		// Create simple scene graph
+		mRoot = new SoSeparator;
+		mRoot->ref();
+		mRoot->addChild(new SoCone);
+	} else {
+		// Read scene graph from file
+		SoInput in;
+		if (!in.openFile(argv[1])) {
+			qWarning("could not open file %s\n", argv[1]);
+			exit(1);
+		}
+		mRoot = SoDB::readAll(&in);
+		if (!mRoot) {
+			qWarning("could not read file %s\n", argv[1]);
+			exit(1);
+		}
+		mRoot->ref();
+	}
 	SoQtExaminerViewer eviewer(mWindow);
 	eviewer.setSceneGraph(mRoot);
 	eviewer.show();
