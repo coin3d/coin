c8691ea945ac36868c47af679e39f4afcf65779c
Diego Kluwe de Souza <dikluwe@gmail.com>
2026-09-13 08:45:17 -0300

    Address review comment on disabled SoSFVec4us filter

--- src/fields/SoSFVec4us.cpp
+++ src/fields/SoSFVec4us.cpp
@@ -131,8 +131,9 @@ BOOST_AUTO_TEST_CASE(textinput)
   SoSFVec4us field;
   field.set("1 2 3 4");
   BOOST_CHECK_EQUAL(field.getValue(), SbVec4us(1, 2, 3, 4));
+  // const char * filters[] = { "read error", NULL }; // all read error messages
   TestSuite::ResetReadErrorCount();
-  // TestSuite::PushMessageSuppressFilters(filters); // filters = { "read error", NULL }
+  // TestSuite::PushMessageSuppressFilters(filters);
   SbBool ok;
   ok = field.set("-3 4 32 3"); // should emit error message on '-3'
   BOOST_CHECK_EQUAL(ok, FALSE);
