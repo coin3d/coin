68313c76898faa366666939a5cb681c53b32df4e
Diego Kluwe de Souza <dikluwe@gmail.com>
2026-09-16 09:04:42 -0300

    Validate legacy SoPath accessor bounds in all builds

--- src/misc/SoPath.cpp
+++ src/misc/SoPath.cpp
@@ -436,13 +436,13 @@ SoPath::getTail(void) const
 SoNode *
 SoPath::getNode(const int index) const
 {
-#if COIN_DEBUG
   if (index < 0 || index >= this->getFullLength()) {
+#if COIN_DEBUG
     SoDebugError::post("SoPath::getNode", "index %d is out of bounds.",
                        index);
+#endif // COIN_DEBUG
     return NULL;
   }
-#endif // COIN_DEBUG
   return this->nodes[index];
 }
 
@@ -453,13 +453,13 @@ SoPath::getNode(const int index) const
 SoNode *
 SoPath::getNodeFromTail(const int index) const
 {
-#if COIN_DEBUG
   if (index < 0 || index >= this->getLength()) {
+#if COIN_DEBUG
     SoDebugError::post("SoPath::getNodeFromTail",
                        "index %d is out of bounds.", index);
+#endif // COIN_DEBUG
     return NULL;
   }
-#endif // COIN_DEBUG
   return this->nodes[this->getLength() - index - 1];
 }
 
@@ -470,13 +470,13 @@ SoPath::getNodeFromTail(const int index) const
 int
 SoPath::getIndex(const int index) const
 {
-#if COIN_DEBUG
   if (index < 0 || index >= this->getFullLength()) {
+#if COIN_DEBUG
     SoDebugError::post("SoPath::getIndex", "index %d is out of bounds.",
                        index);
+#endif // COIN_DEBUG
     return -1;
   }
-#endif // COIN_DEBUG
 
   return this->indices[index];
 }
@@ -489,13 +489,13 @@ SoPath::getIndex(const int index) const
 int
 SoPath::getIndexFromTail(const int index) const
 {
-#if COIN_DEBUG
   if (index < 0 || index >= this->getLength()) {
+#if COIN_DEBUG
     SoDebugError::post("SoPath::getIndexFromTail",
                        "index %d is out of bounds.", index);
+#endif // COIN_DEBUG
     return -1;
   }
-#endif // COIN_DEBUG
   return this->indices[this->getLength() - index - 1];
 }
 
